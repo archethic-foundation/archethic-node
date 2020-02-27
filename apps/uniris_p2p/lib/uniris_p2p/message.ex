f08df84b816f26ad546baee8beb96815254b3799
samuel-uniris <samuel@uniris.io>
2020-02-27 16:44:32 +0100

    Add Anti Corruption layer to the P2P message processing

--- apps/uniris_p2p/lib/uniris_p2p/message.ex
+++ apps/uniris_p2p/lib/uniris_p2p/message.ex
@@ -6,6 +6,9 @@ defmodule UnirisP2P.Message do
   alias UnirisChain.Transaction.ValidationStamp
   alias UnirisValidation, as: Validation
   alias UnirisChain, as: Chain
+  alias UnirisNetwork, as: Network
+  alias UnirisNetwork.Node
+  alias UnirisElection, as: Election
 
   require Logger
 
@@ -16,15 +19,15 @@ defmodule UnirisP2P.Message do
   @spec encode(term()) :: binary()
   def encode(payload) do
     binary_payload = :erlang.term_to_binary(payload)
-    {:ok, public_key} = Crypto.last_public_key(:node)
-    {:ok, sig} = Crypto.sign(binary_payload, source: :node, label: :last)
+    public_key = Crypto.last_node_public_key()
+    sig = Crypto.sign(binary_payload, with: :node, as: :last)
     public_key <> sig <> binary_payload
   end
 
   @doc """
   Decode a encoded message by first verifying its signature and then decode the binary
   """
-  @spec decode(binary()) :: {:ok, term(), public_key :: <<_::264>>} | {:error, :invalid_payload}
+  @spec decode(binary()) :: {:ok, term(), public_key :: <<_::binary-32>>} | {:error, :invalid_payload}
   def decode(<<public_key::binary-33, signature::binary-64, binary_payload::binary>>) do
     case Crypto.verify(signature, binary_payload, public_key) do
       :ok ->
@@ -39,23 +42,23 @@ defmodule UnirisP2P.Message do
   def decode(_), do: {:error, :invalid_payload}
 
   @doc """
-  Process message coming from a P2P request. Act as P2P message controller
+  Process message coming from a P2P request by actin as a message controller/broker providing an Anti Corruption Layer
   """
   @spec process(message :: tuple(), node_public_key :: binary()) ::
           {:ok, term()} | {:error, atom()}
-  def process({:get_transaction, tx_address}, _from) do
+  def process({:get_transaction, tx_address}, _from) when is_binary(tx_address) do
     Chain.get_transaction(tx_address)
   end
 
-  def process({:get_transaction_chain, tx_address}, _from) do
+  def process({:get_transaction_chain, tx_address}, _from) when is_binary(tx_address) do
     Chain.get_transaction_chain(tx_address)
   end
 
-  def process({:get_unspent_outputs, tx_address}, _from) do
+  def process({:get_unspent_outputs, tx_address}, _from) when is_binary(tx_address) do
     Chain.get_unspent_outputs(tx_address)
   end
 
-  def process({:get_proof_of_integrity, tx_address}, _from) do
+  def process({:get_proof_of_integrity, tx_address}, _from) when is_binary(tx_address) do
     with {:ok, %Transaction{validation_stamp: %ValidationStamp{proof_of_integrity: poi}}} <-
            Chain.get_transaction(tx_address) do
       {:ok, poi}
@@ -64,23 +67,45 @@ defmodule UnirisP2P.Message do
 
   def process(
         {:start_validation, tx = %Transaction{}, welcome_node_public_key, validation_nodes},
-        _from
-      ) do
-    Validation.start_validation(tx, welcome_node_public_key, validation_nodes)
+        from
+      ) when is_binary(welcome_node_public_key) and is_list(validation_nodes) do
+        cond do
+          !Enum.all?(validation_nodes, &(is_binary(&1))) ->
+            {:error,  :invalid_validation_nodes}
+          !Enum.all?(validation_nodes ++ [welcome_node_public_key], &Network.node_info(&1)) ->
+            {:error, :invalid_validation_nodes}
+          true ->
+            Validation.start_validation(tx, welcome_node_public_key, validation_nodes)
+        end
   end
 
-  def process({:replicate_transaction, tx = %Transaction{}}, _from) do
-    Validation.replicate_transaction(tx)
+  def process({:replicate_transaction, tx = %Transaction{validation_stamp: %ValidationStamp{}, cross_validation_stamps: stamps}}, from) when is_list(stamps) and length(stamps) >= 0 do
+    validation_node_public_keys = Election.validation_nodes(tx, Network.list_nodes(), Network.daily_nonce()) |> Enum.map(&(&1.last_public_key))
+    if from in validation_node_public_keys do
+      Validation.replicate_transaction(tx)
+    else
+      {:error, :unauthorized}
+    end
   end
 
-  def process({:cross_validate, tx_address, stamp = %ValidationStamp{}}, _from)
+  def process({:cross_validate, tx_address, stamp = %ValidationStamp{}}, from)
       when is_binary(tx_address) do
-    Validation.cross_validate(tx_address, stamp)
+    [%Node{last_public_key: coordinator_public_key} | _] = validation_node_public_keys = Election.validation_nodes(tx, Network.list_nodes(), Network.daily_nonce())
+    if coordinator_public_key == from do
+      Validation.cross_validate(tx_address, stamp)
+    else
+      {:error, :unauthorized}
+    end
   end
 
-  def process({:cross_validation_node, tx_address, {signature, inconsistencies}}, from)
+  def process({:cross_validation_done, tx_address, {signature, inconsistencies}}, from)
       when is_binary(tx_address) and is_binary(signature) and is_list(inconsistencies) do
-    Validation.add_cross_validation_stamp(tx_address, {signature, inconsistencies}, from)
+    # [_ | cross_validation_node_public_keys] = Election.validation_nodes(tx, Network.list_nodes(), Network.daily_nonce()) |> Enum.map(&(&1.last_public_key))
+    # f from in cross_validation_node_public_keys do
+      Validation.add_cross_validation_stamp(tx_address, {signature, inconsistencies}, from)
+    # else
+    #  {:error, :unauthorized}
+    #end
   end
 
   def process(msg, from) do
