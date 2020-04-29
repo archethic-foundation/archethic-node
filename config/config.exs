cc71e96e07ca1c01250cf8ac0e42d43b74b87732
Samuel Manzanera <samuel@uniris.io>
2020-04-29 19:56:36 +0200

    Improve logging with UTC time

--- config/config.exs
+++ config/config.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 import_config "../apps/*/config/config.exs"
 
-config :logger, :console,
-  format: "\n$time $metadata[$level] $levelpad$message\n"
+config :logger, :console, format: "\n$time $metadata[$level] $levelpad$message\n"
 
 config :logger,
-  handle_otp_reports: true
-  # handle_sasl_reports: true
+  utc_log: true,
+  handle_otp_reports: true,
+  handle_sasl_reports: false
