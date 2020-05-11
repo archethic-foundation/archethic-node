718e2e4c7442258f29ae7716317a52610bc2e0c7
Samuel Manzanera <samuel@uniris.io>
2020-05-11 10:21:40 +0200

    add CORS in the deps

--- apps/uniris_web/mix.exs
+++ apps/uniris_web/mix.exs
@@ -45,7 +45,8 @@ defmodule UnirisWeb.MixProject do
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.4.0"},
       {:absinthe_plug, "~> 1.4"},
-      {:absinthe_phoenix, "~> 1.4.0"}
+      {:absinthe_phoenix, "~> 1.4.0"},
+      {:cors_plug, "~> 1.5"}
     ]
   end
 end
