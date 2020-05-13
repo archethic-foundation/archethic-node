27b98dc811da38c37eba74b430c4032c807372d8
Samuel Manzanera <samuel@uniris.io>
2020-05-13 11:33:32 +0200

    load inets application for IPFY application

--- apps/uniris_core/lib/uniris_core/bootstrap/ip_lookup/ipfy_impl.ex
+++ apps/uniris_core/lib/uniris_core/bootstrap/ip_lookup/ipfy_impl.ex
@@ -6,7 +6,6 @@ defmodule UnirisCore.Bootstrap.IPLookup.IPFYImpl do
   @impl true
   @spec get_ip() :: :inet.ip_address()
   def get_ip() do
-    :inets.start()
     {:ok, {_, _, inet_addr}} = :httpc.request('http://api.ipify.org')
     :inets.stop()
     {:ok, ip} = :inet.parse_address(inet_addr)
--- apps/uniris_core/mix.exs
+++ apps/uniris_core/mix.exs
@@ -20,7 +20,7 @@ defmodule UnirisCore.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :inets],
       mod: {UnirisCore.Application, []}
     ]
   end
