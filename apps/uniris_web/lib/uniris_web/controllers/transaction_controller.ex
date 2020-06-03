c54c1b534a7f30ab6f10a6266eac3ba3f93dadcc
Samuel Manzanera <samuel@uniris.io>
2020-06-03 14:32:34 +0200

    Enable caching and compression for transaction content endpoint

--- apps/uniris_web/lib/uniris_web/controllers/transaction_controller.ex
+++ apps/uniris_web/lib/uniris_web/controllers/transaction_controller.ex
@@ -6,13 +6,24 @@ defmodule UnirisWeb.TransactionController do
 
   def last_transaction_content(conn, params = %{"address" => address}) do
     with {:ok, address} <- Base.decode16(address),
-         {:ok, %Transaction{data: %TransactionData{content: content}}} <- UnirisCore.get_last_transaction(address) do
+         {:ok, %Transaction{address: last_address, data: %TransactionData{content: content}}} <- UnirisCore.get_last_transaction(address) do
 
       mime_type = Map.get(params, "mime", "text/plain")
 
+      etag = Base.encode16(last_address, case: :lower)
+      status = case List.first(Plug.Conn.get_req_header(conn, "if-none-match")) do
+        got_etag when got_etag == etag ->
+          304
+        _ ->
+          200
+      end
+
       conn
       |> put_resp_content_type(mime_type, "utf-8")
-      |> send_resp(200, content)
+      |> put_resp_header("content-encoding", "gzip")
+      |> put_resp_header("cache-control", "public")
+      |> put_resp_header("etag", etag)
+      |> send_resp(status, :zlib.gzip(content))
 
     else
       _reason ->
