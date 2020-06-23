7250f6661e8f4ac4bb7f5feda512b61a5b442c15
Samuel Manzanera <samuel@uniris.io>
2020-06-23 10:12:51 +0200

    upgrade version

--- apps/uniris_web/mix.exs
+++ apps/uniris_web/mix.exs
@@ -4,7 +4,7 @@ defmodule UnirisWeb.MixProject do
   def project do
     [
       app: :uniris_web,
-      version: "0.4.0",
+      version: "0.5.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
