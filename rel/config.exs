45984c3aae6a8951a1a4b07982746d0a7f51b798
Samuel Manzanera <samuel@uniris.io>
2020-11-05 11:43:51 +0100

    Include the ERTS in releases to support hot upgrades

--- rel/config.exs
+++ rel/config.exs
@@ -36,7 +36,7 @@ environment :dev do
 end
 
 environment :prod do
-  set include_erts: false
+  set include_erts: true
   set include_src: false
   set cookie: :crypto.strong_rand_bytes(32) |> Base.encode16 |> String.to_atom
   set vm_args: "rel/vm.args"
