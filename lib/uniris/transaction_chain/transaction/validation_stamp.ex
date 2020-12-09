e88af8d616b206f1308bf42a6230b4a704e2257c
Pavlo Martynenko <pavelmart@gmail.com>
2020-12-09 12:13:14 +0200

    Fix ValidationStamp.valid_signature?/2

--- lib/uniris/transaction_chain/transaction/validation_stamp.ex
+++ lib/uniris/transaction_chain/transaction/validation_stamp.ex
@@ -229,12 +229,15 @@ defmodule Uniris.TransactionChain.Transaction.ValidationStamp do
   Determine if the validation stamp signature is valid
   """
   @spec valid_signature?(__MODULE__.t(), Crypto.key()) :: boolean()
-  def valid_signature?(stamp = %__MODULE__{}, public_key) do
+  def valid_signature?(%__MODULE__{signature: nil}, _public_key), do: false
+
+  def valid_signature?(stamp = %__MODULE__{signature: signature}, public_key)
+      when is_binary(signature) do
     raw_stamp =
       stamp
       |> extract_for_signature
       |> serialize
 
-    Crypto.verify(stamp.signature, raw_stamp, public_key)
+    Crypto.verify(signature, raw_stamp, public_key)
   end
 end
