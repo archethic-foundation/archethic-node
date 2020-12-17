90a68ffff40a40ed8340312094330fd31ca3055a
Pavlo Martynenko <pavelmart@gmail.com>
2020-12-17 15:00:25 +0200

    Typecheck all files

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -3,7 +3,5 @@
   {"lib/uniris/self_repair/scheduler.ex", :call, 93},
   {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 69},
   {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 84},
-  {"lib/uniris_web/templates/code/proposal_chat.html.leex", :call, 19},
-  {"lib/uniris/contracts/interpreter.ex"},
-  {"lib/uniris/contracts/worker.ex"}
+  {"lib/uniris_web/templates/code/proposal_chat.html.leex", :call, 19}
 ]
