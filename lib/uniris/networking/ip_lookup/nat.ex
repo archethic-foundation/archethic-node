f3bc4280d7df295fca2dc0008f50c39866703ddc
Dmytro Nasyrov <dmytro@pharosproduction.com>
2021-01-18 23:45:54 +0200

    change hostname and port discovery

--- lib/uniris/networking/ip_lookup/nat.ex
+++ lib/uniris/networking/ip_lookup/nat.ex
@@ -9,10 +9,16 @@ defmodule Uniris.Networking.IPLookup.Nat do
     |> discover
   end
 
-  @spec assign_port(list(atom)) :: {:ok, pos_integer} | {:error, any()}
+  @spec get_random_port() :: {:ok, pos_integer} | {:error, any()}
   def get_random_port do
     [:natupnp_v1, :natupnp_v2, :natpmp]
-    |> assign_port
+    |> assign_port(0)
+  end
+
+  @spec open_port(pos_integer) :: {:ok, pos_integer} | {:error, any()}
+  def open_port(port) do
+    [:natupnp_v1, :natupnp_v2, :natpmp]
+    |> assign_port(port)
   end
 
   # Private
@@ -31,13 +37,14 @@ defmodule Uniris.Networking.IPLookup.Nat do
     end
   end
 
-  @spec assign_port(list(atom)) :: {:ok, pos_integer} | {:error, any()}
-  defp assign_port([]), do: {:error, :port_unassigned}
-  defp assign_port([protocol_module | protocol_modules]) do
+  @spec assign_port(list(atom), pos_integer) :: {:ok, pos_integer} | {:error, any()}
+  defp assign_port([], _port), do: {:error, :port_unassigned}
+  defp assign_port([protocol_module | protocol_modules], port) do
     with {:ok, router_ip} <- protocol_module.discover(),
-    {:ok, _since, port, _external_port, _} <- protocol_module.add_port_mapping(router_ip, :tcp, 0, 0, 0) do
-      {:ok, port}
+    {:ok, _since, internal_port, _external_port, _} <- protocol_module.add_port_mapping(router_ip, :tcp, port, port, 0) do
+      {:ok, internal_port}
     else
+      {:error, {:http_error, _code, _reason}} -> discover(protocol_modules)
       {:error, :einval} -> discover(protocol_modules)
       {:error, :no_nat} -> discover(protocol_modules)
       {:error, :timeout} -> discover(protocol_modules)
