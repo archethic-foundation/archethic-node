d724899c82db2c32a1f0fcee1e215b94ef1e7dbd
pvmart <pvmart@users.noreply.github.com>
2021-01-22 09:49:56 +0200

    Intermediate step towards governance of code on chain (#18)
    * Fix Endpoint.version in prod config
    
    * The code could be not on the master bracnh
    
    * Add Code.succeessor_version? function
    
    * Stronger verification of code proposal version
    
    Version of a code proposal must be a direct successor of the current
    running version.
    
    * Add src_branch configuration option
    
    * Do not run full CI on Code.valid_proposal?/1

--- lib/uniris/governance/code.ex
+++ lib/uniris/governance/code.ex
@@ -20,13 +20,14 @@ defmodule Uniris.Governance.Code do
   alias Uniris.Utils
 
   @src_dir Application.compile_env(:uniris, :src_dir)
+  @src_branch Application.compile_env(:uniris, :src_branch)
 
   @doc """
   List the source files from the master branch
   """
   @spec list_source_files() :: list(binary())
   def list_source_files do
-    {files, 0} = System.cmd("git", ["ls-tree", "-r", "master", "--name-only"], cd: @src_dir)
+    {files, 0} = System.cmd("git", ["ls-tree", "-r", "HEAD", "--name-only"], cd: @src_dir)
     String.split(files, "\n", trim: true)
   end
 
@@ -64,19 +65,101 @@ defmodule Uniris.Governance.Code do
 
   @doc """
   Ensure the code proposal is valid according to the defined rules:
-  - Version in the code proposal must be greater than the current running version.
-  - Git diff/patch must be valid. A fork is make to apply the diff and run the CI tasks
+  - Version in the code proposal must be a direct successor of the current running version.
+  - Git diff/patch must be valid.
   """
   @spec valid_proposal?(Proposal.t()) :: boolean()
   def valid_proposal?(prop = %Proposal{version: version}) do
-    with :gt <- Version.compare(version, current_version()),
-         :ok <- CI.run(prop) do
+    with true <- succeessor_version?(current_version(), version),
+         true <- applicable_proposal?(prop) do
       true
     else
       _ ->
         false
     end
   end
+
+  @doc """
+  Ensure the code proposal is an applicable on the given branch.
+  """
+  @spec applicable_proposal?(Proposal.t()) :: boolean()
+  def applicable_proposal?(
+        %Proposal{changes: changes, address: address},
+        branch \\ @src_branch,
+        src_dir \\ @src_dir
+      ) do
+    random = :crypto.strong_rand_bytes(4) |> Base.encode16()
+    prop_file = Path.join(System.tmp_dir!(), "prop_#{random}_#{Base.encode16(address)}")
+    File.write!(prop_file, changes)
+
+    cmd_options = [stderr_to_stdout: true, cd: src_dir]
+    git = fn args -> System.cmd("git", args, cmd_options) end
+
+    x = byte_size(branch)
+
+    res =
+      case git.(["symbolic-ref", "--short", "HEAD"]) do
+        {<<^branch::binary-size(x), _::binary>>, 0} ->
+          git.(["apply", "--check", prop_file])
+
+        otherwise ->
+          {:error, otherwise}
+      end
+
+    File.rm(prop_file)
+
+    match?({_, 0}, res)
+  end
+
+  @doc """
+  Return true if version2 is a direct successor of version1.
+  Note that build and patch must not be set.
+
+  ## Examples
+
+    iex> Code.succeessor_version?("1.1.1", "1.1.2")
+    true
+
+    iex> Code.succeessor_version?("1.1.1", "1.2.0")
+    true
+
+    iex> Code.succeessor_version?("1.1.1", "2.0.0")
+    true
+
+    iex> Code.succeessor_version?("1.1.1", "1.2.2")
+    false
+
+    iex> Code.succeessor_version?("1.1.1", "1.2.1")
+    false
+
+    iex> Code.succeessor_version?("1.1.1", "1.1.2-pre0")
+    false
+  """
+  @spec succeessor_version?(binary | Version.t(), binary | Version.t()) :: boolean
+  def succeessor_version?(version1, version2)
+      when is_binary(version1) and is_binary(version2) do
+    succeessor_version?(Version.parse!(version1), Version.parse!(version2))
+  end
+
+  def succeessor_version?(
+        %Version{major: ma, minor: mi, patch: pa1, pre: [], build: nil},
+        %Version{major: ma, minor: mi, patch: pa2, pre: [], build: nil}
+      ),
+      do: pa1 + 1 == pa2
+
+  def succeessor_version?(
+        %Version{major: ma, minor: mi1, patch: _, pre: [], build: nil},
+        %Version{major: ma, minor: mi2, patch: 0, pre: [], build: nil}
+      ),
+      do: mi1 + 1 == mi2
+
+  def succeessor_version?(
+        %Version{major: ma1, minor: _, patch: _, pre: [], build: nil},
+        %Version{major: ma2, minor: 0, patch: 0, pre: [], build: nil}
+      ),
+      do: ma1 + 1 == ma2
+
+  def succeessor_version?(%Version{}, %Version{}), do: false
 
   defp current_version do
     {:ok, vsn} = :application.get_key(:uniris, :vsn)
--- test/uniris/governance/code_test.exs
+++ test/uniris/governance/code_test.exs
@@ -2,10 +2,26 @@ defmodule Uniris.Governance.CodeTest do
   use ExUnit.Case
 
   alias Uniris.Governance.Code
+  alias Uniris.Governance.Code.Proposal
 
   test "list_source_files/0 should list the files from the master branch" do
     files = Code.list_source_files()
     assert ".gitignore" in files
     assert "README.md" in files
   end
+
+  test "applicable_proposal? should succeed" do
+    changes = File.read!(Path.join(__DIR__, "0001-Propose-to-ignore-file.patch"))
+    assert Code.applicable_proposal?(%Proposal{changes: changes, address: "123"})
+  end
+
+  test "applicable_proposal? should fail" do
+    changes = """
+    this is not a proposal
+    """
+
+    assert !Code.applicable_proposal?(%Proposal{changes: changes, address: "123"})
+  end
+
+  doctest Code
 end
