ab7e8fd965a3bdc5b88ac55235644015a8e25a0f
Pavlo Martynenko <pavelmart@gmail.com>
2021-01-27 17:17:51 +0200

    Add CICD tests

--- config/config.exs
+++ config/config.exs
@@ -78,6 +78,8 @@ config :uniris, UnirisWeb.Endpoint,
     layout: {UnirisWeb.LayoutView, "live.html"}
   ]
 
+config :uniris, Uniris.Governance.Code.CICD, impl: Uniris.Governance.Code.CICD.Docker
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
