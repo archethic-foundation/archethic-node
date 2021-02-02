7a5851cf8148531c050c34b1ff2bf85eed54b927
Dmytro Nasyrov <dmytro@pharosproduction.com>
2021-02-02 18:17:59 +0200

    remove oracles sup from app sup

--- lib/uniris/application.ex
+++ lib/uniris/application.ex
@@ -41,8 +41,7 @@ defmodule Uniris.Application do
       SelfRepairSupervisor,
       WebSupervisor,
       Bootstrap,
-      {Task.Supervisor, name: Uniris.TaskSupervisor},
-      OraclesSupervisor
+      {Task.Supervisor, name: Uniris.TaskSupervisor}
     ]
 
     opts = [strategy: :rest_for_one, name: Uniris.Supervisor]
