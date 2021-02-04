7d5cab9c0e6aaf4619c1b5f006dd13e3432472b6
Pavlo Martynenko <pavelmart@gmail.com>
2021-02-04 16:13:18 +0200

    Add moduledoc to Governance.Code.CICD

--- lib/uniris/governance/code/CICD.ex
+++ lib/uniris/governance/code/CICD.ex
@@ -1,5 +1,37 @@
 defmodule Uniris.Governance.Code.CICD do
-  @moduledoc false
+  @moduledoc ~S"""
+  Provides CICD pipeline for `Uniris.Governance.Code.Proposal`
+
+  The evolution of uniris-node could be represented using following stages:
+
+    * Init - when source code is copiled into uniris-node (not covered here)
+    * CI - uniris-node is verifying a proposal and generating a release upgrade
+    * CD - uniris-node is forking a testnet to verify release upgrade
+
+  In each stage a transition from a source to a result could happen
+
+      | Stage | Source           | Transition   | Result         |
+      |-------+------------------+--------------+----------------|
+      | Init  | Code             | compile      | Release        |
+      | CI    | Code, Proposal   | run CI tests | CiLog, Upgrade |
+      | CD    | Release, Upgrade | run testnet  | TnLog, Release |
+
+  where
+    * Code - a source code of uniris-node
+    * Propsal - a code proposal transaction
+    * Release - a release of uniris-node
+    * Upgrade - an upgrade to a release of uniris-node
+    * CiLog - unit tests and type checker logs
+    * TnLog - logs retrieved from running testnet fork
+
+  ## CI
+  Given a `Code.Proposal` the `CICD.run_ci!/1` creates a log of application of
+  the `Proposal` to the `Code` and a release upgrade which is a delta between
+  previous release and new release.
+
+  ## CD
+  TODO describe what happens with release upgrade
+  """
 
   alias Uniris.Governance.Code.Proposal
 
