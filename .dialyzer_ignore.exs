255b562f9401f7d986a505a3aaa2494c7a29ca76
Samuel Manzanera <samuel@uniris.io>
2021-02-09 16:26:32 +0100

    Update dialyzer ignore

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,8 +1,8 @@
 [
   {"lib/uniris/self_repair/scheduler.ex", :call, 54},
   {"lib/uniris/self_repair/scheduler.ex", :call, 79},
-  {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 65},
-  {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 76},
+  {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 60},
+  {"lib/uniris/shared_secrets/node_renewal_scheduler.ex", :call, 71},
   {"lib/uniris_web/templates/code/proposal_chat.html.leex", :call, 19},
   {"lib/uniris/beacon_chain/subset/slot_consensus.ex"}
 ]
