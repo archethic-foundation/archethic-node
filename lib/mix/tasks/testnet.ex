5b213553c2cfded18e6a1c8f66105cff4097f66c
Pavlo Martynenko <pavelmart@gmail.com>
2021-03-12 21:04:03 +0200

    Optionally persist mutable data of a testnet

--- lib/mix/tasks/testnet.ex
+++ lib/mix/tasks/testnet.ex
@@ -4,6 +4,7 @@ defmodule Mix.Tasks.Uniris.Testnet do
   @network "testnet"
   @image "uniris-testnet"
   @output "docker-compose.json"
+  @persist false
   @run true
   # hardcoded
   @p2p_port 3002
@@ -17,6 +18,7 @@ defmodule Mix.Tasks.Uniris.Testnet do
     * `-o`, `--output` - use output file name, default "#{@output}"
     * `-n`, `--network` - use network name, default "#{@network}"
     * `-s`, `--subnet` - use subnet for the network, default "#{@subnet}"
+    * `-p`, `--persist` - mount data{n} to /opt/data, default "#{@persist}"
     * `-i`, `--image` - use image name for built container, default "#{@image}"
     * `--run/--no-run` - atuomatically run `docker-compose up`, default "#{@run}"
 
@@ -39,12 +41,13 @@ defmodule Mix.Tasks.Uniris.Testnet do
            strict: [
              help: :boolean,
              output: :string,
+             persist: :boolean,
              network: :string,
              subnet: :string,
              image: :string,
              run: :boolean
            ],
-           aliases: [h: :help, o: :output, n: :network, s: :subnet, i: :image]
+           aliases: [h: :help, o: :output, p: :persist, n: :network, s: :subnet, i: :image]
          ) do
       {_, []} ->
         Mix.shell().cmd("mix help #{Mix.Task.task_name(__MODULE__)}")
@@ -158,6 +161,7 @@ defmodule Mix.Tasks.Uniris.Testnet do
 
   defp from(seeds, opts) do
     image = Keyword.get(opts, :image, @image)
+    persist = Keyword.get(opts, :persist, @persist)
     network = Keyword.get(opts, :network, @network)
     subnet = Keyword.get(opts, :subnet, @subnet)
     base = Subnet.parse!(subnet)
@@ -167,7 +171,7 @@ defmodule Mix.Tasks.Uniris.Testnet do
       seeds
       |> Enum.scan([], fn seed, acc -> [seed | acc] end)
       |> Enum.with_index(1)
-      |> Enum.map(&to_node(&1, image, network, ip))
+      |> Enum.map(&to_node(&1, persist, image, network, ip))
       |> Enum.into(%{})
 
     networks = %{
@@ -179,22 +183,21 @@ defmodule Mix.Tasks.Uniris.Testnet do
     {services, networks}
   end
 
-  defp to_node({[seed], 1}, image, network, ip) do
+  defp to_node({[seed], 1}, persist?, image, network, ip) do
     {"node1",
      %{
        build: %{context: ".", args: %{skip_tests: 1, MIX_ENV: :dev}},
        image: image,
        environment: %{
-         "UNIRIS_MUT_DIR" => "/opt/data",
          "UNIRIS_CRYPTO_SEED" => seed,
          "UNIRIS_P2P_SEEDS" => seeder(ip.(1), seed)
        },
-       volumes: ["./data1:/opt/data"],
        networks: %{network => %{ipv4_address: ip.(1)}}
-     }}
+     }
+     |> mount(persist?, 1)}
   end
 
-  defp to_node({[seed | seeds], n}, image, network, ip) do
+  defp to_node({[seed | seeds], n}, persist?, image, network, ip) do
     seeders =
       seeds
       |> Enum.reverse()
@@ -207,17 +210,27 @@ defmodule Mix.Tasks.Uniris.Testnet do
        image: image,
        depends_on: ["node1"],
        environment: %{
-         "UNIRIS_MUT_DIR" => "/opt/data",
          "UNIRIS_CRYPTO_SEED" => seed,
          "UNIRIS_P2P_SEEDS" => seeders
        },
-       volumes: ["./data#{n}:/opt/data", "./scripts/wait-for-node.sh:/wait.sh:ro"],
+       volumes: ["./scripts/wait-for-node.sh:/wait.sh:ro"],
        command: ["/wait.sh", "http://node1:#{@web_port}/up", "./bin/uniris_node", "foreground"],
        networks: %{network => %{ipv4_address: ip.(n)}}
-     }}
+     }
+     |> mount(persist?, n)}
   end
 
   defp pubkey(seed), do: seed |> Uniris.Crypto.derive_keypair(0) |> elem(0) |> Base.encode16()
 
   defp seeder(ip, seed), do: "#{ip}:#{@p2p_port}:#{pubkey(seed)}:tcp"
+
+  defp mount(m, false, _), do: m
+
+  defp mount(m, true, n) do
+    mount = ["./data#{n}:/opt/data"]
+
+    m
+    |> Map.update(:volumes, mount, fn v -> mount ++ v end)
+    |> Map.update!(:environment, fn e -> Map.put(e, "UNIRIS_MUT_DIR", "/opt/data") end)
+  end
 end
