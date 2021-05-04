5e7e0d648686630f77b9f285712f0c73f357a252
Samuel <samuel@uniris.io>
2021-05-05 00:21:26 +0200

    Improve the self repair notifier when a new authorize noded is detected

--- lib/uniris/self_repair/notifier.ex
+++ lib/uniris/self_repair/notifier.ex
@@ -16,6 +16,8 @@ defmodule Uniris.SelfRepair.Notifier do
   alias Uniris.TransactionChain
   alias Uniris.TransactionChain.Transaction
 
+  alias Uniris.Utils
+
   def start_link(args \\ []) do
     GenServer.start_link(__MODULE__, args)
   end
@@ -26,46 +28,70 @@ defmodule Uniris.SelfRepair.Notifier do
   end
 
   def handle_info(
-        {:node_update, node = %Node{available?: true, first_public_key: first_public_key}},
+        {:node_update,
+         %Node{available?: true, authorized?: true, first_public_key: node_key}},
         state
       ) do
-    available_nodes = P2P.list_nodes(availability: :global)
-    node_key = Crypto.node_public_key(0)
+    current_node_public_key = Crypto.node_public_key(0)
 
+    if node_key == current_node_public_key do
+      {:noreply, state}
+    else
+      node_list = P2P.authorized_nodes() |> Enum.reject(&(&1.first_public_key == node_key))
+
+      node_key
+      |> get_transactions_to_sync(node_list)
+      |> forward_transactions(node_list, current_node_public_key)
+
+      {:noreply, state}
+    end
+  end
+
+  def handle_info(_, state) do
+    {:noreply, state}
+  end
+
+  defp get_transactions_to_sync(node_public_key, node_list) do
     TransactionChain.list_all([:address, :type])
     |> Stream.map(fn %Transaction{address: address, type: type} ->
-      {address, type, Replication.chain_storage_nodes(address, type, available_nodes)}
+      {address, type, Replication.chain_storage_nodes_with_type(address, type, node_list)}
     end)
     |> Stream.filter(fn {address, type, nodes} ->
       Replication.chain_storage_node?(
         address,
         type,
-        first_public_key,
+        node_public_key,
         nodes
       )
     end)
-    |> Stream.each(fn {address, type, _nodes} ->
-      previous_storage_nodes =
-        Replication.chain_storage_nodes(
-          address,
-          type,
-          Enum.reject(available_nodes, &(&1.first_public_key == first_public_key))
-        )
-
-      case previous_storage_nodes do
-        [] ->
-          :ok
-
-        [%Node{first_public_key: first_public_key} | _] when first_public_key == node_key ->
-          {:ok, tx} = TransactionChain.get_transaction(address)
-          P2P.send_message!(node, %ReplicateTransaction{transaction: tx})
-      end
-    end)
-
-    {:noreply, state}
   end
 
-  def handle_info(_, state) do
-    {:noreply, state}
+  defp forward_transactions(
+         transactions,
+         node_list,
+         current_node_public_key
+       ) do
+    transactions
+    |> Stream.each(fn {address, type, _nodes} ->
+      previous_storage_nodes =
+        Replication.chain_storage_nodes_with_type(
+          address,
+          type,
+          node_list
+        )
+
+      next_storage_nodes =
+        Replication.chain_storage_nodes_with_type(
+          address,
+          type
+        )
+
+      with true <- Utils.key_in_node_list?(previous_storage_nodes, current_node_public_key),
+           {:ok, tx} <- TransactionChain.get_transaction(address) do
+        # TODO: improve to request if the node has the transaction already
+        P2P.broadcast_message(next_storage_nodes, %ReplicateTransaction{transaction: tx})
+      end
+    end)
+    |> Stream.run()
   end
 end
