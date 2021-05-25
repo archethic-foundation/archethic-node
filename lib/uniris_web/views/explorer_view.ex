83ed2ee55c9fcb99bc6611ddecffcda852362cfa
Samuel <samuel@uniris.io>
2021-05-25 10:45:30 +0200

    Add a function to format and display transaction content in the explorer

--- lib/uniris_web/views/explorer_view.ex
+++ lib/uniris_web/views/explorer_view.ex
@@ -3,6 +3,13 @@ defmodule UnirisWeb.ExplorerView do
 
   use UnirisWeb, :view
 
+  alias Uniris.BeaconChain.Slot
+  alias Uniris.BeaconChain.Slot.EndOfNodeSync
+  alias Uniris.BeaconChain.Slot.TransactionSummary
+  alias Uniris.BeaconChain.Summary
+
+  alias Uniris.Utils
+
   alias Phoenix.Naming
 
   def roles_to_string(roles) do
@@ -20,4 +27,86 @@ defmodule UnirisWeb.ExplorerView do
 
     content_tag("span", formatted_type, class: "tag is-warning is-light")
   end
+
+  def format_transaction_content(:beacon, content) do
+    {%Slot{
+       subset: subset,
+       transaction_summaries: transaction_summaries,
+       end_of_node_synchronizations: end_of_sync,
+       p2p_view: %{availabilities: availabilities}
+     }, _} = Slot.deserialize(content)
+
+    transaction_summaries_stringified =
+      Enum.map(transaction_summaries, fn %TransactionSummary{
+                                           address: address,
+                                           timestamp: timestamp,
+                                           type: type
+                                         } ->
+        "#{DateTime.to_string(timestamp)} - #{Base.encode16(address)} - #{
+          format_transaction_type(type)
+        }"
+      end)
+      |> Enum.join("\n")
+
+    end_of_sync_stringified =
+      Enum.map(end_of_sync, fn %EndOfNodeSync{public_key: node_public_key, timestamp: timestamp} ->
+        "#{DateTime.to_string(timestamp)} - #{Base.encode16(node_public_key)}"
+      end)
+      |> Enum.join(",")
+
+    """
+    Subset: #{Base.encode16(subset)}
+
+    Transactions:
+    #{transaction_summaries_stringified}
+
+    New node synchronizations
+    #{end_of_sync_stringified}
+
+    P2P node availabilites: #{Utils.bitstring_to_integer_list(availabilities) |> Enum.join(",")}
+    """
+  end
+
+  def format_transaction_content(:beacon_summary, content) do
+    {%Summary{
+       subset: subset,
+       transaction_summaries: transaction_summaries,
+       end_of_node_synchronizations: end_of_sync,
+       node_availabilities: node_availabilities
+     }, _} = Summary.deserialize(content)
+
+    transaction_summaries_stringified =
+      Enum.map(transaction_summaries, fn %TransactionSummary{
+                                           address: address,
+                                           timestamp: timestamp,
+                                           type: type
+                                         } ->
+        "#{DateTime.to_string(timestamp)} - #{Base.encode16(address)} - #{
+          format_transaction_type(type)
+        }"
+      end)
+      |> Enum.join("\n")
+
+    end_of_sync_stringified =
+      Enum.map(end_of_sync, fn %EndOfNodeSync{public_key: node_public_key, timestamp: timestamp} ->
+        "#{DateTime.to_string(timestamp)} - #{Base.encode16(node_public_key)}"
+      end)
+      |> Enum.join(",")
+
+    """
+    Subset: #{Base.encode16(subset)}
+
+    Transactions:
+    #{transaction_summaries_stringified}
+
+    New node synchronizations
+    #{end_of_sync_stringified}
+
+    P2P node availabilites: #{
+      Utils.bitstring_to_integer_list(node_availabilities) |> Enum.join(",")
+    }
+    """
+  end
+
+  def format_transaction_content(_, content), do: content
 end
