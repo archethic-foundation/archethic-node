ec6fdc337f3ae4c237d963574975e4ba496b5f36
Samuel <samuel@uniris.io>
2021-07-15 09:59:36 +0200

    Support dynamic host for Mix task of cleaning database

--- lib/mix/tasks/clean_db.ex
+++ lib/mix/tasks/clean_db.ex
@@ -3,10 +3,19 @@ defmodule Mix.Tasks.ArchEthic.CleanDb do
 
   use Mix.Task
 
-  def run(_) do
+  def run(arg) do
+    host =
+      case(OptionParser.parse!(arg, strict: [host: :string])) do
+        {[], []} ->
+          "127.0.0.1:9042"
+
+        {[host: host], []} ->
+          host
+      end
+
     {:ok, _started} = Application.ensure_all_started(:xandra)
-    {:ok, conn} = Xandra.start_link()
+    {:ok, conn} = Xandra.start_link(nodes: [host])
     Xandra.execute!(conn, "DROP KEYSPACE IF EXISTS archethic;")
-    IO.puts("Database dropped")
+    IO.puts("Database #{host} dropped")
   end
 end
