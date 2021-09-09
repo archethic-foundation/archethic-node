2b8933ef5511d717ac0602b58d91f49c6175c92f
imnik11 <nitesh4y2001@gmail.com>
2021-09-09 18:05:33 +0530

    Fixed title of table with current_date_page-1 & mix.exs version to 0.12.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArchEthic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "0.11.1",
+      version: "0.12.0",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
