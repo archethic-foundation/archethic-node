81dc03c59a0859cda278d035e28a89021ab462f1
Samuel <samuel@uniris.io>
2021-09-16 15:48:32 +0200

    Set 422 instead of 403 error code for invalid tx
    Closes #85

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArchEthic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "0.11.3",
+      version: "0.11.4",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
