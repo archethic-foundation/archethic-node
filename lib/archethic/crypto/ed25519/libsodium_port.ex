3e6fadc26223637e61ff71c424f246ba2bf10990
Samuel <42943690+samuel-uniris@users.noreply.github.com>
2021-09-16 17:31:12 +0200

    Use PortHandler for the libsodium port (#87)

--- lib/archethic/crypto/ed25519/libsodium_port.ex
+++ lib/archethic/crypto/ed25519/libsodium_port.ex
@@ -4,6 +4,10 @@ defmodule ArchEthic.Crypto.Ed25519.LibSodiumPort do
 
   require Logger
 
+  @table_name :libsodium_port
+
+  alias ArchEthic.Utils.PortHandler
+
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, opts, name: __MODULE__)
   end
@@ -13,7 +17,8 @@ defmodule ArchEthic.Crypto.Ed25519.LibSodiumPort do
   """
   @spec convert_public_key_to_x25519(binary()) :: {:ok, binary()} | {:error, String.t()}
   def convert_public_key_to_x25519(<<public_key::binary-32>>) do
-    GenServer.call(__MODULE__, {:convert_public_key, public_key})
+    [{_, port_handler}] = :ets.lookup(@table_name, :port)
+    PortHandler.request(port_handler, 1, public_key)
   end
 
   @doc """
@@ -21,57 +26,18 @@ defmodule ArchEthic.Crypto.Ed25519.LibSodiumPort do
   """
   @spec convert_secret_key_to_x25519(binary()) :: {:ok, binary()} | {:error, String.t()}
   def convert_secret_key_to_x25519(<<secret_key::binary-64>>) do
-    GenServer.call(__MODULE__, {:convert_secret_key, secret_key})
+    [{_, port_handler}] = :ets.lookup(@table_name, :port)
+    PortHandler.request(port_handler, 2, secret_key)
   end
 
   def init(_opts) do
     libsodium = Application.app_dir(:archethic, "/priv/c_dist/libsodium_port")
 
-    port =
-      Port.open({:spawn_executable, libsodium}, [
-        :binary,
-        :exit_status,
-        {:packet, 4}
-      ])
+    {:ok, port_handler} = PortHandler.start_link(program: libsodium)
 
-    {:ok, %{port: port, next_id: 1, awaiting: %{}}}
-  end
+    :ets.new(@table_name, [:set, :named_table, read_concurrency: true])
+    :ets.insert(@table_name, {:port, port_handler})
 
-  def handle_call({:convert_public_key, public_key}, from, state) do
-    {id, state} = send_request(state, 1, public_key)
-    {:noreply, %{state | awaiting: Map.put(state.awaiting, id, from)}}
-  end
-
-  def handle_call({:convert_secret_key, secret_key}, from, state) do
-    {id, state} = send_request(state, 2, secret_key)
-    {:noreply, %{state | awaiting: Map.put(state.awaiting, id, from)}}
-  end
-
-  def handle_info({_port, {:data, <<req_id::32, response::binary>>} = _data}, state) do
-    case state.awaiting[req_id] do
-      nil ->
-        {:noreply, state}
-
-      caller ->
-        case response do
-          <<0::8, error_message::binary>> ->
-            GenServer.reply(caller, {:error, error_message})
-
-          <<1::8, data::binary>> ->
-            GenServer.reply(caller, {:ok, data})
-        end
-
-        {:noreply, %{state | awaiting: Map.delete(state.awaiting, req_id)}}
-    end
-  end
-
-  def handle_info({_port, {:exit_status, status}}, _state) do
-    :erlang.error({:port_exit, status})
-  end
-
-  defp send_request(state, request_type, data) do
-    id = state.next_id
-    Port.command(state.port, <<id::32, request_type::8, data::binary>>)
-    {id, %{state | next_id: id + 1}}
+    {:ok, %{}}
   end
 end
