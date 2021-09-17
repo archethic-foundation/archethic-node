91d6111aab8ba6b54c24d2c41332aaaf5d927bbd
Samuel <samuel@uniris.io>
2021-09-17 15:11:43 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ArchEthic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "0.11.5",
+      version: "0.11.6",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
