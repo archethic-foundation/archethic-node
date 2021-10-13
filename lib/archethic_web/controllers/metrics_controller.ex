730ad21d95248cad59fd43c4f8de8e044e8be7fa
ambareesha7 <ambareesha7@gmail.com>
2021-10-13 19:17:30 +0530

    changed the functions

--- lib/archethic_web/controllers/metrics_controller.ex
+++ lib/archethic_web/controllers/metrics_controller.ex
@@ -10,28 +10,62 @@ defmodule ArchEthicWeb.MetricsController do
     |> send_resp(200, metrics)
   end
 
-  def parse(content) do
-    [_, metric_name, labels, value] = Regex.run(~r/(.*){(.*)}(.*)/, content)
-
-    labels_data =
-      labels
-      |> remove_quotes(~r/"/)
-      |> String.split(",")
-      |> Enum.reduce(%{}, fn match, acc ->
-        [key, val] = String.split(match, "=")
-        Map.put(acc, key, val)
-      end)
-
-    bucket_map = Map.merge(labels_data, %{"value" => value})
-    Map.put_new(%{}, metric_name, bucket_map)
+  def parse_help(content) do
+    [_, _, name] = Regex.run(~r/(.*\s)(.*)/, content)
+    %{"help" => name}
+    # Jason.encode!(map)
   end
 
-  def parse_other(content) do
-    [_, metric_name, value] = Regex.run(~r/(.*\s)(.*)/, content)
-    Map.put_new(%{}, metric_name, %{"value" => value})
+  def parse_type(content) do
+    [_, _, _, type] = Regex.run(~r/(.*\s)(.*\s)(.*)/, content)
+    %{"type" => type}
+    # Jason.encode!(map)
   end
 
-  def remove_quotes(str, regex, replacement \\ "") do
+  def parse_with_curly(content) do
+    [_, _metric_name, lab, value] = Regex.run(~r/(.*){(.*)}(.*)/, content)
+    [bucket, label, handler] = data_within_curl(lab)
+    bucket_val = %{bucket => value |> remove_string(~r/ /, "")}
+    buckets = %{"buckets" => bucket_val}
+    labels = %{"labels" => %{"method" => label, "handler" => handler}}
+    {:ok, buckets, labels}
+  end
+
+  def parse_with_curly_no_label(content) do
+    [_, _metric_name, lab, value] = Regex.run(~r/(.*){(.*)}(.*)/, content)
+    [bucket] = data_within_curl(lab)
+    bucket_val = %{bucket => value |> remove_string(~r/ /, "")}
+    %{"buckets" => bucket_val}
+  end
+
+  def parse_sum_count_with_label(content) do
+    [_, name, _lab, value] = Regex.run(~r/_(sum|count){(.*)}(.*)/, content)
+    %{name => value |> remove_string(~r/ /, "")}
+  end
+
+  def parse_sum_count_no_label_no_curly(content) do
+    [_, name, value] = Regex.run(~r/_(sum|count)(.*)/, content)
+    %{name => value |> remove_string(~r/ /, "")}
+  end
+
+  defp data_within_curl(labels) do
+    labels
+    |> remove_string(~r/"/)
+    |> String.split(",")
+    |> Enum.reduce([], fn match, acc ->
+      [key, val] = String.split(match, "=")
+      [key | [val | acc]] |> Enum.reject(fn le -> le == "le" end)
+    end)
+  end
+
+  def remove_string(str, regex, replacement \\ "") do
     Regex.replace(regex, str, replacement)
   end
+
+  def check_for_help?(content), do: String.match?(content, ~r/HELP/)
+  def check_for_type?(content), do: String.match?(content, ~r/TYPE/)
+  def check_for_curly?(content), do: String.match?(content, ~r/{|}/)
+  def check_for_no_curly?(content), do: !String.match?(content, ~r/{|}|(HELP)|(TYPE)/)
+  def check_for_sum_count_with_curly?(content), do: String.match?(content, ~r/_sum{|_count{/)
+  def check_for_le_with_curly?(content), do: String.match?(content, ~r/le/)
 end
