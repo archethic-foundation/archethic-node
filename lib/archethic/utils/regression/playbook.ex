ea97ed9e77ed9cbb38c2d7fdbfe07985e93562a4
Samuel <samuel@uniris.io>
2021-12-03 14:39:45 +0100

    Provide better abstraction for the Playbook behavior

--- lib/archethic/utils/regression/playbook.ex
+++ lib/archethic/utils/regression/playbook.ex
@@ -3,8 +3,205 @@ defmodule ArchEthic.Utils.Regression.Playbook do
   Playbook is executed on a testnet to verify correctness of the testnet.
   """
 
-  @doc """
-  Run playbook.
-  """
+  alias ArchEthic.Crypto
+
+  alias ArchEthic.TransactionChain.Transaction
+  alias ArchEthic.TransactionChain.TransactionData
+  alias ArchEthic.TransactionChain.TransactionData.Ledger
+  alias ArchEthic.TransactionChain.TransactionData.NFTLedger
+  alias ArchEthic.TransactionChain.TransactionData.NFTLedger.Transfer, as: NFTTransfer
+  alias ArchEthic.TransactionChain.TransactionData.Ownership
+  alias ArchEthic.TransactionChain.TransactionData.UCOLedger
+  alias ArchEthic.TransactionChain.TransactionData.UCOLedger.Transfer, as: UCOTransfer
+
+  alias ArchEthic.Utils.WebClient
+
   @callback play!([String.t()], Keyword.t()) :: :ok
+
+  @genesis_origin_private_key "01009280BDB84B8F8AEDBA205FE3552689964A5626EE2C60AA10E3BF22A91A036009"
+                              |> Base.decode16!()
+
+  @faucet_seed Application.compile_env(:archethic, [ArchEthicWeb.FaucetController, :seed])
+
+  defmacro __using__(_opts \\ []) do
+    quote do
+      @behaviour ArchEthic.Utils.Regression.Playbook
+    end
+  end
+
+  def send_funds_to(recipient_address, host, port, amount \\ 10) do
+    send_transaction(
+      @faucet_seed,
+      :transfer,
+      %TransactionData{
+        ledger: %Ledger{
+          uco: %UCOLedger{
+            transfers: [
+              %UCOTransfer{
+                to: recipient_address,
+                amount: amount * 100_000_000
+              }
+            ]
+          }
+        }
+      },
+      host,
+      port,
+      :ed25519
+    )
+  end
+
+  def send_transaction(
+        transaction_seed,
+        tx_type,
+        transaction_data = %TransactionData{},
+        host,
+        port,
+        curve \\ Crypto.default_curve()
+      ) do
+    chain_length = get_chain_size(transaction_seed, curve, host, port)
+
+    {previous_public_key, previous_private_key} =
+      Crypto.derive_keypair(transaction_seed, chain_length, curve)
+
+    {next_public_key, _} = Crypto.derive_keypair(transaction_seed, chain_length + 1, curve)
+
+    tx =
+      %Transaction{
+        address: Crypto.hash(next_public_key),
+        type: tx_type,
+        data: transaction_data,
+        previous_public_key: previous_public_key
+      }
+      |> Transaction.previous_sign_transaction(previous_private_key)
+      |> Transaction.origin_sign_transaction(@genesis_origin_private_key)
+
+    true =
+      Crypto.verify?(
+        tx.previous_signature,
+        Transaction.extract_for_previous_signature(tx) |> Transaction.serialize(),
+        tx.previous_public_key
+      )
+
+    case WebClient.with_connection(
+           host,
+           port,
+           &WebClient.json(&1, "/api/transaction", tx_to_json(tx))
+         ) do
+      {:ok, %{"status" => "ok"}} ->
+        {:ok, tx.address}
+
+      _ ->
+        :error
+    end
+  end
+
+  defp tx_to_json(%Transaction{
+         version: version,
+         address: address,
+         type: type,
+         data: %TransactionData{
+           ledger: %Ledger{
+             uco: %UCOLedger{transfers: uco_transfers},
+             nft: %NFTLedger{transfers: nft_transfers}
+           },
+           code: code,
+           content: content,
+           recipients: recipients,
+           ownerships: ownerships
+         },
+         previous_public_key: previous_public_key,
+         previous_signature: previous_signature,
+         origin_signature: origin_signature
+       }) do
+    %{
+      "version" => version,
+      "address" => Base.encode16(address),
+      "type" => Atom.to_string(type),
+      "previousPublicKey" => Base.encode16(previous_public_key),
+      "previousSignature" => Base.encode16(previous_signature),
+      "originSignature" => Base.encode16(origin_signature),
+      "data" => %{
+        "ledger" => %{
+          "uco" => %{
+            "transfers" =>
+              Enum.map(uco_transfers, fn %UCOTransfer{to: to, amount: amount} ->
+                %{"to" => Base.encode16(to), "amount" => amount}
+              end)
+          },
+          "nft" => %{
+            "transfers" =>
+              Enum.map(nft_transfers, fn %NFTTransfer{
+                                           to: to,
+                                           amount: amount,
+                                           nft: nft_address
+                                         } ->
+                %{"to" => Base.encode16(to), "amount" => amount, "nft" => nft_address}
+              end)
+          }
+        },
+        "code" => code,
+        "content" => content,
+        "recipients" => Enum.map(recipients, &Base.encode16(&1)),
+        "ownerships" =>
+          Enum.map(ownerships, fn %Ownership{
+                                    secret: secret,
+                                    authorized_keys: authorized_keys
+                                  } ->
+            %{
+              "secret" => Base.encode16(secret),
+              "authorizedKeys" =>
+                Enum.map(authorized_keys, fn {public_key, encrypted_secret_key} ->
+                  %{
+                    "publicKey" => Base.encode16(public_key),
+                    "encryptedSecretKey" => Base.encode16(encrypted_secret_key)
+                  }
+                end)
+            }
+          end)
+      }
+    }
+  end
+
+  def get_chain_size(seed, curve, host, port) do
+    genesis_address =
+      seed
+      |> Crypto.derive_keypair(0, curve)
+      |> elem(0)
+      |> Crypto.hash()
+
+    query =
+      ~s|query {last_transaction(address: "#{Base.encode16(genesis_address)}"){ chainLength }}|
+
+    case WebClient.with_connection(host, port, &WebClient.query(&1, query)) do
+      {:ok, %{"errors" => [%{"message" => "transaction_not_exists"}]}} ->
+        0
+
+      {:ok, %{"data" => %{"last_transaction" => %{"chainLength" => chain_length}}}} ->
+        chain_length
+    end
+  end
+
+  def get_uco_balance(address, host, port) do
+    query = ~s|query {lastTransaction(address: "#{Base.encode16(address)}"){ balance { uco }}}|
+
+    case WebClient.with_connection(host, port, &WebClient.query(&1, query)) do
+      {:ok, %{"data" => %{"lastTransaction" => %{"balance" => %{"uco" => uco}}}}} ->
+        uco
+
+      {:ok, %{"errors" => [%{"message" => "transaction_not_exists"}]}} ->
+        balance_query = ~s| query { balance(address: "#{Base.encode16(address)}") { uco } } |
+
+        {:ok,
+         %{
+           "data" => %{
+             "balance" => %{
+               "uco" => uco
+             }
+           }
+         }} = WebClient.with_connection(host, port, &WebClient.query(&1, balance_query))
+
+        uco
+    end
+  end
 end
--- lib/archethic/utils/regression/playbooks/uco.ex
+++ lib/archethic/utils/regression/playbooks/uco.ex
@@ -9,21 +9,14 @@ defmodule ArchEthic.Utils.Regression.Playbook.UCO do
 
   alias ArchEthic.Crypto
 
-  alias ArchEthic.TransactionChain.Transaction
   alias ArchEthic.TransactionChain.TransactionData
   alias ArchEthic.TransactionChain.TransactionData.Ledger
   alias ArchEthic.TransactionChain.TransactionData.UCOLedger
   alias ArchEthic.TransactionChain.TransactionData.UCOLedger.Transfer, as: UCOTransfer
 
   alias ArchEthic.Utils.Regression.Playbook
-  alias ArchEthic.Utils.WebClient
 
-  @behaviour Playbook
-
-  @genesis_origin_private_key "01009280BDB84B8F8AEDBA205FE3552689964A5626EE2C60AA10E3BF22A91A036009"
-                              |> Base.decode16!()
-
-  @faucet_seed Application.compile_env(:archethic, [ArchEthicWeb.FaucetController, :seed])
+  use Playbook
 
   def play!(nodes, opts) do
     Logger.info("Play UCO transactions on #{inspect(nodes)} with #{inspect(opts)}")
@@ -46,21 +39,14 @@ defmodule ArchEthic.Utils.Regression.Playbook.UCO do
       "Genesis pool allocation owner is sending 10 UCO to #{Base.encode16(recipient_address)}"
     )
 
-    prev_balance = get_uco_balance(recipient_address, host, port)
+    prev_balance = Playbook.get_uco_balance(recipient_address, host, port)
 
-    {:ok, address} =
-      send_transfer_transaction(
-        @faucet_seed,
-        [%{to: recipient_address, amount: 10 * 100_000_000}],
-        host,
-        port,
-        :ed25519
-      )
+    {:ok, address} = Playbook.send_funds_to(recipient_address, host, port)
 
     Logger.info("Transaction #{Base.encode16(address)} submitted")
 
     # Ensure the recipient got the 10.0 UCO
-    new_balance = get_uco_balance(recipient_address, host, port)
+    new_balance = Playbook.get_uco_balance(recipient_address, host, port)
     true = new_balance == prev_balance + 10.0
 
     Logger.info("#{Base.encode16(recipient_address)} received 10 UCO")
@@ -72,22 +58,33 @@ defmodule ArchEthic.Utils.Regression.Playbook.UCO do
     )
 
     {:ok, address} =
-      send_transfer_transaction(
+      Playbook.send_transaction(
         "recipient_1",
-        [%{to: new_recipient_address, amount: 5 * 100_000_000}],
+        :transfer,
+        %TransactionData{
+          ledger: %Ledger{
+            uco: %UCOLedger{
+              transfers: [
+                %UCOTransfer{
+                  to: new_recipient_address,
+                  amount: 5 * 100_000_000
+                }
+              ]
+            }
+          }
+        },
         host,
-        port,
-        :ed25519
+        port
       )
 
     Logger.info("Transaction #{Base.encode16(address)} submitted")
 
     # Ensure the second recipient received the 5.0 UCO
-    5.0 = get_uco_balance(new_recipient_address, host, port)
+    5.0 = Playbook.get_uco_balance(new_recipient_address, host, port)
     Logger.info("#{Base.encode16(new_recipient_address)} received 5.0 UCO")
 
     # Ensure the first recipient amount have decreased
-    recipient_balance2 = get_uco_balance(recipient_address, host, port)
+    recipient_balance2 = Playbook.get_uco_balance(recipient_address, host, port)
     # 5.0 - transaction fee
     true = recipient_balance2 <= new_balance - 5.0
     Logger.info("#{Base.encode16(recipient_address)} now got #{recipient_balance2} UCO")
@@ -98,111 +95,25 @@ defmodule ArchEthic.Utils.Regression.Playbook.UCO do
     recipient_address = <<0::8, :crypto.strong_rand_bytes(32)::binary>>
 
     :error =
-      send_transfer_transaction(
+      Playbook.send_transaction(
         from_seed,
-        [%{to: recipient_address, amount: 10 * 100_000_000}],
-        host,
-        port,
-        :secp256r1
-      )
-
-    Logger.info("Transaction with insufficient funds is rejected")
-  end
-
-  defp send_transfer_transaction(transaction_seed, transfers, host, port, curve) do
-    chain_length = get_chain_size(transaction_seed, curve, host, port)
-
-    {previous_public_key, previous_private_key} =
-      Crypto.derive_keypair(transaction_seed, chain_length, curve)
-
-    {next_public_key, _} = Crypto.derive_keypair(transaction_seed, chain_length + 1, curve)
-
-    tx =
-      %Transaction{
-        address: Crypto.hash(next_public_key),
-        type: :transfer,
-        data: %TransactionData{
+        :transfer,
+        %TransactionData{
           ledger: %Ledger{
             uco: %UCOLedger{
-              transfers:
-                Enum.map(transfers, fn %{to: to, amount: amount} ->
-                  %UCOTransfer{to: to, amount: amount}
-                end)
+              transfers: [
+                %UCOTransfer{
+                  to: recipient_address,
+                  amount: 10 * 100_000_000
+                }
+              ]
             }
           }
         },
-        previous_public_key: previous_public_key
-      }
-      |> Transaction.previous_sign_transaction(previous_private_key)
-      |> Transaction.origin_sign_transaction(@genesis_origin_private_key)
+        host,
+        port
+      )
 
-    tx_json = %{
-      "version" => tx.version,
-      "address" => Base.encode16(tx.address),
-      "type" => "transfer",
-      "previousPublicKey" => Base.encode16(tx.previous_public_key),
-      "previousSignature" => Base.encode16(tx.previous_signature),
-      "originSignature" => Base.encode16(tx.origin_signature),
-      "data" => %{
-        "ledger" => %{
-          "uco" => %{
-            "transfers" =>
-              Enum.map(transfers, fn %{to: to, amount: amount} ->
-                %{"to" => Base.encode16(to), "amount" => amount}
-              end)
-          }
-        }
-      }
-    }
-
-    case WebClient.with_connection(host, port, &WebClient.json(&1, "/api/transaction", tx_json)) do
-      {:ok, %{"status" => "ok"}} ->
-        {:ok, tx.address}
-
-      _ ->
-        :error
-    end
-  end
-
-  defp get_chain_size(seed, curve, host, port) do
-    genesis_address =
-      seed
-      |> Crypto.derive_keypair(0, curve)
-      |> elem(0)
-      |> Crypto.hash()
-
-    query =
-      ~s|query {last_transaction(address: "#{Base.encode16(genesis_address)}"){ chainLength }}|
-
-    case WebClient.with_connection(host, port, &WebClient.query(&1, query)) do
-      {:ok, %{"errors" => [%{"message" => "transaction_not_exists"}]}} ->
-        0
-
-      {:ok, %{"data" => %{"last_transaction" => %{"chainLength" => chain_length}}}} ->
-        chain_length
-    end
-  end
-
-  defp get_uco_balance(address, host, port) do
-    query = ~s|query {lastTransaction(address: "#{Base.encode16(address)}"){ balance { uco }}}|
-
-    case WebClient.with_connection(host, port, &WebClient.query(&1, query)) do
-      {:ok, %{"data" => %{"lastTransaction" => %{"balance" => %{"uco" => uco}}}}} ->
-        uco
-
-      {:ok, %{"errors" => [%{"message" => "transaction_not_exists"}]}} ->
-        balance_query = ~s| query { balance(address: "#{Base.encode16(address)}") { uco } } |
-
-        {:ok,
-         %{
-           "data" => %{
-             "balance" => %{
-               "uco" => uco
-             }
-           }
-         }} = WebClient.with_connection(host, port, &WebClient.query(&1, balance_query))
-
-        uco
-    end
+    Logger.info("Transaction with insufficient funds is rejected")
   end
 end
