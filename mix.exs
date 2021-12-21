3a5cb1a822d620e680802d96cbd3e7e959e092d9
Samuel <samuel@uniris.io>
2021-12-21 12:00:40 +0100

    Reorder dependencies

--- mix.exs
+++ mix.exs
@@ -33,49 +33,64 @@ defmodule ArchEthic.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mint, "~> 1.0"},
-      {:benchee, "~> 1.0"},
-      {:xandra, "~> 0.11"},
+      # Web
       {:phoenix, ">= 1.5.4"},
-      {:phoenix_pubsub, "~> 2.0"},
       {:phoenix_html, "~> 2.14"},
       {:phoenix_live_view, "~> 0.14.0"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.3"},
       {:absinthe, "~> 1.5.0"},
       {:absinthe_plug, "~> 1.5"},
       {:absinthe_phoenix, "~> 2.0"},
       {:cors_plug, "~> 1.5"},
-      {:phoenix_live_dashboard, "~> 0.2.7"},
+      {:mint, "~> 1.0"},
+      {:ecto, "~> 3.5"},
+
+      # Dev
+      {:benchee, "~> 1.0"},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
-      {:observer_cli, "~> 1.5"},
-      {:distillery, github: "bitwalker/distillery", ref: "6700edb"},
-      {:crontab, "~> 1.1"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:git_hooks, "~> 0.4.0", runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:elixir_make, "~> 0.6.0", runtime: false},
       {:dialyxir, "~> 1.0", runtime: false},
+      # {:broadway_dashboard, "~> 0.2.0", only: :dev},
+
+      # Test
       {:mox, "~> 0.5.2", only: [:test]},
       {:stream_data, "~> 0.5.0", only: [:test], runtime: false},
-      {:elixir_make, "~> 0.6.0", runtime: false},
-      {:earmark, "~> 1.4"},
-      {:sizeable, "~> 1.0"},
-      {:exjsonpath, "~> 0.9.0"},
-      {:rand_compat, "~> 0.0.3"},
+
+      # P2P
+      {:ranch, "~> 2.1", override: true},
+      {:connection, "~> 1.1"},
+
+      # DB
+      {:xandra, "~> 0.11"},
+
+      # Net
       {:inet_ext, "~> 1.0"},
       {:inet_cidr, "~> 1.1", hex: :erl_cidr, override: true},
+
+      # Monitoring
+      {:observer_cli, "~> 1.5"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_metrics_prometheus_core, "~> 1.0.0"},
       {:telemetry_poller, "~> 0.5.1"},
-      {:ecto, "~> 3.5"},
+      {:phoenix_live_dashboard, "~> 0.2.7"},
+
+      # Utils
+      {:crontab, "~> 1.1"},
+      {:earmark, "~> 1.4"},
+      {:sizeable, "~> 1.0"},
+      {:distillery, github: "bitwalker/distillery", ref: "6700edb"},
+      {:exjsonpath, "~> 0.9.0"},
+      {:rand_compat, "~> 0.0.3"},
       {:gen_state_machine, "~> 3.0"},
       {:retry, "~> 0.14.1"},
       {:gen_stage, "~> 1.1"},
-      {:knigge, "~> 1.4"},
       {:flow, "~> 1.0"},
-      {:ranch, "~> 2.1", override: true},
       {:broadway, "~> 1.0"},
-      # {:broadway_dashboard, "~> 0.2.0", only: :dev},
-      {:connection, "~> 1.1"}
+      {:knigge, "~> 1.4"}
     ]
   end
 end
