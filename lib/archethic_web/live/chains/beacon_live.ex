2dda9c9e5ff6d5bd7ab2cc77d2c0c0b2e4c6f0d4
Imnik11 <nitesh4y2001@gmail.com>
2022-01-21 00:56:34 +0530

    Added live streaming transaction for beacon explorer

--- lib/archethic_web/live/chains/beacon_live.ex
+++ lib/archethic_web/live/chains/beacon_live.ex
@@ -24,41 +24,25 @@ defmodule ArchEthicWeb.BeaconChainLive do
   require Logger
 
   defp list_transaction_by_date(date = %DateTime{}) do
-    Enum.reduce(BeaconChain.list_subsets(), %{}, fn subset, acc ->
+    Enum.map(BeaconChain.list_subsets(), fn subset ->
       b_address = Crypto.derive_beacon_chain_address(subset, date, true)
       node_list = P2P.authorized_nodes()
       nodes = Election.beacon_storage_nodes(subset, date, node_list)
+      %Node{network_patch: patch} = P2P.get_node_info()
 
-      Enum.reduce(nodes, acc, fn node, acc ->
-        Map.update(acc, node, [b_address], &[b_address | &1])
-      end)
+      {b_address, nodes, patch}
     end)
-    |> Stream.transform([], fn
-      {_, []}, acc ->
-        {[], acc}
-
-      {node, addresses}, acc ->
-        addresses_to_fetch = Enum.reject(addresses, &(&1 in acc))
-
-        case P2P.send_message(node, %GetBeaconSummaries{addresses: addresses_to_fetch}) do
-          {:ok, %BeaconSummaryList{summaries: summaries}} ->
-            summaries_address_resolved =
-              Enum.map(
-                summaries,
-                &Crypto.derive_beacon_chain_address(&1.subset, &1.summary_time, true)
-              )
-
-            {summaries, acc ++ summaries_address_resolved}
-
-          _ ->
-            {[], acc}
-        end
-    end)
-    |> Stream.map(fn %BeaconSummary{transaction_summaries: transaction_summaries} ->
+    |> Task.async_stream(
+      fn {address, nodes, patch} ->
+        BeaconSummaryHandler.download_summary(address, nodes, patch)
+      end,
+      on_timeout: :kill_task,
+      max_concurrency: 256
+    )
+    |> Stream.filter(&match?({:ok, {:ok, %BeaconSummary{}}}, &1))
+    |> Enum.flat_map(fn {:ok, {:ok, %BeaconSummary{transaction_summaries: transaction_summaries}}} ->
       transaction_summaries
     end)
-    |> Stream.flat_map(& &1)
-    |> Enum.to_list()
   end
 
   defp list_transaction_by_date(nil), do: []
