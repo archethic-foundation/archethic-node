bb72fbccff56216265dd98c9833fae42425ac1ad
apoorv-2204 <apoorva@uniris.io>
2022-02-22 19:51:55 +0545

    Addition of more doc test for helpers.ex

--- lib/archethic/metrics/helpers.ex
+++ lib/archethic/metrics/helpers.ex
@@ -116,6 +116,234 @@ defmodule ArchEthic.Metrics.Helpers do
     end
   end
 
+
+  @doc """
+  Fetches the metric name and count,sum of the given histogram metrics.
+  Fetches the metric name and metic value for guage metrics.
+
+
+
+  """
+  def retrieve_metric_parameter_data(data) do
+    Enum.map(data, fn each_metric ->
+      case each_metric.type do
+        "histogram" -> fetch_histogram_sum_count(each_metric)
+        "gauge" -> fetch_guage_value(each_metric)
+      end
+    end)
+  end
+
+  def calculate_points(list_of_map_of_metric_with_name_count_sum_value) do
+    Enum.map(
+      list_of_map_of_metric_with_name_count_sum_value,
+      fn metric_map ->
+        [{metric_name, count_or_sum_or_value_map}] = metric_map |> Map.to_list()
+
+        case count_or_sum_or_value_map do
+          %{count: count, sum: sum} ->
+            fetch_avg(metric_name, count, sum)
+
+          value ->
+            %{metric_name => value}
+        end
+      end
+    )
+  end
+
+  def fetch_avg(metric_name, count, sum) do
+    avg =
+      case count == 0 do
+        true -> 0
+        false -> sum / count
+      end
+
+    %{metric_name => avg}
+  end
+
+  def fetch_guage_value(single_metric_map) do
+    [value_map | _tail] = single_metric_map.metrics
+
+    {guage_value, _} =
+      case Map.fetch(value_map, :value) do
+        {:ok, value} -> Float.parse(value)
+        :error -> {0, ""}
+      end
+
+    %{single_metric_map.name => guage_value}
+  end
+
+  def fetch_histogram_sum_count(single_metric_map) do
+    list2 =
+      Enum.map(single_metric_map.metrics, fn inner_data ->
+        {sum, count} = get_sum_count(inner_data)
+        %{sum: sum, count: count}
+      end)
+
+    %{
+      single_metric_map.name => %{
+        sum: Enum.reduce(list2, 0, fn data_tuple, acc -> acc + data_tuple.sum end),
+        count: Enum.reduce(list2, 0, fn data_tuple, acc -> acc + data_tuple.count end)
+      }
+    }
+  end
+
+  def get_sum_count(inner_data) do
+    {count, _} =
+      case Map.fetch(inner_data, "count") do
+        {:ok, value} -> Float.parse(value)
+        :error -> {0, ""}
+      end
+
+    {sum, _} =
+      case Map.fetch(inner_data, "sum") do
+        {:ok, value} -> Float.parse(value)
+        :error -> {0, ""}
+      end
+
+    {sum, count}
+  end
+
+
+  def retrieve_node_ip_address() do
+    Enum.map(ArchEthic.P2P.list_nodes(), fn node_details ->
+      ip = :inet.ntoa(node_details.ip)
+      "#{ip}"
+    end)
+  end
+
+  def remove_noise(data) do
+    data
+    |> Stream.map(fn {:ok, result} -> result end)
+    |> Stream.filter(&remove_blank_responses/1)
+    |> Stream.map(&retrieve_data_from_response/1)
+  end
+
+  def remove_blank_responses(result) do
+    case result do
+      [] -> false
+      _ -> true
+    end
+  end
+
+  def retrieve_data_from_response(data) do
+    list =
+      Enum.filter(data, fn dataelement ->
+        case dataelement do
+          {:data, _, _} -> true
+          _ -> false
+        end
+      end)
+
+    list2 = Enum.map(list, fn x -> elem(x, 2) end)
+    [head | _tail] = list2
+    head
+  end
+
+
+  @doc """
+  Method combines maps of metrics of different nodes into a single map of respective metrics
+
+  ## Examples
+      iex> [
+      ...>   %{"vm_memory_atom" => 100.0},
+      ...>   %{"archethic_p2p_send_message_duration" => %{count: 100, sum: 10}},
+      ...>   %{"vm_memory_atom" => 200.0},
+      ...>   %{"archethic_p2p_send_message_duration" => %{count: 200, sum: 20}},
+      ...>   %{"vm_memory_atom" => 300.0},
+      ...>   %{"archethic_p2p_send_message_duration" => %{count: 300, sum: 30}}
+      ...> ]|>ArchEthic.Metrics.Helpers.aggregate_sum_n_count_n_value()
+      %{
+        "archethic_mining_full_transaction_validation_duration" => %{count: 0, sum: 0},
+        "archethic_mining_proof_of_work_duration" => %{count: 0, sum: 0},
+        "archethic_p2p_send_message_duration" => %{count: 600, sum: 60},
+        "tps" => %{count: 0, sum: 0},
+        "vm_memory_atom" => 600.0
+      }
+
+  """
+  def aggregate_sum_n_count_n_value(combined_list_of_map_metric) do
+    Enum.reduce(combined_list_of_map_metric, get_metric_default_value(), fn metric_map, acc ->
+      [{metric_name, count_or_sum_or_value_map}] = metric_map |> Map.to_list()
+
+      case count_or_sum_or_value_map do
+        %{count: count, sum: sum} -> update_histogram_acc(metric_name, count, sum, acc)
+        value -> update_guage_acc(metric_name, value, acc)
+      end
+    end)
+  end
+
+  @doc """
+    I/P map of metrics with count n sum and n value
+    O/P list of map of metrics with avg and value
+
+  ## Examples
+      iex> %{
+      ...>   "archethic_mining_full_transaction_validation_duration" => %{count: 5, sum: 10},
+      ...>   "archethic_p2p_send_message_duration" => %{count: 600, sum: 60},
+      ...>   "tps" => %{count: 5, sum: 50 },
+      ...>   "vm_memory_atom" => 600.0
+      ...> }|>ArchEthic.Metrics.Helpers.calculate_network_points()
+      [
+         %{"archethic_mining_full_transaction_validation_duration" => 2.0},
+         %{"archethic_p2p_send_message_duration" => 0.1},
+         %{"tps" => 10.0},
+         %{"vm_memory_atom" => 600.0}
+      ]
+  """
+  def calculate_network_points(map_of_metrics) do
+    Enum.map(map_of_metrics, fn {metric_name, count_or_sum_or_value_map} ->
+      case count_or_sum_or_value_map do
+        %{count: count, sum: sum} -> fetch_avg(metric_name, count, sum)
+        value -> %{metric_name => value}
+      end
+    end)
+  end
+
+
+  @doc """
+  Reduces to a single map of metrics rather than list of maps of metrics
+
+  ## Examples
+
+      iex>[
+      ...>   %{"archethic_mining_full_transaction_validation_duration" => 2.0},
+      ...>   %{"archethic_p2p_send_message_duration" => 0.1},
+      ...>   %{"tps" => 10.0},
+      ...>   %{"vm_memory_atom" => 600.0}
+      ...>]|>ArchEthic.Metrics.Helpers.reduce_to_single_map()
+      %{
+            "archethic_mining_full_transaction_validation_duration" => 2.0,
+            "archethic_p2p_send_message_duration" => 0.1,
+            "tps" => 10.0,
+            "vm_memory_atom" => 600.0
+        }
+  """
+  def reduce_to_single_map(data_list_of_maps) do
+    Enum.reduce(data_list_of_maps, fn a, b ->
+      Map.merge(a, b, fn _key, a1, a2 ->
+        a1 + a2
+      end)
+    end)
+  end
+
+
+
+  def update_histogram_acc(metric_name, count, sum, acc) do
+    cummlative_sum = get_in(acc, [metric_name, :sum])
+    cummlative_count = get_in(acc, [metric_name, :count])
+
+    put_in(
+      put_in(acc, [metric_name, :sum], cummlative_sum + sum),
+      [metric_name, :count],
+      cummlative_count + count
+    )
+  end
+
+  def update_guage_acc(metric_name, value, acc) do
+    cummlative_value = get_in(acc, [metric_name])
+    put_in(acc, [metric_name], cummlative_value + value)
+  end
+
   @doc """
   Tampered to change the required metrics.
   """
@@ -200,8 +428,8 @@ defmodule ArchEthic.Metrics.Helpers do
       # %{metrics: _, name: "vm_memory_atom_used", type: _} ->
       #   true
 
-      # %{metrics: _, name: "vm_memory_atom", type: _} ->
-      #   true
+      %{metrics: _, name: "vm_memory_atom", type: _} ->
+        true
 
       %{metrics: _, name: _, type: _} ->
         false
@@ -210,139 +438,15 @@ defmodule ArchEthic.Metrics.Helpers do
         false
     end
   end
-
-  @doc """
-  Fetches the metric name and count,sum of the given histogram metrics.
-  Fetches the metric name and metic value for guage metrics.
+  @doc"""
+  Returns a default for Metric Poller
   """
-  def retrieve_metric_parameter_data(data) do
-    Enum.map(data, fn each_metric ->
-      case each_metric.type do
-        "histogram" -> fetch_histogram_sum_count(each_metric)
-        "gauge" -> fetch_guage_value(each_metric)
-      end
-    end)
-  end
-
-  def calculate_points(list_of_map_of_metric_with_name_count_sum_value) do
-    Enum.map(
-      list_of_map_of_metric_with_name_count_sum_value,
-      fn metric_map ->
-        [{metric_name, count_or_sum_or_value_map}] = metric_map |> Map.to_list()
-
-        case count_or_sum_or_value_map do
-          %{count: count, sum: sum} ->
-            fetch_avg(metric_name, count, sum)
-
-          value ->
-            %{metric_name => value}
-        end
-      end
-    )
-  end
-
-  def fetch_avg(metric_name, count, sum) do
-    avg =
-      case count == 0 do
-        true -> 0
-        false -> sum / count
-      end
-
-    %{metric_name => avg}
-  end
-
-  def fetch_guage_value(single_metric_map) do
-    [value_map | _tail] = single_metric_map.metrics
-
-    {guage_value, _} =
-      case Map.fetch(value_map, :value) do
-        {:ok, value} -> Float.parse(value)
-        :error -> {0, ""}
-      end
-
-    %{single_metric_map.name => guage_value}
-  end
-
-  def fetch_histogram_sum_count(single_metric_map) do
-    list2 =
-      Enum.map(single_metric_map.metrics, fn inner_data ->
-        {sum, count} = get_sum_count(inner_data)
-        %{sum: sum, count: count}
-      end)
-
-    %{
-      single_metric_map.name => %{
-        sum: Enum.reduce(list2, 0, fn data_tuple, acc -> acc + data_tuple.sum end),
-        count: Enum.reduce(list2, 0, fn data_tuple, acc -> acc + data_tuple.count end)
-      }
-    }
-  end
-
-  def get_sum_count(inner_data) do
-    {count, _} =
-      case Map.fetch(inner_data, "count") do
-        {:ok, value} -> Float.parse(value)
-        :error -> {0, ""}
-      end
-
-    {sum, _} =
-      case Map.fetch(inner_data, "sum") do
-        {:ok, value} -> Float.parse(value)
-        :error -> {0, ""}
-      end
-
-    {sum, count}
-  end
-
-  def reduce_to_single_map(data_list_of_maps) do
-    Enum.reduce(data_list_of_maps, fn a, b ->
-      Map.merge(a, b, fn _key, a1, a2 ->
-        a1 + a2
-      end)
-    end)
-  end
-
-  def retrieve_node_ip_address() do
-    Enum.map(ArchEthic.P2P.list_nodes(), fn node_details ->
-      ip = :inet.ntoa(node_details.ip)
-      "#{ip}"
-    end)
-  end
-
-  def remove_noise(data) do
-    data
-    |> Stream.map(fn {:ok, result} -> result end)
-    |> Stream.filter(&remove_blank_responses/1)
-    |> Stream.map(&retrieve_data_from_response/1)
-  end
-
-  def remove_blank_responses(result) do
-    case result do
-      [] -> false
-      _ -> true
-    end
-  end
-
-  def retrieve_data_from_response(data) do
-    list =
-      Enum.filter(data, fn dataelement ->
-        case dataelement do
-          {:data, _, _} -> true
-          _ -> false
-        end
-      end)
-
-    list2 = Enum.map(list, fn x -> elem(x, 2) end)
-    [head | _tail] = list2
-    head
-  end
-
   def get_client_metric_default_value() do
     %{
       "archethic_mining_full_transaction_validation_duration" => 0,
       "archethic_mining_proof_of_work_duration" => 0,
       "archethic_p2p_send_message_duration" => 0,
-      "tps" => 0
+      "tps" => 0,
       # "archethic_election_validation_nodes_duration" => 0,
       # "archethic_election_storage_nodes_duration" => 0,
       # "archethic_mining_pending_transaction_validation_duration" => 0,
@@ -364,18 +468,19 @@ defmodule ArchEthic.Metrics.Helpers do
       # "vm_memory_code" => 0,
       # "vm_memory_binary" => 0,
       # "vm_memory_atom_used" => 0,
-      # "vm_memory_atom" => 0
+      "vm_memory_atom" => 0
     }
   end
 
   @doc """
+  Returns metric default value for accumulator
   """
   def get_metric_default_value() do
     %{
       "tps" => %{count: 0, sum: 0},
       "archethic_mining_full_transaction_validation_duration" => %{count: 0, sum: 0},
       "archethic_p2p_send_message_duration" => %{count: 0, sum: 0},
-      "archethic_mining_proof_of_work_duration" => %{count: 0, sum: 0}
+      "archethic_mining_proof_of_work_duration" => %{count: 0, sum: 0},
       # "archethic_election_validation_nodes_duration" => %{count: 0, sum: 0},
       # "archethic_election_storage_nodes_duration" => %{count: 0, sum: 0},
       # "archethic_mining_pending_transaction_validation_duration" => %{count: 0, sum: 0},
@@ -397,64 +502,7 @@ defmodule ArchEthic.Metrics.Helpers do
       # "vm_memory_code" => 0,
       # "vm_memory_binary" => 0,
       # "vm_memory_atom_used" => 0,
-      # "vm_memory_atom" => 0
+      "vm_memory_atom" => 0
     }
-  end
-
-  @doc """
-  Method combines maps of metrics of different nodes into a single map of respective metrics
-  Expected Input :
-    [ %{"metric_name" => %{ count: 0, sum: 0 }} , %{"metric_name" => 0} , %{ } , %{ } , %{ } ...]
-    list of metrics , where metric is a map with name and count and sum for histogram type metrics ,
-    for metric type guage it is a map with %{"metric_name" => 0}.
-  Expected Output :
-
-
-  """
-  def aggregate_sum_n_count_n_value(combined_list_of_map_metric) do
-    Enum.reduce(combined_list_of_map_metric, get_metric_default_value(), fn metric_map, acc ->
-      [{metric_name, count_or_sum_or_value_map}] = metric_map |> Map.to_list()
-
-      case count_or_sum_or_value_map do
-        %{count: count, sum: sum} -> update_histogram_acc(metric_name, count, sum, acc)
-        value -> update_guage_acc(metric_name, value, acc)
-      end
-    end)
-  end
-
-  @doc """
-    I/P map of metrics with count n sum and n value
-    O/P list of map of metrics with avg and value
-  """
-  def calculate_network_points(map_of_metrics) do
-    Enum.map(map_of_metrics, fn {metric_name, count_or_sum_or_value_map} ->
-      case count_or_sum_or_value_map do
-        %{count: count, sum: sum} -> fetch_avg(metric_name, count, sum)
-        value -> %{metric_name => value}
-      end
-    end)
-  end
-
-  # [head | _tail] = Map.keys(x)
-  # csum = get_in(acc, [head, :sum])
-  # ccount = get_in(acc, [head, :count])
-  # sum = get_in(x, [head, :sum])
-  # count = get_in(x, [head, :count])
-  # put_in(put_in(acc, [head, :sum], csum + sum), [head, :count], ccount + count)
-
-  def update_histogram_acc(metric_name, count, sum, acc) do
-    cummlative_sum = get_in(acc, [metric_name, :sum])
-    cummlative_count = get_in(acc, [metric_name, :count])
-
-    put_in(
-      put_in(acc, [metric_name, :sum], cummlative_sum + sum),
-      [metric_name, :count],
-      cummlative_count + count
-    )
-  end
-
-  def update_guage_acc(metric_name, value, acc) do
-    cummlative_value = get_in(acc, [metric_name])
-    put_in(acc, [metric_name], cummlative_value + value)
   end
 end
