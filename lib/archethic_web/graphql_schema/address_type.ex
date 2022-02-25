ae9ea9ff8ee0aeeae53b59b50f38fb8e6cbd8b57
Samuel <samuel@uniris.io>
2022-02-25 10:40:43 +0100

    Add API Address checks

--- lib/archethic_web/graphql_schema/address_type.ex
+++ lib/archethic_web/graphql_schema/address_type.ex
@@ -5,10 +5,9 @@ defmodule ArchEthicWeb.GraphQLSchema.AddressType do
   alias ArchEthic.Crypto
 
   @desc """
-  The [Address] scalar type represents a cryptographic hash used in
-  the ArchEthic network with an identification byte to specify from which algorithm the hash was generated.
-  The Hash appears in a JSON response as Base16 formatted string. The parsed hash will
-  be converted to a binary and any invalid hash with an invalid algorithm or invalid size will be rejected
+  The [Address] scalar type represents a transaction's address.
+  The Address appears in a JSON response as Base16 formatted string. The parsed address will
+  be converted to a binary and any invalid address with an invalid algorithm or invalid size will be rejected
   """
   scalar :address do
     serialize(&Base.encode16/1)
@@ -16,10 +15,10 @@ defmodule ArchEthicWeb.GraphQLSchema.AddressType do
   end
 
   @spec parse_address(Absinthe.Blueprint.Input.String.t()) :: {:ok, binary()} | :error
-  defp parse_address(%Absinthe.Blueprint.Input.String{value: hash}) do
-    with {:ok, hash} <- Base.decode16(hash, case: :mixed),
-         true <- Crypto.valid_hash?(hash) do
-      {:ok, hash}
+  defp parse_address(%Absinthe.Blueprint.Input.String{value: address}) do
+    with {:ok, addr} <- Base.decode16(address, case: :mixed),
+         true <- Crypto.valid_address?(addr) do
+      {:ok, addr}
     else
       _ ->
         :error
---
+++ lib/archethic_web/graphql_schema/hash_type.ex
@@ -0,0 +1,30 @@
+defmodule ArchEthicWeb.GraphQLSchema.HashType do
+  @moduledoc false
+
+  use Absinthe.Schema.Notation
+  alias ArchEthic.Crypto
+
+  @desc """
+  The [Hash] scalar type represents a cryptographic hash used in
+  the ArchEthic network with an identification byte to specify from which algorithm the hash was generated.
+  The Hash appears in a JSON response as Base16 formatted string. The parsed hash will
+  be converted to a binary and any invalid hash with an invalid algorithm or invalid size will be rejected
+  """
+  scalar :hash do
+    serialize(&Base.encode16/1)
+    parse(&parse_hash/1)
+  end
+
+  @spec parse_hash(Absinthe.Blueprint.Input.String.t()) :: {:ok, binary()} | :error
+  defp parse_hash(%Absinthe.Blueprint.Input.String{value: hash}) do
+    with {:ok, hash} <- Base.decode16(hash, case: :mixed),
+         true <- Crypto.valid_hash?(hash) do
+      {:ok, hash}
+    else
+      _ ->
+        :error
+    end
+  end
+
+  defp parse_hash(_), do: :error
+end
