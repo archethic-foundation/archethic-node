75800a0c0b8a61c1942a58ebb5a63f771bc3b852
apoorv-2204 <90304197+apoorv-2204@users.noreply.github.com>
2022-03-08 21:48:27 +0530

    Refactor and improve testing of metrics polling #235 (#236)
    Co-authored-by: Samuel <samuel@uniris.io>

--- assets/js/app.js
+++ assets/js/app.js
@@ -74,7 +74,7 @@ Hooks.Logs = {
   }
 }
 
-
+//metric dashboard hook /metrics/dashboard
 Hooks.network_charts = {
   mounted() {
 
@@ -82,9 +82,6 @@ Hooks.network_charts = {
     this.handleEvent("network_points", ({
       points
     }) => {
-      console.log("---------------")
-      console.log(points);
-      console.log("------------------")
       points = metric_config_obj.structure_metric_points(points)
      
       network_metric_obj = metric_config_obj.update_network_live_visuals(network_metric_obj , points);
@@ -103,12 +100,7 @@ Hooks.explorer_charts = {
       this.handleEvent("explorer_stats_points", ({
         points
       }) => {
-        console.log(points);
-        console.log("------------------")
         points = metric_config_obj.structure_metric_points(points)
-        console.log("=================")
-        console.log(points);
-        console.log("=================")
         explorer_metric_obj = metric_config_obj.update_explorer_live_visuals(explorer_metric_obj , points);    
       });
   }
--- lib/archethic/metrics/metric_supervisor.ex
+++ lib/archethic/metrics/metric_supervisor.ex
@@ -1,19 +1,18 @@
 defmodule ArchEthic.Metrics.MetricSupervisor do
-  @moduledoc """
-  Supervisor implementation for child process :
-      ArchEthic.Metrics.Poller,
-      Strat-Used : one for one
-  """
+  @moduledoc false
   use Supervisor
+
+  alias ArchEthic.Utils
 
   def start_link(_initial_state) do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
   def init(_initial_state) do
-    children = [
-      ArchEthic.Metrics.Poller
-    ]
+    children =
+      Utils.configurable_children([
+        ArchEthic.Metrics.Poller
+      ])
 
     Supervisor.init(children, strategy: :one_for_one)
   end
