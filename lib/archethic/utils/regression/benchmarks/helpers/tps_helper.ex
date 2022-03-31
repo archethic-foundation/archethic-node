bb550c95baab0cd9f0186231471d42d51ae68300
apoorv-2204 <apoorva@uniris.io>
2022-03-31 08:44:10 -0700

    Txn creation

--- lib/archethic/utils/regression/benchmarks/helpers/tps_helper.ex
+++ lib/archethic/utils/regression/benchmarks/helpers/tps_helper.ex
@@ -3,11 +3,6 @@ defmodule ArchEthic.Utils.Regression.Benchmarks.Helpers.TPSHelper do
   Helpers Methods to carry out the TPS benchmarks
   """
 
-  def main() do
-    recipient_address = "0000c084b09c60e3bde2d0a81df08b20d82d8b6dfc1d39bc3dfa5e41b731718f09e1"
-    withdraw_uco_via_this_node(recipient_address)
-  end
-
   def withdraw_uco_from_testnet_endpoint(recipient_address) do
     alias ArchEthic.Utils.Regression.Benchmarks.Helpers.FaucetEndpoint
     FaucetEndpoint.main(recipient_address)
--- lib/archethic/utils/regression/benchmarks/tps.ex
+++ lib/archethic/utils/regression/benchmarks/tps.ex
@@ -7,10 +7,92 @@ defmodule ArchEthic.Utils.Regression.Benchmark.TPS do
   require Logger
 
   alias ArchEthic.Utils.Regression.Benchmark
-  # alias ArchEthic.Utils.Regression.Benchmarks.Helpers.TPSHelper
+  alias ArchEthic.Utils.Regression.Playbook
+  alias ArchEthic.Crypto
+  alias ArchEthic.Utils.Regression.Benchmarks.Helpers.TPSHelper
+
+  alias ArchEthic.TransactionChain.TransactionData
+  alias ArchEthic.TransactionChain.TransactionData.Ledger
+  alias ArchEthic.TransactionChain.TransactionData.UCOLedger
+  alias ArchEthic.TransactionChain.TransactionData.UCOLedger.Transfer, as: UCOTransfer
 
   @behaviour Benchmark
 
-  def plan([_host | _nodes], _opts) do
+  def plan([host | _nodes], _opts) do
+    port = Application.get_env(:archethic, ArchEthic.P2P.Listener)[:port]
+
+    {alice, bob} = preliminaries()
+
+    TPSHelper.withdraw_uco_from_testnet_endpoint(alice.address)
+    TPSHelper.withdraw_uco_from_testnet_endpoint(bob.address)
+
+    dispatch_transactions(alice, bob, host, port)
+  end
+
+  def preliminaries() do
+    alice = %{seed: "StupidAlice"}
+    bob = %{seed: "IdiotBob"}
+
+    alice = Map.put(alice, :address, get_address(alice.seed))
+    bob = Map.put(bob, :address, get_address(bob.seed))
+
+    alice = Map.put(alice, :transaction_data, get_transaction_data(bob.address))
+    bob = Map.put(bob, :transaction_data, get_transaction_data(alice.address))
+
+    {alice, bob}
+  end
+
+  def get_address(seed) do
+    {next_public_key, _next_private_key} =
+      Crypto.derive_keypair(seed, 0 + 1, Crypto.default_curve())
+
+    Crypto.derive_address(next_public_key)
+  end
+
+  def get_transaction_data(recipient_address) do
+    # exchange 100 x 10^-8 uco exchange or 10^-6 uco
+    # 10% fees 10^-7
+    %TransactionData{
+      ledger: %Ledger{
+        uco: %UCOLedger{
+          transfers: [
+            %UCOTransfer{
+              to: recipient_address,
+              amount: 100
+            }
+          ]
+        }
+      }
+    }
+  end
+
+  def dispatch_transactions(alice, bob, host, port) do
+    Playbook.send_transaction(
+      alice.seed,
+      :transfer,
+      alice.transaction_data,
+      host,
+      port,
+      Crypto.default_curve()
+    )
+
+    Playbook.send_transaction(
+      bob.seed,
+      :transfer,
+      bob.transaction_data,
+      host,
+      port,
+      Crypto.default_curve()
+    )
+  end
+
+  def benchmark(alice, bob, host, port) do
+    Task.async_stream(
+      1..3,
+      fn _x ->
+        dispatch_transactions(alice, bob, host, port)
+      end,
+      max_concurrenct: System.schedulers_online() * 10
+    )
   end
 end
