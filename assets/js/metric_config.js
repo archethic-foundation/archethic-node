05b44fd82ea29aed275967b3e0702a927f5698f8
Apoorv2204 <apoorva@uniris.io>
2022-05-10 05:36:01 -0700

    add Support for dark mode

--- assets/js/metric_config.js
+++ assets/js/metric_config.js
@@ -1,8 +1,8 @@
 import * as echarts from 'echarts';
 
-//adds 0 to the metric not recieved to avoid charts going blank
-
+//adds 0 to the metrics value, to avoid charts going blank
 function structure_metric_points(latest_points) {
+
   var points_default_value = {
     "archethic_archethic_election_validation_nodes_durations_duration": 0.0,
     "archethic_election_storage_nodes_duration": 0.0,
@@ -35,27 +35,33 @@ function structure_metric_points(latest_points) {
   return points_default_value;
 }
 
-function get_visuals_dom() {
-  var metric_object, x_axis_data;
-  x_axis_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  return metric_object =
-  {
+function get_visuals_dom(theme) {
+  var metric_object, x_axis_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  return metric_object = {
     seconds_after_loading_of_this_graph: 0,
+
     x_axis_data: x_axis_data,
-    archethic_mining_proof_of_work_duration: generateEchartObjects('PoW Duration(ms)', 'archethic_mining_proof_of_work_duration', x_axis_data),
-    archethic_mining_full_transaction_validation_duration: generateEchartObjects('Transaction Validation Duration(ms)', 'archethic_mining_full_transaction_validation_duration', x_axis_data),
-    tps: generate_echart_guage("Transactions Per Second(tps)", 'tps'),
-    archethic_p2p_send_message_duration: generate_echart_guage("P2P Message duration(ms) (Supervised Multicast)", 'archethic_p2p_send_message_duration',),
+
+    archethic_mining_proof_of_work_duration: generateEchartObjects(
+      'PoW Duration(ms)', 'archethic_mining_proof_of_work_duration', x_axis_data, theme),
+
+    archethic_mining_full_transaction_validation_duration: generateEchartObjects(
+      'Transaction Validation Duration(ms)',
+      'archethic_mining_full_transaction_validation_duration', x_axis_data, theme),
+
+    tps: generate_echart_guage("Transactions Per Second(tps)", 'tps', theme),
+
+    archethic_p2p_send_message_duration: generate_echart_guage(
+      "P2P Message duration(ms) (Supervised Multicast)",
+      'archethic_p2p_send_message_duration', theme),
   };
 }
 
-function generateEchartObjects(heading, echartContainer, x_axis_data) {
-  var y_axis_data =
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  var chart = echarts.init(document.getElementById(echartContainer));
+//echarts line_graph default  theme 
+function line_graph_default_theme(heading, x_axis_data, y_axis_data) {
 
-  var option = {
-
+  return {
     grid: {
       left: '10%',
       right: '5%',
@@ -66,11 +72,12 @@ function generateEchartObjects(heading, echartContainer, x_axis_data) {
       left: 'center',
       text: ` ${heading}`,
       textStyle: {
-        color: '#000000', fontSize: 16, fontFamily:
-          "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+        color: '#000000',
+        fontSize: 16,
+        fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
       }
     },
-
+    // backgroundColor: "#FFFFFF",
     xAxis: {
       type: 'category',
       // boundaryGap: false,
@@ -85,9 +92,70 @@ function generateEchartObjects(heading, echartContainer, x_axis_data) {
       boundaryGap: [0, '50%'],
       type: 'value',
       axisLabel: {
-        formatter: '{value}', textStyle: {
-          color: '#000000', fontSize: 16, fontFamily:
-            "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+        formatter: '{value}',
+        textStyle: {
+          color: '#000000',
+          fontSize: 16,
+          fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+        }
+      },
+      splitLine: {
+        show: true,
+        lineStyle: { color: "lightgrey", width: 0.5 }
+      },
+    },
+    series: [{
+      type: 'line',
+      symbol: 'none',
+      triggerLineEvent: false,
+      itemStyle: {
+        color: 'rgb(0, 164, 219,1)'
+      },
+      silent: true,
+      data: y_axis_data,
+    }]
+  };
+}
+
+//echarts line_graph dark theme  
+function line_graph_dark_theme(heading, x_axis_data, y_axis_data) {
+
+  return {
+    grid: {
+      left: '10%',
+      right: '5%',
+      bottom: '5%',
+      top: "15%"
+    },
+    title: {
+      left: 'center',
+      text: ` ${heading}`,
+      textStyle: {
+        color: '#000000',
+        fontSize: 16,
+        fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+      }
+    },
+    backgroundColor: "#FFFFFF",
+    xAxis: {
+      type: 'category',
+      // boundaryGap: false,
+      data: x_axis_data,
+      show: false,
+      splitLine: {
+        show: true,
+        lineStyle: { color: "lightgrey", width: 0.5 }
+      }
+    },
+    yAxis: {
+      boundaryGap: [0, '50%'],
+      type: 'value',
+      axisLabel: {
+        formatter: '{value}',
+        textStyle: {
+          color: '#000000',
+          fontSize: 16,
+          fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
         }
       },
       splitLine: {
@@ -96,20 +164,26 @@ function generateEchartObjects(heading, echartContainer, x_axis_data) {
       },
 
     },
-    series: [
-      {
-        type: 'line',
-        symbol: 'none',
-        triggerLineEvent: false,
-        itemStyle: {
-          color: 'rgb(0, 164, 219,1)'
-        },
-        silent: true,
-        data: y_axis_data,
-      }
-    ]
-
+    series: [{
+      type: 'line',
+      symbol: 'none',
+      triggerLineEvent: false,
+      itemStyle: {
+        color: 'rgb(0, 164, 219,1)'
+      },
+      silent: true,
+      data: y_axis_data,
+    }]
   };
+}
+
+function generateEchartObjects(heading, echartContainer, x_axis_data, theme) {
+  var y_axis_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var chart = echarts.init(document.getElementById(echartContainer));
+
+  var option =
+    theme == 0 ? line_graph_default_theme(heading, x_axis_data, y_axis_data) :
+      line_graph_dark_theme(heading, x_axis_data, y_axis_data);
 
   option && chart.setOption(option);
   window.addEventListener('resize', function () {
@@ -117,97 +191,189 @@ function generateEchartObjects(heading, echartContainer, x_axis_data) {
   });
 
   return { chart: chart, ydata: y_axis_data };
-
 }
 
 
-function generate_echart_guage(heading, eguageContainer) {
-  var guage = echarts.init(document.getElementById(eguageContainer));
+function guage_default_theme(heading) {
 
-  var guage_options = {
+  return {
 
     title: {
       left: 'center',
-      text: `${heading}`, textStyle: {
-        color: '#000000', fontSize: 16, fontFamily:
-          "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+      text: `${heading}`,
+      textStyle: {
+        color: '#000000',
+        fontSize: 16,
+        fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
       }
     },
-    series: [
-      {
+    series: [{
 
-        type: 'gauge',
-        center: ['50%', '74%'],
-        startAngle: 200,
-        endAngle: -20,
-        min: 0,
-        max: 0,
-        splitNumber: 5,
-        itemStyle: {
-          color: '#00a4db'
-        },
-        progress: {
-          show: true,
+      type: 'gauge',
+      center: ['50%', '74%'],
+      startAngle: 200,
+      endAngle: -20,
+      min: 0,
+      max: 0,
+      splitNumber: 5,
+      backgroundColor: "#fffff",
+      itemStyle: {
+        color: '#00a4db'
+      },
+      progress: {
+        show: true,
+        width: 30
+      },
+      pointer: {
+        show: false
+      },
+      axisLine: {
+        lineStyle: {
           width: 30
+        }
+      },
+      axisTick: {
+        distance: -45,
+        splitNumber: 5,
+        lineStyle: {
+          width: 2,
+          color: '#000000'
+        }
+      },
+      splitLine: {
+        distance: -52,
+        length: 14,
+        lineStyle: {
+          width: 3,
+          color: '#000000'
+        }
+      },
+      axisLabel: {
+        distance: -20,
+        color: '#000000 ',
+        fontSize: 16,
+        formatter: function (value) {
+          return exponent_formatter(value);
+        }
+      },
+      anchor: {
+        show: false
+      },
+      title: {
+        show: false
+      },
+      detail: {
+        valueAnimation: true,
+        width: '60%',
+        lineHeight: 40,
+        borderRadius: 8,
+        offsetCenter: [0, '-15%'],
+        fontSize: 16,
+        fontWeight: 'bolder',
+        formatter: function (value) {
+          return exponent_formatter(value);
         },
-        pointer: {
-          show: false
-        },
-        axisLine: {
-          lineStyle: {
-            width: 30
-          }
-        },
-        axisTick: {
-          distance: -45,
-          splitNumber: 5,
-          lineStyle: {
-            width: 2,
-            color: '#000000'
-          }
-        },
-        splitLine: {
-          distance: -52,
-          length: 14,
-          lineStyle: {
-            width: 3,
-            color: '#000000'
-          }
-        },
-        axisLabel: {
-          distance: -20,
-          color: '#000000 ',
-          fontSize: 16,
-          formatter: function (value) {
-            return exponent_formatter(value);
-          }
-        },
-        anchor: {
-          show: false
-        },
-        title: {
-          show: false
-        },
-        detail: {
-          valueAnimation: true,
-          width: '60%',
-          lineHeight: 40,
-          borderRadius: 8,
-          offsetCenter: [0, '-15%'],
-          fontSize: 16,
-          fontWeight: 'bolder',
-          formatter: function (value) {
-            return exponent_formatter(value);
-          },
-          color: 'auto'
-        },
-        data: [
-          {
-            value: 0
-          }
-        ]
+        color: 'auto'
+      },
+      data: [{
+        value: 0
       }]
-  };
+    }]
+  }
+}
+
+function guage_dark_theme(heading) {
+
+  return {
+    title: {
+      left: 'center',
+      text: `${heading}`,
+      textStyle: {
+        color: '#000000',
+        fontSize: 16,
+        fontFamily: "BlinkMacSystemFont,-apple-system,Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,Helvetica, Arial, sans-serif"
+      }
+    },
+    series: [{
+      type: 'gauge',
+      center: ['50%', '74%'],
+      startAngle: 200,
+      endAngle: -20,
+      min: 0,
+      max: 0,
+      splitNumber: 5,
+      "backgroundColor": "#fffff",
+      itemStyle: {
+        color: '#00a4db'
+      },
+      progress: {
+        show: true,
+        width: 30
+      },
+      pointer: {
+        show: false
+      },
+      axisLine: {
+        lineStyle: {
+          width: 30
+        }
+      },
+      axisTick: {
+        distance: -45,
+        splitNumber: 5,
+        lineStyle: {
+          width: 2,
+          color: '#000000'
+        }
+      },
+      splitLine: {
+        distance: -52,
+        length: 14,
+        lineStyle: {
+          width: 3,
+          color: '#000000'
+        }
+      },
+      axisLabel: {
+        distance: -20,
+        color: '#000000 ',
+        fontSize: 16,
+        formatter: function (value) {
+          return exponent_formatter(value);
+        }
+      },
+      anchor: {
+        show: false
+      },
+      title: {
+        show: false
+      },
+      detail: {
+        valueAnimation: true,
+        width: '60%',
+        lineHeight: 40,
+        borderRadius: 8,
+        offsetCenter: [0, '-15%'],
+        fontSize: 16,
+        fontWeight: 'bolder',
+        formatter: function (value) {
+          return exponent_formatter(value);
+        },
+        color: 'auto'
+      },
+      data: [{
+        value: 0
+      }]
+    }]
+  }
+}
+
+function generate_echart_guage(heading, eguageContainer, theme) {
+  var guage = echarts.init(document.getElementById(eguageContainer));
+
+  var guage_options =
+    theme == 0 ? guage_default_theme(heading) :
+      guage_dark_theme(heading);
 
   guage_options && guage.setOption(guage_options);
   window.addEventListener('resize', function () {
@@ -227,11 +393,16 @@ function exponent_formatter(new_point) {
 
 //update the charts with new data
 function update_chart_data(chart_obj, x_axis_data, points, point_name) {
-  var new_data = 0, new_point = 0, shifted_value = 0;
+  var new_data = 0,
+    new_point = 0,
+    shifted_value = 0;
+
   new_point = points[point_name];
   new_data = chart_obj.ydata[chart_obj.ydata.length - 1] + new_point;
+
   shifted_value = chart_obj.ydata.shift();
   chart_obj.ydata.push(new_data);
+
   chart_obj.chart.setOption({
     xAxis: {
       data: x_axis_data
@@ -245,26 +416,28 @@ function update_chart_data(chart_obj, x_axis_data, points, point_name) {
 
 // update the guage with new data
 function update_guage_data(guage_obj, points, point_name) {
-  var new_data = 0, new_point = 0;
+  var new_data = 0,
+    new_point = 0;
+
   new_point = points[point_name];
   new_data = new_point;
   if (new_data >= guage_obj.max) {
     guage_obj.max = new_data
   }
+
   guage_obj.guage.setOption({
-    series:
-      [
-        {
-          min: 0,
-          max: guage_obj.max, splitNumber: 5,
-          data: [{ value: new_data }]
-        }]
+    series: [{
+      min: 0,
+      max: guage_obj.max,
+      splitNumber: 5,
+      data: [{ value: new_data }]
+    }]
   });
 }
 
-function create_network_live_visuals() {
-  var metric_obj = get_visuals_dom();
-
+//themes: ArchethicDark =1  and defaultUniirs = 0
+function create_network_live_visuals(theme = 0) {
+  var metric_obj = get_visuals_dom(theme);
   return metric_obj;
 }
 
@@ -274,24 +447,31 @@ function update_network_live_visuals(network_metric_obj, points) {
 }
 
 function update_live_visuals(metric_obj, points) {
+
   metric_obj.seconds_after_loading_of_this_graph += 10;
   var shifted = metric_obj.x_axis_data.shift();
   metric_obj.x_axis_data.push(metric_obj.seconds_after_loading_of_this_graph);
+
   update_chart_data(metric_obj.archethic_mining_proof_of_work_duration, metric_obj.x_axis_data, points, "archethic_mining_proof_of_work_duration");
   update_chart_data(metric_obj.archethic_mining_full_transaction_validation_duration, metric_obj.x_axis_data, points, "archethic_mining_full_transaction_validation_duration");
+
   update_guage_data(metric_obj.archethic_p2p_send_message_duration, points, "archethic_p2p_send_message_duration");
   update_guage_data(metric_obj.tps, points, "tps");
 
   return metric_obj;
 }
 
-function create_explorer_live_visuals() {
+function create_explorer_live_visuals(theme = 0) {
   var obj, x_axis_data;
   x_axis_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   obj = {
     seconds_after_loading_of_this_graph: 0,
     x_axis_data: x_axis_data,
-    archethic_mining_full_transaction_validation_duration: generateEchartObjects('Transaction Validation duration (ms)', 'archethic_mining_full_transaction_validation_duration', x_axis_data)
+
+    archethic_mining_full_transaction_validation_duration:
+
+      generateEchartObjects('Transaction Validation duration (ms)',
+        'archethic_mining_full_transaction_validation_duration', x_axis_data, theme)
   };
   return obj;
 };
@@ -301,14 +481,13 @@ function update_explorer_live_visuals(explorer_metric_obj, points) {
   //passing objects's variable through by reference
   explorer_metric_obj.seconds_after_loading_of_this_graph += 5;
   var shifted = explorer_metric_obj.x_axis_data.shift();
-  explorer_metric_obj.x_axis_data.push(explorer_metric_obj.seconds_after_loading_of_this_graph);
 
+  explorer_metric_obj.x_axis_data.push(explorer_metric_obj.seconds_after_loading_of_this_graph);
   update_chart_data(explorer_metric_obj.archethic_mining_full_transaction_validation_duration,
     explorer_metric_obj.x_axis_data, points, "archethic_mining_full_transaction_validation_duration");
 
   return explorer_metric_obj;
 };
-
 
 export {
   create_network_live_visuals,
