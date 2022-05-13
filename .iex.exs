6f0e18cd9dee7f85d3e245d0c864bbd95e9c1664
neylix <julien.leclerc05@protonmail.com>
2022-05-13 15:49:04 +0200

    Fix renaming of ArchEthic to Archethic

--- .iex.exs
+++ .iex.exs
@@ -1,15 +1,15 @@
 IEx.configure(inspect: [limit: :infinity])
 
-alias ArchEthic.Crypto
-alias ArchEthic.DB
-alias ArchEthic.P2P
-alias ArchEthic.P2P.Node
-alias ArchEthic.SharedSecrets
-alias ArchEthic.Account
-alias ArchEthic.Election
-alias ArchEthic.Governance
-alias ArchEthic.Contracts
-alias ArchEthic.TransactionChain
-alias ArchEthic.TransactionChain.Transaction
-alias ArchEthic.TransactionChain.TransactionData
-alias ArchEthic.BeaconChain
+alias Archethic.Crypto
+alias Archethic.DB
+alias Archethic.P2P
+alias Archethic.P2P.Node
+alias Archethic.SharedSecrets
+alias Archethic.Account
+alias Archethic.Election
+alias Archethic.Governance
+alias Archethic.Contracts
+alias Archethic.TransactionChain
+alias Archethic.TransactionChain.Transaction
+alias Archethic.TransactionChain.TransactionData
+alias Archethic.BeaconChain
