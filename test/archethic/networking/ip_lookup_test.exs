5dcf619b176b4d2435bd5a411312e11042925283
Apoorv2204 <apoorva@uniris.io>
2022-05-24 03:07:09 -0700

    simplifiy tests

--- test/archethic/networking/ip_lookup_test.exs
+++ test/archethic/networking/ip_lookup_test.exs
@@ -7,69 +7,52 @@ defmodule Archethic.Networking.IPLookupTest do
   alias Archethic.Networking.IPLookup.LocalDiscovery
   alias Archethic.Networking.IPLookup.RemoteDiscovery
 
-  describe "In Dev mode: get_node_ip()/0" do
-    Mix.env(:test)
-    assert :test == Mix.env()
-    Mix.env(:dev)
-    assert :dev == Mix.env()
+  def put_conf(validate_node_ip: validate_node_ip, ip_provider: ip_provider) do
+    Application.put_env(
+      :archethic,
+      Archethic.Networking,
+      validate_node_ip: validate_node_ip
+    )
 
-    test "Static IP Should not be Validated to be a Public IP| Must Return Static IP" do
+    Application.put_env(
+      :archethic,
+      Archethic.Networking.IPLookup,
+      ip_provider,
+      persistent: false
+    )
+  end
+
+  describe("Mix.env() as :dev, Archethic.Networking.IPLookup.get_node_ip()/0 ") do
+    # During mix.env as :dev we should not use NAT//IPIFY
+    test "Dev-mode:  Static IP Should not be Validated to be a Public IP" do
+      put_conf(validate_node_ip: false, ip_provider: MockStatic)
+
       MockStatic
       |> expect(:get_node_ip, fn -> {:ok, {127, 0, 0, 1}} end)
 
       assert {127, 0, 0, 1} == get_node_ip()
-    end
-
-    test "Localdiscovery/NAT & RemoteDiscovery/IPIFY " do
-      MockStatic
-      |> expect(:get_node_ip, fn -> {:ok, {127, 0, 0, 1}} end)
-
-      assert {127, 0, 0, 1} == get_node_ip()
-    end
-
-    test "assert currnet config is :dev  & restore to :test" do
-      assert :dev == Mix.env()
-      Mix.env(:test)
-      assert :test == Mix.env()
     end
   end
 
-  describe "In Prod mode: get_node_ip()/0" do
-    test "assert current config is test  & changed to :prod" do
-      Mix.env(:test)
-      assert :test == Mix.env()
-      Mix.env(:prod)
-      assert :prod == Mix.env()
-    end
-
-    test "If Static IP, it must raise an error" do
-      Application.put_env(
-        :archethic,
-        Archethic.Networking.IPLookup,
-        MockStatic,
-        persistent: false
-      )
+  describe "Mix.env() as :prod, Archethic.Networking.IPLookup.get_node_ip()/0" do
+    test "If Static IP, it must raise an error " do
+      # set prod mode configuration values
+      put_conf(validate_node_ip: true, ip_provider: MockStatic)
 
       MockStatic
       |> expect(:get_node_ip, fn -> {:ok, {127, 0, 0, 1}} end)
 
       provider = MockStatic
-
       reason = :invalid_ip
 
-      assert_raise RuntimeError,
-                   "Cannot use #{provider} IP lookup - #{inspect(reason)}",
-                   fn -> get_node_ip() end
+      assert_raise(RuntimeError, "Cannot use #{provider} IP lookup - #{inspect(reason)}", fn ->
+        get_node_ip()
+      end)
     end
 
-    test "Private IP(NAT), it must fallback to IPIFY to get public IP" do
+    test "If Private IP(NAT), it must fallback to IPIFY to get public IP" do
       # set prod mode configuration values
-      Application.put_env(
-        :archethic,
-        Archethic.Networking.IPLookup,
-        LocalDiscovery,
-        persistent: false
-      )
+      put_conf(validate_node_ip: true, ip_provider: LocalDiscovery)
 
       MockNAT
       |> expect(:get_node_ip, fn -> {:ok, {0, 0, 0, 0}} end)
@@ -82,23 +65,12 @@ defmodule Archethic.Networking.IPLookupTest do
 
     test "IPIFIY IP: returns public IP" do
       # set prod mode configuration values
-      Application.put_env(
-        :archethic,
-        Archethic.Networking.IPLookup,
-        RemoteDiscovery,
-        persistent: false
-      )
+      put_conf(validate_node_ip: true, ip_provider: RemoteDiscovery)
 
       MockIPIFY
       |> expect(:get_node_ip, fn -> {:ok, {17, 5, 7, 8}} end)
 
       assert {17, 5, 7, 8} == get_node_ip()
-    end
-
-    test "assert currnet config is :prod & restore to :test" do
-      assert :prod == Mix.env()
-      Mix.env(:test)
-      assert :test == Mix.env()
     end
   end
 end
