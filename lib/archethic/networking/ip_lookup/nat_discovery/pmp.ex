d4cced7d9fadbfdf9faad8f712fde479734fdf35
Apoorv2204 <apoorva@uniris.io>
2022-05-25 08:21:56 -0700

    remove specs causing dialyzer issues

--- lib/archethic/networking/ip_lookup/nat_discovery/pmp.ex
+++ lib/archethic/networking/ip_lookup/nat_discovery/pmp.ex
@@ -5,17 +5,6 @@ defmodule Archethic.Networking.IPLookup.NATDiscovery.PMP do
   @behaviour Impl
 
   @impl Impl
-  @spec get_node_ip ::
-          {:error,
-           :bad_response
-           | :einval
-           | :network_failure
-           | :no_nat
-           | :not_authorized
-           | :out_of_resource
-           | :unsupported_opcode
-           | :unsupported_version}
-          | {:ok, :inet.ip_address()}
   def get_node_ip() do
     with {:ok, router_ip} <- :natpmp.discover(),
          {:ok, ip_chars} <- :natpmp.get_external_address(router_ip) do
