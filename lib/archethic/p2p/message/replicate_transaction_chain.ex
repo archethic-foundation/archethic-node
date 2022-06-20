2ab4d74b5373d5a1fbfe6feb63c330629a3645dd
neylix <julien.leclerc05@protonmail.com>
2022-06-20 13:51:53 +0200

    Remove ack_storage not used

--- lib/archethic/p2p/message/replicate_transaction_chain.ex
+++ lib/archethic/p2p/message/replicate_transaction_chain.ex
@@ -3,12 +3,11 @@ defmodule Archethic.P2P.Message.ReplicateTransactionChain do
   Represents a message to initiate the replication of the transaction chain related to the given transaction
   """
   @enforce_keys [:transaction]
-  defstruct [:transaction, ack_storage?: false]
+  defstruct [:transaction]
 
   alias Archethic.TransactionChain.Transaction
 
   @type t :: %__MODULE__{
-          transaction: Transaction.t(),
-          ack_storage?: boolean()
+          transaction: Transaction.t()
         }
 end
