a4b02dc60fc223f32a8ce4c0b0417c1d3e623687
Prince Anuragi <prince@uniris.io>
2022-06-21 11:15:19 +0530

    Added Version Number for Running Archethic Version in Dashboard, Added Archethic Logo in Navbar

--- lib/archethic_web/live/dashboard_live.ex
+++ lib/archethic_web/live/dashboard_live.ex
@@ -11,7 +11,11 @@ defmodule ArchethicWeb.DashboardLive do
       Archethic.Metrics.Poller.monitor()
     end
 
-    {:ok, socket}
+    version = Application.spec(:archethic, :vsn)
+    applications = Application.spec(:archethic, :applications)
+    new_socket = socket |> assign(%{version: version, applications: applications})
+
+    {:ok, new_socket}
   end
 
   def handle_info({:update_data, data}, socket) do
