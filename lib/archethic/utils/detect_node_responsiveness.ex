ef5dc68270f723eeabbadef3f8990ccde2960834
Imnik11 <nitesh4y2001@gmail.com>
2022-06-23 19:34:17 +0530

    Removed PubSub & leveraging db instead

--- lib/archethic/utils/detect_node_responsiveness.ex
+++ lib/archethic/utils/detect_node_responsiveness.ex
@@ -3,36 +3,42 @@ defmodule Archethic.Utils.DetectNodeResponsiveness do
   Detects the nodes responsiveness based on timeouts
   """
   @default_timeout 3 * 1000
-  alias Archethic.PubSub
   alias Archethic.P2P
+  alias Archethic.DB
 
   use GenStateMachine
+  require Logger
 
   def start_link(address, replaying_fn) do
     GenStateMachine.start_link(__MODULE__, [address, replaying_fn], [])
   end
 
   def init([address, replaying_fn]) do
-    PubSub.register_to_new_transaction_by_address(address)
     schedule_timeout()
-    {:ok, :waiting, %{address: address, replaying_fn: replaying_fn, count: 0}}
+    {:ok, :waiting, %{address: address, replaying_fn: replaying_fn, count: 1}}
   end
 
-  def handle_event(:info, :soft_timeout, :waiting, %{replaying_fn: replaying_fn, count: count}) do
-    if count < length(P2P.authorized_and_available_nodes()) do
+  def handle_event(
+        :info,
+        :soft_timeout,
+        :waiting,
+        state = %{
+          address: address,
+          replaying_fn: replaying_fn,
+          count: count
+        }
+      ) do
+    with false <- DB.transaction_exists?(address),
+         true <- count < length(P2P.authorized_and_available_nodes()) do
+      Logger.error("calling replay fn with count=#{count}")
       replaying_fn.(count)
       schedule_timeout()
-      count = count + 1
-      {:keep_state, %{count: count, replaying_fn: replaying_fn}}
+      new_count = count + 1
+      {:keep_state, %{state | count: new_count}}
     else
       # hard_timeout
-
-      :stop
+      _ -> :stop
     end
-  end
-
-  def handle_event(:info, {:new_transaction, _, _, _}, :waiting, _) do
-    :stop
   end
 
   defp schedule_timeout(interval \\ @default_timeout, pid \\ self()) do
