f9a247e2f21e81ea1b079700fbace8e05f00529b
Samuel <samuel@uniris.io>
2022-08-18 09:38:34 +0200

    Add timestamp in the LastTransactionAddress message

--- lib/archethic/p2p/message/last_transaction_address.ex
+++ lib/archethic/p2p/message/last_transaction_address.ex
@@ -3,11 +3,12 @@ defmodule Archethic.P2P.Message.LastTransactionAddress do
   Represents a message with the last address key from a transaction chain
   """
   @enforce_keys [:address]
-  defstruct [:address]
+  defstruct [:address, :timestamp]
 
   alias Archethic.Crypto
 
   @type t :: %__MODULE__{
-          address: Crypto.versioned_hash()
+          address: Crypto.versioned_hash(),
+          timestamp: DateTime.t()
         }
 end
