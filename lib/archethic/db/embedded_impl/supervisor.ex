9a31dead041befed8fffe00e096364b7536c5990
Samuel <samuel@uniris.io>
2022-08-26 18:28:49 +0200

    Add writers pool

--- lib/archethic/db/embedded_impl/supervisor.ex
+++ lib/archethic/db/embedded_impl/supervisor.ex
@@ -8,7 +8,6 @@ defmodule Archethic.DB.EmbeddedImpl.Supervisor do
   alias Archethic.DB.EmbeddedImpl.ChainIndex
   alias Archethic.DB.EmbeddedImpl.P2PView
   alias Archethic.DB.EmbeddedImpl.StatsInfo
-  alias Archethic.DB.EmbeddedImpl.Queue
 
   require Logger
 
@@ -20,16 +19,28 @@ defmodule Archethic.DB.EmbeddedImpl.Supervisor do
     path = Archethic.DB.EmbeddedImpl.db_path()
     Logger.info("Load database at #{path}")
     File.mkdir_p!(path)
+    :ets.new(:archethic_db_chain_writers, [:named_table, :public])
 
     children = [
+      {DynamicSupervisor,
+       strategy: :one_for_one, name: Archethic.DB.EmbeddedImpl.ChainWriterSupervisor},
+      {Task, fn -> initialize_chain_writers(path) end},
       {ChainIndex, path: path},
-      {ChainWriter, path: path},
+      # {ChainWriter, path: path},
       {BootstrapInfo, path: path},
       {P2PView, path: path},
-      {StatsInfo, path: path},
-      Queue
+      {StatsInfo, path: path}
     ]
 
     Supervisor.init(children, strategy: :rest_for_one)
+  end
+
+  defp initialize_chain_writers(path) do
+    Enum.each(1..20, fn i ->
+      DynamicSupervisor.start_child(
+        Archethic.DB.EmbeddedImpl.ChainWriterSupervisor,
+        {ChainWriter, path: path, partition: i}
+      )
+    end)
   end
 end
