3a1ab427d9294ce4465cadbf85b4bf799d60c306
Apoorv-2204 <90304197+apoorv-2204@users.noreply.github.com>
2022-09-09 12:30:12 +0530

    enforce resync of network txs (#515)
    If there is quick disconnection and re-connection of a (authorised and available) node, it is a possibility that it will miss some oracle and node shared secrets transactions , which results in inconsistent node state.Post bootstrap we must resync node shared secrets and oracle txs so that current node state is same as other nodes state. So during validation it will not report invalid tx

--- test/support/transaction_factory.ex
+++ test/support/transaction_factory.ex
@@ -1,11 +1,12 @@
 defmodule Archethic.TransactionFactory do
   @moduledoc false
 
-  alias Archethic.Crypto
-
-  alias Archethic.Election
-
-  alias Archethic.Mining.Fee
+  alias Archethic.{
+    Crypto,
+    Election,
+    Mining.Fee,
+    SharedSecrets
+  }
 
   alias Archethic.TransactionChain
   alias Archethic.TransactionChain.Transaction
@@ -207,5 +208,52 @@ defmodule Archethic.TransactionFactory do
       )
 
     %{tx | validation_stamp: validation_stamp, cross_validation_stamps: [cross_validation_stamp]}
+  end
+
+  @doc """
+  Creates a valid Node Shared Secrets Tx with parameters index, timestamp, prev_txn
+  """
+  @spec create_network_tx(:node_shared_secrets, keyword) ::
+          Archethic.TransactionChain.Transaction.t()
+  def create_network_tx(_type = :node_shared_secrets, opts) do
+    inputs = Keyword.get(opts, :inputs, [])
+    seed = Keyword.get(opts, :seed, "daily_nonce_seed")
+    index = Keyword.get(opts, :index)
+    timestamp = Keyword.get(opts, :timestamp)
+    aes_key = :crypto.strong_rand_bytes(32)
+    prev_txn = Keyword.get(opts, :prev_txn, [])
+
+    tx =
+      SharedSecrets.new_node_shared_secrets_transaction(
+        [Crypto.last_node_public_key()],
+        seed,
+        aes_key,
+        index
+      )
+
+    ledger_operations =
+      %LedgerOperations{
+        fee: Fee.calculate(tx, 0.07),
+        transaction_movements: Transaction.get_movements(tx)
+      }
+      |> LedgerOperations.consume_inputs(tx.address, inputs)
+
+    validation_stamp =
+      %ValidationStamp{
+        timestamp: timestamp,
+        proof_of_work: Crypto.origin_node_public_key(),
+        proof_of_election: Election.validation_nodes_election_seed_sorting(tx, timestamp),
+        proof_of_integrity: TransactionChain.proof_of_integrity([tx | prev_txn]),
+        ledger_operations: ledger_operations
+      }
+      |> ValidationStamp.sign()
+
+    cross_validation_stamp = CrossValidationStamp.sign(%CrossValidationStamp{}, validation_stamp)
+
+    %{
+      tx
+      | validation_stamp: validation_stamp,
+        cross_validation_stamps: [cross_validation_stamp]
+    }
   end
 end
