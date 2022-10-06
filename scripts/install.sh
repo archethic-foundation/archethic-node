46187c73229d9928b94f66ff97d69b10c45922da
Samuel Manzanera <samuelmanzanera@users.noreply.github.com>
2022-10-06 15:54:19 +0200

    Fix OpenSSL installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -37,13 +37,13 @@ echo "Install OpenSSL"
 OPENSSL_VERSION=$(openssl version | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 if [[ $(version_to_int $OPENSSL_VERSION) -lt $(version_to_int "1.1.1") ]]; then
     cd /usr/local/src/
-    sudo wget  https://www.openssl.org/source/openssl-1.1.1g.tar.gz 
-    sudo tar -xf openssl-1.1.1g.tar.gz 
-    cd openssl-1.1.1g 
+    sudo wget https://www.openssl.org/source/openssl-1.1.1q.tar.gz 
+    sudo tar -xf openssl-1.1.1q.tar.gz 
+    cd openssl-1.1.1q 
     sudo ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl shared zlib
     sudo make
     sudo make install
-    suco bash -c 'echo "/usr/local/ssl/lib" >> /etc/ld.so.conf.d/openssl-1.1.1g.conf'
+    sudo bash -c 'echo "/usr/local/ssl/lib" >> /etc/ld.so.conf.d/openssl-1.1.1q.conf'
     sudo ldconfig
     sudo bash -c 'echo "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/local/ssl/bin" >> /etc/environment'
     sudo bash -c 'source /etc/environment'
