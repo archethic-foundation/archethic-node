b18a5054e581eccdfb02c5c7956ca481f9ab6b9a
Neylix <julien.leclerc05@protonmail.com>
2022-10-16 22:16:26 +0200

    Fix api web_hosting while using "/" in file source

--- lib/archethic_web/controllers/explorer_root_controller.ex
+++ lib/archethic_web/controllers/explorer_root_controller.ex
@@ -3,7 +3,33 @@ defmodule ArchethicWeb.ExplorerRootController do
 
   use ArchethicWeb, :controller
 
-  def index(conn, _params), do: redirect(conn, to: "/explorer")
+  # def index(conn, _params), do: redirect(conn, to: "/explorer")
+  def index(conn, _params) do
+    case get_web_hosting_address(conn) do
+      nil ->
+        redirect(conn, to: "/explorer")
+
+      address ->
+        path = Map.get(conn, :request_path)
+        redirect(conn, to: "/api/web_hosting/" <> address <> path)
+    end
+  end
 
   def return_404(conn, _params), do: send_resp(conn, 404, "Not found")
+
+  defp get_web_hosting_address(conn) do
+    case get_req_header(conn, "referer") do
+      [] ->
+        nil
+
+      [referer] ->
+        case Regex.scan(~r/(?<=\/api\/web_hosting\/).*(?!$)/, referer) do
+          [] ->
+            nil
+
+          [match] ->
+            List.first(match)
+        end
+    end
+  end
 end
