63eae264cef0a0525be1459572501e64f44ee069
Neylix <julien.leclerc05@protonmail.com>
2022-10-16 22:52:37 +0200

    Fix hosted website using controlled url

--- lib/archethic_web/controllers/aeweb_root_controller.ex
+++ lib/archethic_web/controllers/aeweb_root_controller.ex
@@ -5,12 +5,23 @@ defmodule ArchethicWeb.AEWebRootController do
 
   use ArchethicWeb, :controller
 
-  def index(conn, params) do
+  def index(conn, params = %{"url_path" => url_path}) do
     cache_headers = WebHostingController.get_cache_headers(conn)
 
     case WebHostingController.get_website(params, cache_headers) do
       {:ok, file_content, encodage, mime_type, cached?, etag} ->
         WebHostingController.send_response(conn, file_content, encodage, mime_type, cached?, etag)
+
+      {:error, :not_found} ->
+        # If file is not found, returning default file (url can be handled by index file)
+        case url_path do
+          [] ->
+            send_resp(conn, 404, "Not Found")
+
+          _path ->
+            params = Map.put(params, "url_path", [])
+            index(conn, params)
+        end
 
       _ ->
         send_resp(conn, 404, "Not Found")
