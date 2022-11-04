81c67e17dad9d1d8fde1bc1f776ecf066b18bd2e
Samuel Manzanera <samuelmanzanera@protonmail.com>
2022-11-04 15:49:37 +0100

    Specify length of beacon chain replication tree

--- lib/archethic/p2p/message.ex
+++ lib/archethic/p2p/message.ex
@@ -298,7 +298,8 @@ defmodule Archethic.P2P.Message do
         confirmed_validation_nodes: confirmed_validation_nodes
       }) do
     nb_validation_nodes = length(chain_replication_tree)
-    tree_size = chain_replication_tree |> List.first() |> bit_size()
+    chain_tree_size = chain_replication_tree |> List.first() |> bit_size()
+    beacon_tree_size = beacon_replication_tree |> List.first() |> bit_size()
 
     io_tree_size =
       case io_replication_tree do
@@ -312,9 +313,9 @@ defmodule Archethic.P2P.Message do
       end
 
     <<9::8, address::binary, ValidationStamp.serialize(stamp)::bitstring, nb_validation_nodes::8,
-      tree_size::8, :erlang.list_to_bitstring(chain_replication_tree)::bitstring,
-      :erlang.list_to_bitstring(beacon_replication_tree)::bitstring, io_tree_size::8,
-      :erlang.list_to_bitstring(io_replication_tree)::bitstring,
+      chain_tree_size::8, :erlang.list_to_bitstring(chain_replication_tree)::bitstring,
+      beacon_tree_size::8, :erlang.list_to_bitstring(beacon_replication_tree)::bitstring,
+      io_tree_size::8, :erlang.list_to_bitstring(io_replication_tree)::bitstring,
       bit_size(confirmed_validation_nodes)::8, confirmed_validation_nodes::bitstring>>
   end
 
@@ -749,12 +750,13 @@ defmodule Archethic.P2P.Message do
     {address, rest} = Utils.deserialize_address(rest)
     {validation_stamp, <<nb_validations::8, rest::bitstring>>} = ValidationStamp.deserialize(rest)
 
-    <<tree_size::8, rest::bitstring>> = rest
+    <<chain_tree_size::8, rest::bitstring>> = rest
 
-    {chain_tree, rest} = deserialize_bit_sequences(rest, nb_validations, tree_size, [])
+    {chain_tree, <<beacon_tree_size::8, rest::bitstring>>} =
+      deserialize_bit_sequences(rest, nb_validations, chain_tree_size, [])
 
     {beacon_tree, <<io_tree_size::8, rest::bitstring>>} =
-      deserialize_bit_sequences(rest, nb_validations, tree_size, [])
+      deserialize_bit_sequences(rest, nb_validations, beacon_tree_size, [])
 
     {io_tree, rest} =
       if io_tree_size > 0 do
