a9efd7035821b67182ee4f8a90e91d7d2ee7d03f
Hamza Belhaj <hamza.belhaj@jobteaser.com>
2022-11-22 15:45:37 +0100

    Remove old daily keys from ets table (#706)

--- lib/archethic/crypto/keystore/shared_secrets/software_impl.ex
+++ lib/archethic/crypto/keystore/shared_secrets/software_impl.ex
@@ -244,6 +244,7 @@ defmodule Archethic.Crypto.SharedSecretsKeystore.SoftwareImpl do
       end
 
       :ets.insert(@daily_keys, {DateTime.to_unix(timestamp), sign_daily_nonce_fun})
+      remove_older_daily_keys(DateTime.to_unix(timestamp))
 
       :ets.insert(@keystore_table, {:transaction_sign_fun, transaction_sign_fun})
       :ets.insert(@keystore_table, {:network_pool_sign_fun, network_pool_sign_fun})
@@ -253,6 +254,23 @@ defmodule Archethic.Crypto.SharedSecretsKeystore.SoftwareImpl do
       :ets.insert(@keystore_table, {:network_pool_seed_wrap_fun, network_pool_seed_wrap_fun})
 
       :ok
+    end
+  end
+
+  defp remove_older_daily_keys(unix_timestamp) do
+    case :ets.prev(@daily_keys, unix_timestamp) do
+      :"$end_of_table" ->
+        :ok
+
+      prev_unix_timestamp ->
+        # generate match pattern
+        # :ets.fun2ms(fn {key, _sign_function} -> key < prev_unix_timestamp end)
+
+        match_pattern = [
+          {{:"$1", :_}, [{:<, :"$1", prev_unix_timestamp}], [true]}
+        ]
+
+        :ets.select_delete(@daily_keys, match_pattern)
     end
   end
 
