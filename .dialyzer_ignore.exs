fcf3f32d0b1da5078f3c2af040fd10510a202b53
Samuel Manzanera <samuelmanzanera@protonmail.com>
2022-11-23 16:37:37 +0100

    Ignore unsolved dialyzer issues

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,2 +1,23 @@
 [
+  {"lib/archethic/contracts.ex:91:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/contracts.ex:119:no_return Function parse!/1 has no local return."},
+  {"lib/archethic/contracts.ex:120:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/contracts.ex:147:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/contracts.ex:166:unused_fun Function validate_conditions/2 will never be called."},
+  {"lib/archethic/contracts.ex:175:unused_fun Function validate_triggers/3 will never be called."},
+  {"lib/archethic/contracts.ex:188:unused_fun Function valid_from_trigger?/3 will never be called."},
+  {"lib/archethic/contracts/contract.ex:47:no_return Function from_transaction!/1 has no local return."},
+  {"lib/archethic/contracts/contract.ex:49:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/contracts/interpreter.ex:174:call The function call format_error_reason will not succeed."},
+  {"lib/archethic/contracts/interpreter.ex:209:pattern_match The pattern can never match the type {:error, {:unexpected_term, _}}."},
+  {"lib/archethic/contracts/interpreter.ex:220:call The function call parse will not succeed."},
+  {"lib/archethic/contracts/interpreter.ex:230:call The function call parse will not succeed."},
+  {"lib/archethic/mining/pending_transaction_validation.ex:198:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/mining/pending_transaction_validation.ex:208:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/mining/proof_of_work.ex:121:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/mining/validation_context.ex:763:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/validation_context.ex:806:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/validation_context.ex:814:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/validation_context.ex:1065:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/replication/transaction_validator.ex:79:guard_fail The guard clause can never succeed."}
 ]
