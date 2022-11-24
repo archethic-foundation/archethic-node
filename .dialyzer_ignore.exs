bb54462f929845a46c0a788497d9336cc0985b55
Samuel Manzanera <samuelmanzanera@protonmail.com>
2022-11-24 23:45:47 +0100

    updat e .dialyzer_ignore.exs

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -12,12 +12,15 @@
   {"lib/archethic/contracts/interpreter.ex:209:pattern_match The pattern can never match the type {:error, {:unexpected_term, _}}."},
   {"lib/archethic/contracts/interpreter.ex:220:call The function call parse will not succeed."},
   {"lib/archethic/contracts/interpreter.ex:230:call The function call parse will not succeed."},
-  {"lib/archethic/mining/pending_transaction_validation.ex:198:pattern_match The pattern can never match the type {:error, binary()}."},
-  {"lib/archethic/mining/pending_transaction_validation.ex:208:pattern_match The pattern can never match the type {:error, binary()}."},
   {"lib/archethic/mining/proof_of_work.ex:121:pattern_match The pattern can never match the type {:error, binary()}."},
-  {"lib/archethic/mining/validation_context.ex:763:guard_fail The guard clause can never succeed."},
-  {"lib/archethic/mining/validation_context.ex:806:guard_fail The guard clause can never succeed."},
-  {"lib/archethic/mining/validation_context.ex:814:guard_fail The guard clause can never succeed."},
-  {"lib/archethic/mining/validation_context.ex:1065:guard_fail The guard clause can never succeed."},
-  {"lib/archethic/replication/transaction_validator.ex:79:guard_fail The guard clause can never succeed."}
+  {"lib/archethic/mining/pending_transaction_validation.ex:238:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/mining/validation_context.ex:764:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/replication/transaction_validator.ex:80:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/pending_transaction_validation.ex:228:pattern_match The pattern can never match the type {:error, binary()}."},
+  {"lib/archethic/mining/validation_context.ex:807:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/validation_context.ex:815:guard_fail The guard clause can never succeed."},
+  {"lib/archethic/mining/validation_context.ex:1067:guard_fail The guard clause can never succeed."}
+
+
 ]
+
