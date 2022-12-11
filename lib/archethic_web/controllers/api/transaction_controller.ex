ec4d5dfe35597b386b5888033c12816f88d51a47
Neylix <julien.leclerc05@protonmail.com>
2022-12-11 12:40:39 +0100

    Fix test and credo

--- lib/archethic_web/controllers/api/transaction_controller.ex
+++ lib/archethic_web/controllers/api/transaction_controller.ex
@@ -42,17 +42,7 @@ defmodule ArchethicWeb.API.TransactionController do
         storage_nodes =
           Election.chain_storage_nodes(tx_address, P2P.authorized_and_available_nodes())
 
-        conflict_resolver = fn results ->
-          # Prioritize transactions results over not found
-          case Enum.filter(results, &match?(%TransactionSummary{}, &1)) do
-            [] ->
-              %NotFound{}
-
-            res ->
-              Enum.sort_by(res, & &1.timestamp, {:desc, DateTime})
-              |> List.first()
-          end
-        end
+        conflict_resolver = summary_conflict_resolver()
 
         case P2P.quorum_read(
                storage_nodes,
@@ -79,6 +69,20 @@ defmodule ArchethicWeb.API.TransactionController do
         |> put_status(400)
         |> put_view(ErrorView)
         |> render("400.json", changeset: changeset)
+    end
+  end
+
+  defp summary_conflict_resolver() do
+    fn results ->
+      # Prioritize transactions results over not found
+      case Enum.filter(results, &match?(%TransactionSummary{}, &1)) do
+        [] ->
+          %NotFound{}
+
+        res ->
+          Enum.sort_by(res, & &1.timestamp, {:desc, DateTime})
+          |> List.first()
+      end
     end
   end
 
