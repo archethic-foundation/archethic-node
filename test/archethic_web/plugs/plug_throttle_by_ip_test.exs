0467e08386e400efa8e29b87adaa84e58434690e
tenmoves <hamza.belhaj07@gmail.com>
2022-12-14 16:38:09 +0100

    refactored tests

---
+++ test/archethic_web/plugs/plug_throttle_by_ip_test.exs
@@ -0,0 +1,29 @@
+defmodule ArchethicWeb.PlugThrottleByIPTest do
+  use ArchethicWeb.ConnCase
+
+  use ArchethicCase, async: false
+
+  describe "plug attack should return forbidden when the user makes more than the authorized number of requests " do
+    @tag ratelimit: true
+    test "within timeframe with the same ip",
+         %{conn: conn} do
+      limit = Application.get_env(:archethic, :throttle)[:by_ip][:limit] + 1
+
+      is_rate_limited? =
+        Task.async_stream(
+          1..limit,
+          fn n ->
+            conn = get(conn, "/transaction/#{n}")
+            conn.status
+          end,
+          ordered: false
+        )
+        |> Enum.reduce_while(false, fn
+          {:ok, 403}, _acc -> {:halt, true}
+          _, acc -> {:cont, acc}
+        end)
+
+      assert is_rate_limited?
+    end
+  end
+end
