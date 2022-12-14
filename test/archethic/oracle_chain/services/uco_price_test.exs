98dc69cbf0dcb979e14f6b5fd25ceb4ff9df0f6f
Yan Guiborat <yan.guiborat@gmail.com>
2022-12-14 10:43:24 +0100

    Adding tests for error cases, formatting

--- test/archethic/oracle_chain/services/uco_price_test.exs
+++ test/archethic/oracle_chain/services/uco_price_test.exs
@@ -80,71 +80,109 @@ defmodule Archethic.OracleChain.Services.UCOPriceTest do
 
       assert false == UCOPrice.verify?(%{"eur" => 0.10, "usd" => 0.14})
     end
+  end
 
-    test "should return the median value when multiple providers queried" do
-      MockUCOPriceProvider1
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.20], "usd" => [0.12]}}
-      end)
+  test "should return the median value when multiple providers queried" do
+    MockUCOPriceProvider1
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.20], "usd" => [0.12]}}
+    end)
 
-      MockUCOPriceProvider2
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.30], "usd" => [0.12]}}
-      end)
+    MockUCOPriceProvider2
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.30], "usd" => [0.12]}}
+    end)
 
-      MockUCOPriceProvider3
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.40], "usd" => [0.12]}}
-      end)
+    MockUCOPriceProvider3
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.40], "usd" => [0.12]}}
+    end)
 
-      assert false == UCOPrice.verify?(%{"eur" => 0.30, "usd" => 0.12})
-    end
+    assert false == UCOPrice.verify?(%{"eur" => 0.30, "usd" => 0.12})
+  end
 
-    test "should return the average of median values when a even number of providers queried" do
-      ## Define a fourth mock to have even number of mocks
-      Mox.defmock(MockUCOPriceProvider4,
-        for: Archethic.OracleChain.Services.UCOPrice.Providers.Impl
-      )
+  test "should return the average of median values when a even number of providers queried" do
+    ## Define a fourth mock to have even number of mocks
+    Mox.defmock(MockUCOPriceProvider4,
+      for: Archethic.OracleChain.Services.UCOPrice.Providers.Impl
+    )
 
-      ## Backup old environment variable, and update it with fourth provider
-      old_env = Application.get_env(:archethic, Archethic.OracleChain.Services.UCOPrice)
+    ## Backup old environment variable, and update it with fourth provider
+    old_env = Application.get_env(:archethic, Archethic.OracleChain.Services.UCOPrice)
 
-      new_uco_env =
-        old_env
-        |> Keyword.replace(:providers, [
-          MockUCOPriceProvider1,
-          MockUCOPriceProvider2,
-          MockUCOPriceProvider3,
-          MockUCOPriceProvider4
-        ])
+    new_uco_env =
+      old_env
+      |> Keyword.replace(:providers, [
+        MockUCOPriceProvider1,
+        MockUCOPriceProvider2,
+        MockUCOPriceProvider3,
+        MockUCOPriceProvider4
+      ])
 
-      Application.put_env(:archethic, Archethic.OracleChain.Services.UCOPrice, new_uco_env)
+    Application.put_env(:archethic, Archethic.OracleChain.Services.UCOPrice, new_uco_env)
 
-      ## Define mocks expectations
-      MockUCOPriceProvider1
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.20], "usd" => [0.12]}}
-      end)
+    ## Define mocks expectations
+    MockUCOPriceProvider1
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.20], "usd" => [0.12]}}
+    end)
 
-      MockUCOPriceProvider2
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.30], "usd" => [0.12]}}
-      end)
+    MockUCOPriceProvider2
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.30], "usd" => [0.12]}}
+    end)
 
-      MockUCOPriceProvider3
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.40], "usd" => [0.12]}}
-      end)
+    MockUCOPriceProvider3
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.40], "usd" => [0.12]}}
+    end)
 
-      MockUCOPriceProvider4
-      |> expect(:fetch, fn _pairs ->
-        {:ok, %{"eur" => [0.50], "usd" => [0.12]}}
-      end)
+    MockUCOPriceProvider4
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.50], "usd" => [0.12]}}
+    end)
 
-      ## Restore original environment
-      Application.put_env(:archethic, Archethic.OracleChain.Services.UCOPrice, old_env)
+    ## Restore original environment
+    Application.put_env(:archethic, Archethic.OracleChain.Services.UCOPrice, old_env)
 
-      assert false == UCOPrice.verify?(%{"eur" => 0.35, "usd" => 0.12})
-    end
+    assert false == UCOPrice.verify?(%{"eur" => 0.35, "usd" => 0.12})
+  end
+
+  test "should return false when no data are returned from providers" do
+    MockUCOPriceProvider1
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [], "usd" => []}}
+    end)
+
+    MockUCOPriceProvider2
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [], "usd" => []}}
+    end)
+
+    MockUCOPriceProvider3
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [], "usd" => []}}
+    end)
+
+    assert false == UCOPrice.verify?(%{})
+  end
+
+  test "should report values even if a provider returns an error" do
+    MockUCOPriceProvider1
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.50], "usd" => [0.12]}}
+    end)
+
+    MockUCOPriceProvider2
+    |> expect(:fetch, fn _pairs ->
+      {:error, :error_message}
+    end)
+
+    MockUCOPriceProvider3
+    |> expect(:fetch, fn _pairs ->
+      {:ok, %{"eur" => [0.60], "usd" => [0.12]}}
+    end)
+
+    assert false == UCOPrice.verify?(%{"eur" => 0.55, "usd" => 0.12})
   end
 end
