00d630e3006ec5f3f150767437d36cef9a5f2dad
Neylix <julien.leclerc05@protonmail.com>
2023-01-18 19:49:32 +0100

    Update release script to handle prepare and upgrade

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,6 +6,7 @@ trap 'echo "******* FAILED *******" 1>&2' ERR
 USER=$(whoami)
 INSTALL_DIR=/home/$USER/aebot/build
 UPGRADE=0
+PREPARE=0
 SERVICE_CREATION=0
 
 usage() {
@@ -20,22 +21,23 @@ usage() {
   echo ""
 }
 
-while getopts :suhd: option
+while getopts :suphd: option
 do
-    case "${option}"
-    in
-        d) INSTALL_DIR=${OPTARG};;
-        u) UPGRADE=1;;
-        s) SERVICE_CREATION=1;;
-        h)
-          usage
-          exit 0
-          ;;
-        *)
-          usage
-          exit 1
-          ;;
-    esac
+  case "${option}"
+  in
+    d) INSTALL_DIR=${OPTARG};;
+    u) UPGRADE=1;;
+    p) PREPARE=1;;
+    s) SERVICE_CREATION=1;;
+    h)
+      usage
+      exit 0
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
 done
 shift $((OPTIND -1))
 
@@ -59,11 +61,25 @@ then
   asdf global nodejs $NODEJS_VERSION
 fi
 
+VERSION=$(grep 'version:' mix.exs | cut -d '"' -f2)
+echo ""
+echo "Version: ${VERSION}"
+echo "Installation dir: ${INSTALL_DIR}"
+
+export MIX_ENV=prod
+
+if [[ $UPGRADE == 1 ]]
+then
+  # Run upgrade
+  echo "Run the upgrade"
+  $INSTALL_DIR/bin/archethic_node upgrade $VERSION
+
+  exit
+fi
+
 # Install updated versions of hex/rebar
 mix local.rebar --force
 mix local.hex --if-missing --force
-
-export MIX_ENV=prod
 
 # Fetch deps and compile
 mix deps.get
@@ -75,81 +91,68 @@ cd ..
 mix assets.saas
 mix assets.deploy
 
-VERSION=$(grep 'version:' mix.exs | cut -d '"' -f2)
-echo ""
-echo "Version: ${VERSION}"
-echo "Installation dir: ${INSTALL_DIR}"
-
 mkdir -p $INSTALL_DIR
 
-if [ $UPGRADE == 1 ]
+if [[ $PREPARE == 1 ]]
 then
-    # Build upgrade releases
-    echo "Build the upgrade release"
-    MIX_ENV=prod mix distillery.release --upgrade
+  # Build upgrade releases
+  echo "Build the upgrade release"
+  MIX_ENV=prod mix distillery.release --upgrade
 
-    echo "Copy upgraded release into ${INSTALL_DIR}/releases/${VERSION}"
-    mkdir -p $INSTALL_DIR/releases/$VERSION
-    cp _build/prod/rel/archethic_node/releases/$VERSION/archethic_node.tar.gz $INSTALL_DIR/releases/$VERSION/archethic_node.tar.gz
-
-    echo "Run the upgrade"
-    $INSTALL_DIR/bin/archethic_node upgrade $VERSION
+  echo "Copy upgraded release into ${INSTALL_DIR}/releases/${VERSION}"
+  mkdir -p $INSTALL_DIR/releases/$VERSION
+  cp _build/prod/rel/archethic_node/releases/$VERSION/archethic_node.tar.gz $INSTALL_DIR/releases/$VERSION/archethic_node.tar.gz
 else
-    # Build and install the releases
+  # Build and install the releases
+  echo "Generate release"
+  MIX_ENV=prod mix distillery.release
 
-    echo "Generate release"
-    MIX_ENV=prod mix distillery.release
+  echo "Install release"
+  tar zxvf _build/prod/rel/archethic_node/releases/$VERSION/archethic_node.tar.gz -C $INSTALL_DIR
+  echo "Release has been installed on ${INSTALL_DIR}"
 
-    echo "Install release"
-    tar zxvf _build/prod/rel/archethic_node/releases/$VERSION/archethic_node.tar.gz -C $INSTALL_DIR
-    echo "Release has been installed on ${INSTALL_DIR}"
+  if [ $SERVICE_CREATION == 1 ]
+  then
+  
+  echo "Creating service file"
+  sudo bash -c 'cat > /etc/systemd/system/archethic.service' << EOF
 
-    if [ $SERVICE_CREATION == 1 ]
-    then
-    
-    echo "Creating service file"
-    sudo bash -c 'cat > /etc/systemd/system/archethic.service' << EOF
-
-    [Unit]
-    Description=ARCHEthic service
-    After=local-fs.target network.target
-    
-    [Service]
-    Type=simple
-    User=$USER
-    Group=$USER
-    
-    WorkingDirectory=$INSTALL_DIR
-    
-    ExecStart=$INSTALL_DIR/bin/archethic_node foreground
-    ExecStop=$INSTALL_DIR/bin/archethic_node stop
-    
-    EnvironmentFile=/etc/default/archethic.env
-    Environment=LANG=en_US.utf8
-    Environment=MIX_ENV=prod
-    Environment=ERLANG_COOKIE=$ERLANG_COOKIE
-    
-    Restart=on-failure
-    RemainAfterExit=yes
-    RestartSec=5
-    
-    LimitNOFILE=65535
-    UMask=0027
-    SyslogIdentifier=archethic
-    
-    [Install]
-    WantedBy=multi-user.target
+  [Unit]
+  Description=ARCHEthic service
+  After=local-fs.target network.target
+  
+  [Service]
+  Type=simple
+  User=$USER
+  Group=$USER
+  
+  WorkingDirectory=$INSTALL_DIR
+  
+  ExecStart=$INSTALL_DIR/bin/archethic_node foreground
+  ExecStop=$INSTALL_DIR/bin/archethic_node stop
+  
+  EnvironmentFile=/etc/default/archethic.env
+  Environment=LANG=en_US.utf8
+  Environment=MIX_ENV=prod
+  Environment=ERLANG_COOKIE=$ERLANG_COOKIE
+  
+  Restart=on-failure
+  RemainAfterExit=yes
+  RestartSec=5
+  
+  LimitNOFILE=65535
+  UMask=0027
+  SyslogIdentifier=archethic
+  
+  [Install]
+  WantedBy=multi-user.target
 EOF
 
-    # restart daemon, enable and start service
-    echo "Reloading daemon and enabling service"
-    sudo systemctl daemon-reload 
-    sudo systemctl enable archethic
-    sudo systemctl start archethic
-    echo "Service Started"
-    fi
-
-    
+  # restart daemon, enable
+  echo "Reloading daemon and enabling service"
+  sudo systemctl daemon-reload 
+  sudo systemctl enable archethic
+  fi
 fi
 
 exit
