a7232d75cd6f5357782beb92238b189d0d4e429b
Yan Guiborat <yan.guiborat@gmail.com>
2023-01-19 00:08:45 +0100

    Adding initial keys for hydrating caches

--- config/dev.exs
+++ config/dev.exs
@@ -77,6 +77,26 @@ config :archethic,
             Archethic.Crypto.NodeKeystore.Origin.TPMImpl
         end)
 
+config :archethic, Archethic.Utils.HydratingCache.CachesManager,
+  uco_service: [
+    {Archethic.OracleChain.Services.UCOPrice.Providers.CoinPaprika,
+     Archethic.OracleChain.Services.UCOPrice.Providers.CoinPaprika, :fetch, [["usd", "eur"]],
+     3000},
+    {Archethic.OracleChain.Services.UCOPrice.Providers.Coingecko,
+     Archethic.OracleChain.Services.UCOPrice.Providers.Coingecko, :fetch, [["usd", "eur"]], 3000},
+    {Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCap,
+     Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCap, :fetch, [["usd", "eur"]],
+     3000}
+  ],
+  coco_service: [
+    {:coin_paprika, Archethic.OracleChain.Services.UCOPrice.Providers.CoinPaprika, :fetch,
+     [["usd", "eur"]], 3000},
+    {:coin_gecko, Archethic.OracleChain.Services.UCOPrice.Providers.Coingecko, :fetch,
+     [["usd", "eur"]], 3000},
+    {:coin_market_cap, Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCap, :fetch,
+     [["usd", "eur"]], 3000}
+  ]
+
 config :archethic, Archethic.Governance.Pools,
   initial_members: [
     technical_council: [
