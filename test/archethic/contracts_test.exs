b292617a6bf036696dda0e9d68d02620b4ab8b26
apoorv-2204 <apoorva@uniris.io>
2023-01-23 13:55:45 +0530

    redo missed feature

--- test/archethic/contracts_test.exs
+++ test/archethic/contracts_test.exs
@@ -95,7 +95,7 @@ defmodule Archethic.ContractsTest do
       ]
 
       actions triggered_by: transaction do
-        add_uco_transfer to: "\3265CCD78CD74984FAB3CC6984D30C8C82044EBBAB1A4FFFB683BDB2D8C5BCF9", amount: 1000000000
+        add_uco_transfer to: \"3265CCD78CD74984FAB3CC6984D30C8C82044EBBAB1A4FFFB683BDB2D8C5BCF9\", amount: 1000000000
         set_content "hello"
         set_type transfer
       end
