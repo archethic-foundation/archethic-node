74bde29420471a999e09b4f7527189ca65931bca
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-02-07 15:44:10 +0100

    interpreter.execute_trigger/3 to remove dependency on version0 from worker

--- lib/archethic/contracts/interpreter/version1.ex
+++ lib/archethic/contracts/interpreter/version1.ex
@@ -3,6 +3,8 @@ defmodule Archethic.Contracts.Interpreter.Version1 do
 
   alias Archethic.Contracts.ContractConditions, as: Conditions
 
+  alias Archethic.TransactionChain.Transaction
+
   @spec parse(code :: binary(), {integer(), integer(), integer()}) :: {:error, binary()}
   def parse(code, {1, _, _}), do: parse_v1(code)
 
@@ -12,6 +14,15 @@ defmodule Archethic.Contracts.Interpreter.Version1 do
   @spec valid_conditions?(Conditions.t(), map()) :: bool()
   def valid_conditions?(_conditions, _constants) do
     false
+  end
+
+  @doc """
+  Execute the trigger/action code.
+  May return a new transaction or nil
+  """
+  @spec execute_trigger(Macro.t(), map()) :: Transaction.t() | nil
+  def execute_trigger(_ast, _constants) do
+    nil
   end
 
   defp parse_v1(code) when is_binary(code) do
