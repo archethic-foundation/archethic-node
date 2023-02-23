bec16d7c627cfa8c9d80a74e0466fde8c5332eca
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-02-23 15:35:59 +0100

    feedback: rename fetch_ to get_

--- lib/archethic/contracts/interpreter/version1/library/common/chain.ex
+++ lib/archethic/contracts/interpreter/version1/library/common/chain.ex
@@ -5,31 +5,31 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.Chain do
   alias Archethic.Contracts.Interpreter.ASTHelper, as: AST
   alias Archethic.Contracts.Interpreter.Version0
 
-  @spec fetch_genesis_address(binary()) :: binary()
-  defdelegate fetch_genesis_address(address),
+  @spec get_genesis_address(binary()) :: binary()
+  defdelegate get_genesis_address(address),
     to: Version0.Library,
     as: :get_genesis_address
 
-  @spec fetch_first_transaction_address(binary()) :: binary()
-  defdelegate fetch_first_transaction_address(address),
+  @spec get_first_transaction_address(binary()) :: binary()
+  defdelegate get_first_transaction_address(address),
     to: Version0.Library,
     as: :get_first_transaction_address
 
-  @spec fetch_genesis_public_key(binary()) :: binary()
-  defdelegate fetch_genesis_public_key(public_key),
+  @spec get_genesis_public_key(binary()) :: binary()
+  defdelegate get_genesis_public_key(public_key),
     to: Version0.Library,
     as: :get_genesis_public_key
 
   @spec check_types(atom(), list()) :: boolean()
-  def check_types(:fetch_genesis_address, [first]) do
+  def check_types(:get_genesis_address, [first]) do
     AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
   end
 
-  def check_types(:fetch_first_transaction_address, [first]) do
+  def check_types(:get_first_transaction_address, [first]) do
     AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
   end
 
-  def check_types(:fetch_genesis_public_key, [first]) do
+  def check_types(:get_genesis_public_key, [first]) do
     AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
   end
 
--- test/archethic/contracts/interpreter/version1/library/common/chain_test.exs
+++ test/archethic/contracts/interpreter/version1/library/common/chain_test.exs
@@ -55,14 +55,14 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.ChainTest do
   end
 
   # ----------------------------------------
-  describe "fetch_genesis_address/1" do
+  describe "get_genesis_address/1" do
     test "should work" do
       tx_address = <<0::8, 0::8, :crypto.strong_rand_bytes(32)::binary>>
       genesis_address = <<0::8, 0::8, :crypto.strong_rand_bytes(32)::binary>>
 
       code = ~s"""
       actions triggered_by: transaction do
-        Contract.set_content Chain.fetch_genesis_address("#{Base.encode16(tx_address)}")
+        Contract.set_content Chain.get_genesis_address("#{Base.encode16(tx_address)}")
       end
       """
 
@@ -78,14 +78,14 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.ChainTest do
   end
 
   # ----------------------------------------
-  describe "fetch_first_transaction_address/1" do
+  describe "get_first_transaction_address/1" do
     test "should work when there is a first transaction" do
       tx_address = <<0::8, 0::8, :crypto.strong_rand_bytes(32)::binary>>
       first_address = <<0::8, 0::8, :crypto.strong_rand_bytes(32)::binary>>
 
       code = ~s"""
       actions triggered_by: transaction do
-        Contract.set_content Chain.fetch_first_transaction_address("#{Base.encode16(tx_address)}")
+        Contract.set_content Chain.get_first_transaction_address("#{Base.encode16(tx_address)}")
       end
       """
 
@@ -105,7 +105,7 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.ChainTest do
 
       code = ~s"""
       actions triggered_by: transaction do
-        Contract.set_content Chain.fetch_first_transaction_address("#{Base.encode16(tx_address)}")
+        Contract.set_content Chain.get_first_transaction_address("#{Base.encode16(tx_address)}")
       end
       """
 
@@ -122,14 +122,14 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.ChainTest do
   end
 
   # ----------------------------------------
-  describe "fetch_genesis_public_key/1" do
+  describe "get_genesis_public_key/1" do
     test "should work" do
       {genesis_pub_key, _} = Crypto.generate_deterministic_keypair("seed")
       {pub_key, _} = Crypto.derive_keypair("seed", 19)
 
       code = ~s"""
       actions triggered_by: transaction do
-        Contract.set_content Chain.fetch_genesis_public_key("#{Base.encode16(pub_key)}")
+        Contract.set_content Chain.get_genesis_public_key("#{Base.encode16(pub_key)}")
       end
       """
 
