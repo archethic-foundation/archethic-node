bffa3620f72e637024ebc86cc14a5c88f0b06c1a
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-02-28 11:10:02 +0100

    add Map.get/3 Map.get/2 Map.set/3 library functions

--- lib/archethic/contracts/interpreter/version1/library/common/map.ex
+++ lib/archethic/contracts/interpreter/version1/library/common/map.ex
@@ -9,9 +9,35 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.Map do
     length(Map.keys(map))
   end
 
+  @spec get(map(), binary(), any()) :: any()
+  defdelegate get(map, key, default \\ nil),
+    to: Map
+
+  @spec set(map(), binary(), any()) :: map()
+  def set(map, key, value) do
+    Map.update(map, key, value, fn _ ->
+      value
+    end)
+  end
+
   @spec check_types(atom(), list()) :: boolean()
   def check_types(:size, [first]) do
     AST.is_map?(first) || AST.is_variable_or_function_call?(first)
+  end
+
+  def check_types(:get, [first, second]) do
+    (AST.is_map?(first) || AST.is_variable_or_function_call?(first)) &&
+      (AST.is_binary?(second) || AST.is_variable_or_function_call?(second))
+  end
+
+  def check_types(:get, [first, second, _third]) do
+    (AST.is_map?(first) || AST.is_variable_or_function_call?(first)) &&
+      (AST.is_binary?(second) || AST.is_variable_or_function_call?(second))
+  end
+
+  def check_types(:set, [first, second, _third]) do
+    (AST.is_map?(first) || AST.is_variable_or_function_call?(first)) &&
+      (AST.is_binary?(second) || AST.is_variable_or_function_call?(second))
   end
 
   def check_types(_, _), do: false
--- test/archethic/contracts/interpreter/version1/library/common/map_test.exs
+++ test/archethic/contracts/interpreter/version1/library/common/map_test.exs
@@ -28,6 +28,74 @@ defmodule Archethic.Contracts.Interpreter.Version1.Library.Common.MapTest do
     end
   end
 
+  # ----------------------------------------
+  describe "get/2" do
+    test "should return value when key exist" do
+      code = ~s"""
+      actions triggered_by: transaction do
+        numbers = [one: 1, two: 2, three: 3]
+        Contract.set_content Map.get(numbers, "one")
+      end
+      """
+
+      assert %Transaction{data: %TransactionData{content: "1"}} = sanitize_parse_execute(code)
+    end
+
+    test "should return nil when key not found" do
+      code = ~s"""
+      actions triggered_by: transaction do
+        numbers = [one: 1, two: 2, three: 3]
+        if nil == Map.get(numbers, "four") do
+          Contract.set_content "ok"
+        end
+      end
+      """
+
+      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+    end
+  end
+
+  # ----------------------------------------
+  describe "get/3" do
+    test "should return value when key exist" do
+      code = ~s"""
+      actions triggered_by: transaction do
+        numbers = [one: 1, two: 2, three: 3]
+        Contract.set_content Map.get(numbers, "one", 12)
+      end
+      """
+
+      assert %Transaction{data: %TransactionData{content: "1"}} = sanitize_parse_execute(code)
+    end
+
+    test "should return default when key not found" do
+      code = ~s"""
+      actions triggered_by: transaction do
+        numbers = [one: 1, two: 2, three: 3]
+        Contract.set_content Map.get(numbers, "four", 4)
+      end
+      """
+
+      assert %Transaction{data: %TransactionData{content: "4"}} = sanitize_parse_execute(code)
+    end
+  end
+
+  # ----------------------------------------
+  describe "set/3" do
+    test "should work" do
+      code = ~s"""
+      actions triggered_by: transaction do
+        numbers = [one: 1]
+        numbers = Map.set(numbers, "two", 2)
+        Contract.set_content Json.to_string(numbers)
+      end
+      """
+
+      assert %Transaction{data: %TransactionData{content: "{\"one\":1,\"two\":2}"}} =
+               sanitize_parse_execute(code)
+    end
+  end
+
   defp sanitize_parse_execute(code, constants \\ %{}) do
     with {:ok, sanitized_code} <- Interpreter.sanitize_code(code),
          {:ok, _, action_ast} <- ActionInterpreter.parse(sanitized_code) do
