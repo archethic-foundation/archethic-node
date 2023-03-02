f1965ea8a1817fdd10a7522065d3fff306ae0fb9
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-03-02 15:18:08 +0100

    300ms tests instead of 21s

--- test/archethic_cache/hydrating_cache_test.exs
+++ test/archethic_cache/hydrating_cache_test.exs
@@ -11,22 +11,17 @@ defmodule ArchethicCache.HydratingCacheTest do
   test "If value stored, it is returned immediatly" do
     {:ok, pid} = HydratingCache.start_link(:test_service_normal)
 
-    result =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          {:ok, 1}
-        end,
-        "simple_func",
-        10_000,
-        15_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, 1}
+      end,
+      "simple_func",
+      10_000,
+      :infinity
+    )
 
-    assert result == :ok
-    ## WAit a little to be sure value is registered and not being refreshed
-    :timer.sleep(500)
-    r = HydratingCache.get(pid, "simple_func", 10_000)
-    assert r == {:ok, 1}
+    assert {:ok, 1} = HydratingCache.get(pid, "simple_func")
   end
 
   test "Hydrating function runs periodically" do
@@ -34,179 +29,139 @@ defmodule ArchethicCache.HydratingCacheTest do
 
     :persistent_term.put("test", 1)
 
-    result =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          value = :persistent_term.get("test")
-          value = value + 1
-          :persistent_term.put("test", value)
-          {:ok, value}
-        end,
-        "test_inc",
-        1_000,
-        50_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        value = :persistent_term.get("test")
+        value = value + 1
+        :persistent_term.put("test", value)
+        {:ok, value}
+      end,
+      "test_inc",
+      10,
+      :infinity
+    )
 
-    assert result == :ok
-
-    :timer.sleep(3000)
-    {:ok, value} = HydratingCache.get(pid, "test_inc", 3000)
-
-    assert value >= 3
+    Process.sleep(50)
+    assert {:ok, value} = HydratingCache.get(pid, "test_inc")
+    assert value >= 5
   end
 
   test "Update hydrating function while another one is running returns new hydrating value from new function" do
     {:ok, pid} = HydratingCache.start_link(:test_service_hydrating)
 
-    result =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(5000)
-          {:ok, 1}
-        end,
-        "test_reregister",
-        10_000,
-        50_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(5000)
+        {:ok, 1}
+      end,
+      "test_reregister",
+      10_000,
+      :infinity
+    )
 
-    assert result == :ok
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, 2}
+      end,
+      "test_reregister",
+      10_000,
+      :infinity
+    )
 
-    _result =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          {:ok, 2}
-        end,
-        "test_reregister",
-        10_000,
-        50_000
-      )
-
-    :timer.sleep(5000)
-    {:ok, value} = HydratingCache.get(pid, "test_reregister", 4000)
-
-    assert value == 2
+    Process.sleep(50)
+    assert {:ok, 2} = HydratingCache.get(pid, "test_reregister")
   end
 
   test "Getting value while function is running and previous value is available returns value" do
     {:ok, pid} = HydratingCache.start_link(:test_service_running)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          {:ok, 1}
-        end,
-        "test_reregister",
-        40_000,
-        50_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, 1}
+      end,
+      "test_reregister",
+      10_000,
+      :infinity
+    )
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(5000)
-          {:ok, 2}
-        end,
-        "test_reregister",
-        40_000,
-        50_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(5000)
+        {:ok, 2}
+      end,
+      "test_reregister",
+      10_000,
+      :infinity
+    )
 
-    {:ok, value} = HydratingCache.get(pid, "test_reregister", 4000)
-
-    assert value == 1
+    assert {:ok, 1} = HydratingCache.get(pid, "test_reregister")
   end
 
   test "Two hydrating function can run at same time" do
     {:ok, pid} = HydratingCache.start_link(:test_service_simultaneous)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(5000)
-          {:ok, :result_timed}
-        end,
-        "timed_value",
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(5000)
+        {:ok, :result_timed}
+      end,
+      "timed_value",
+      10_000,
+      :infinity
+    )
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          {:ok, :result}
-        end,
-        "direct_value",
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, :result}
+      end,
+      "direct_value",
+      10_000,
+      :infinity
+    )
 
-    ## We query the value with timeout smaller than timed function
-    {:ok, _value} = HydratingCache.get(pid, "direct_value", 2000)
-  end
-
-  test "Querying key while first refreshed will block the calling process until refreshed and provide the value" do
-    {:ok, pid} = HydratingCache.start_link(:test_service_refresh)
-
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(4000)
-          {:ok, :valid_result}
-        end,
-        "delayed_result",
-        70_000,
-        80_000
-      )
-
-    ## We query the value with timeout smaller than timed function
-    assert {:ok, :valid_result} = HydratingCache.get(pid, "delayed_result", 5000)
+    assert {:ok, :result} = HydratingCache.get(pid, "direct_value")
   end
 
   test "Querying key while first refreshed will block the calling process until timeout" do
     {:ok, pid} = HydratingCache.start_link(:test_service_block)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(2000)
-          {:ok, :valid_result}
-        end,
-        "delayed_result",
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(5000)
+        {:ok, :valid_result}
+      end,
+      "delayed_result",
+      10_000,
+      :infinity
+    )
 
     ## We query the value with timeout smaller than timed function
-    assert {:error, :timeout} = HydratingCache.get(pid, "delayed_result", 1000)
+    assert {:error, :timeout} = HydratingCache.get(pid, "delayed_result", 1)
   end
 
   test "Multiple process can wait for a delayed value" do
     {:ok, pid} = HydratingCache.start_link(:test_service_delayed)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(2_000)
-          {:ok, :valid_result}
-        end,
-        "delayed_result",
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(100)
+        {:ok, :valid_result}
+      end,
+      "delayed_result",
+      10_000,
+      :infinity
+    )
 
-    ## We query the value with timeout smaller than timed function
-    results =
-      Task.async_stream(1..10, fn _ -> HydratingCache.get(pid, "delayed_result", 4000) end)
+    results = Task.async_stream(1..10, fn _ -> HydratingCache.get(pid, "delayed_result") end)
 
     assert Enum.all?(results, fn
              {:ok, {:ok, :valid_result}} -> true
@@ -217,35 +172,30 @@ defmodule ArchethicCache.HydratingCacheTest do
   test "If hydrating function crash, key fsm will still be operationnal" do
     {:ok, pid} = HydratingCache.start_link(:test_service_crash)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          ## Exit hydrating function
-          exit(1)
-          {:ok, :badmatch}
-        end,
-        :key,
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        ## Exit hydrating function
+        exit(1)
+      end,
+      :key,
+      10_000,
+      :infinity
+    )
 
-    :timer.sleep(1000)
+    Process.sleep(1)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          ## Trigger badmatch
-          {:ok, :value}
-        end,
-        :key,
-        70_000,
-        80_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, :value}
+      end,
+      :key,
+      10_000,
+      :infinity
+    )
 
-    result = HydratingCache.get(pid, :key, 3000)
-    assert result == {:ok, :value}
+    assert {:ok, :value} = HydratingCache.get(pid, :key)
   end
 
   ## This could occur if hydrating function takes time to answer.
@@ -254,85 +204,77 @@ defmodule ArchethicCache.HydratingCacheTest do
   test "value gets discarded after some time" do
     {:ok, pid} = HydratingCache.start_link(:test_service)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          case :persistent_term.get("flag", nil) do
-            1 ->
-              :timer.sleep(3_000)
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        case :persistent_term.get("flag", nil) do
+          1 ->
+            Process.sleep(5000)
 
-            nil ->
-              :persistent_term.put("flag", 1)
-              {:ok, :value}
-          end
-        end,
-        :key,
-        500,
-        1_000
-      )
+          nil ->
+            :persistent_term.put("flag", 1)
+            {:ok, :value}
+        end
+      end,
+      :key,
+      10,
+      20
+    )
 
-    :timer.sleep(1_100)
-    result = HydratingCache.get(pid, :key, 3000)
-    assert result == {:error, :discarded}
+    assert {:ok, :value} = HydratingCache.get(pid, :key)
+    Process.sleep(25)
+    assert {:error, :discarded} = HydratingCache.get(pid, :key)
   end
 
   test "Can get a value while another request is waiting for results" do
     {:ok, pid} = HydratingCache.start_link(:test_service_wait)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          :timer.sleep(10_000)
-          {:ok, :value}
-        end,
-        :key1,
-        30_000,
-        40_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        Process.sleep(5000)
+        {:ok, :value}
+      end,
+      :key1,
+      10_000,
+      :infinity
+    )
 
-    :erlang.spawn(fn -> HydratingCache.get(pid, :key1, 35_000) end)
+    :erlang.spawn(fn -> HydratingCache.get(pid, :key1, 15_000) end)
 
-    _ =
-      HydratingCache.register_function(
-        pid,
-        fn ->
-          {:ok, :value2}
-        end,
-        :key2,
-        500,
-        1_000
-      )
+    HydratingCache.register_function(
+      pid,
+      fn ->
+        {:ok, :value2}
+      end,
+      :key2,
+      10_000,
+      :infinity
+    )
 
-    result = HydratingCache.get(pid, :key2, 3000)
-    assert result == {:ok, :value2}
+    assert {:ok, :value2} = HydratingCache.get(pid, :key2)
   end
 
   test "can retrieve all values beside erroneous ones" do
     {:ok, pid} =
       HydratingCache.start_link(:test_service_get_all, [
-        {"key1", {__MODULE__, :val_hydrating_function, [10]}, 30_000, 40_000},
-        {"key2", {__MODULE__, :failval_hydrating_function, [20]}, 30_000, 40_000},
-        {"key3", {__MODULE__, :val_hydrating_function, [30]}, 30_000, 40_000}
+        {"key1", {__MODULE__, :val_hydrating_function, [10]}, 10_00, :infinity},
+        {"key2", {__MODULE__, :failval_hydrating_function, [20]}, 10_00, :infinity},
+        {"key3", {__MODULE__, :val_hydrating_function, [30]}, 10_00, :infinity}
       ])
 
-    :timer.sleep(1000)
-    values = HydratingCache.get_all(pid)
-    assert values == [10, 30]
+    assert [10, 30] = HydratingCache.get_all(pid)
   end
 
   test "Retrieving all values supports delayed values" do
     {:ok, pid} =
       HydratingCache.start_link(:test_service_get_all_delayed, [
-        {"key1", {__MODULE__, :val_hydrating_function, [10]}, 30_000, 40_000},
-        {"key2", {__MODULE__, :timed_hydrating_function, [2000, 20]}, 30_000, 40_000},
-        {"key3", {__MODULE__, :failval_hydrating_function, [30]}, 30_000, 40_000}
+        {"key1", {__MODULE__, :val_hydrating_function, [10]}, 10_000, :infinity},
+        {"key2", {__MODULE__, :timed_hydrating_function, [50, 20]}, 10_000, :infinity},
+        {"key3", {__MODULE__, :failval_hydrating_function, [30]}, 10_000, :infinity}
       ])
 
-    :timer.sleep(1000)
-    values = HydratingCache.get_all(pid)
-    assert values == [10, 20]
+    assert [10, 20] = HydratingCache.get_all(pid)
   end
 
   def val_hydrating_function(value) do
@@ -344,7 +286,7 @@ defmodule ArchethicCache.HydratingCacheTest do
   end
 
   def timed_hydrating_function(delay, value) do
-    :timer.sleep(delay)
+    Process.sleep(delay)
     {:ok, value}
   end
 end
