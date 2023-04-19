0518f6ffa8d39ae12ee23ac4b014a5122fc5fe6c
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-04-19 19:12:58 +0200

    add unit tests on extracted function

--- test/archethic/self_repair/self_repair_test.exs
+++ test/archethic/self_repair/self_repair_test.exs
@@ -7,16 +7,20 @@ defmodule Archethic.SelfRepairTest do
   alias Archethic.P2P.Client.DefaultImpl
 
   alias Archethic.Crypto
-
+  alias Archethic.P2P
   alias Archethic.P2P.Node
   alias Archethic.P2P.Message.GetNextAddresses
+  alias Archethic.P2P.Message.GetTransaction
 
   alias Archethic.TransactionChain.Transaction
   alias Archethic.TransactionChain.Transaction.ValidationStamp
 
+  alias Archethic.Replication
   alias Archethic.SelfRepair
 
+  import ArchethicCase
   import Mox
+  import Mock
 
   setup do
     start_supervised!({SummaryTimer, interval: "0 0 * * *"})
@@ -70,5 +74,93 @@ defmodule Archethic.SelfRepairTest do
     assert_receive :get_next_addresses
     assert_receive :add_alice3
     assert_receive :add_alice4
+  end
+
+  describe "replicate_transaction/2 (storage=chain)" do
+    setup do
+      P2P.add_and_connect_node(%Node{
+        ip: {127, 0, 0, 1},
+        port: 3000,
+        first_public_key: Crypto.first_node_public_key(),
+        last_public_key: Crypto.first_node_public_key(),
+        network_patch: "AAA",
+        geo_patch: "AAA",
+        available?: true,
+        authorized?: true,
+        authorization_date: ~U[2001-01-01 00:00:00Z]
+      })
+    end
+
+    test "should replicate a new transaction" do
+      address = random_address()
+
+      MockClient
+      |> expect(:send_message, fn _, %GetTransaction{address: ^address}, _ ->
+        {:ok, %Transaction{address: address}}
+      end)
+
+      with_mock(Replication, validate_and_store_transaction_chain: fn _, _, _ -> :ok end) do
+        assert :ok = SelfRepair.replicate_transaction(address, true)
+
+        assert_called(Replication.validate_and_store_transaction_chain(:_, :_, :_))
+      end
+    end
+
+    test "should not replicate an existing transaction" do
+      address = random_address()
+
+      MockDB
+      |> expect(:transaction_exists?, fn _, _ -> true end)
+
+      assert {:error, :transaction_already_exists} =
+               SelfRepair.replicate_transaction(address, true)
+    end
+  end
+
+  describe "replicate_transaction/2 (storage=io)" do
+    setup do
+      P2P.add_and_connect_node(%Node{
+        ip: {127, 0, 0, 1},
+        port: 3000,
+        first_public_key: Crypto.first_node_public_key(),
+        last_public_key: Crypto.first_node_public_key(),
+        network_patch: "AAA",
+        geo_patch: "AAA",
+        available?: true,
+        authorized?: true,
+        authorization_date: ~U[2001-01-01 00:00:00Z]
+      })
+    end
+
+    test "should replicate a new transaction" do
+      address = random_address()
+
+      MockClient
+      |> expect(:send_message, fn _, %GetTransaction{address: ^address}, _ ->
+        {:ok,
+         %Transaction{
+           address: address,
+           type: :transfer,
+           validation_stamp: %ValidationStamp{
+             timestamp: ~U[2023-04-01 00:00:00Z]
+           }
+         }}
+      end)
+
+      with_mock(Replication, validate_and_store_transaction: fn _, _ -> :ok end) do
+        assert :ok = SelfRepair.replicate_transaction(address, false)
+        assert_called(Replication.validate_and_store_transaction(:_, :_))
+      end
+    end
+
+    test "should not replicate an existing transaction" do
+      address = random_address()
+
+      MockDB
+      |> expect(:transaction_exists?, fn _, _ -> true end)
+
+      assert {:error, :transaction_already_exists} =
+               SelfRepair.replicate_transaction(address, false)
+    end
   end
 end
