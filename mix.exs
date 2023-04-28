5a6fd24443821a3652d88ab860604371369d6db7
Neylix <julien.leclerc05@protonmail.com>
2023-04-28 13:31:56 +0200

    Up version 1.0.8-rc1 and create appup

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.0.7",
+      version: "1.0.8-rc1",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
