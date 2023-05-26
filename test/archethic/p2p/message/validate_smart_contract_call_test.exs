ea98e0c5e702784f5905584cab62af6b0816ad62
Samuel Manzanera <samuelmanzanera@protonmail.com>
2023-05-26 15:20:40 +0200

    Fix format

--- test/archethic/p2p/message/validate_smart_contract_call_test.exs
+++ test/archethic/p2p/message/validate_smart_contract_call_test.exs
@@ -17,7 +17,6 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
   import Mox
 
   describe "process/1" do
-
     setup do
       P2P.add_and_connect_node(%Node{
         ip: {127, 0, 0, 1},
@@ -34,31 +33,30 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
     end
 
     test "should validate smart contract call and return valid message" do
-
       MockDB
       |> expect(:get_transaction, fn "@SC1", _, _ ->
-        {:ok, %Transaction{
-          data: %TransactionData{
-            code: ~s"""
-            @version 1
+        {:ok,
+         %Transaction{
+           data: %TransactionData{
+             code: ~s"""
+             @version 1
 
-            condition transaction: [
-              content: "hola"
-            ]
+             condition transaction: [
+               content: "hola"
+             ]
 
-            actions triggered_by: transaction do
-              Contract.set_content "hello"
-            end
-            """
-          }
-        }}
+             actions triggered_by: transaction do
+               Contract.set_content "hello"
+             end
+             """
+           }
+         }}
       end)
 
       MockClient
       |> expect(:send_message, fn _, %GetContractCalls{}, _ ->
         {:ok, %TransactionList{transactions: []}}
       end)
-
 
       incoming_tx = %Transaction{
         data: %TransactionData{
@@ -67,39 +65,40 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
         validation_stamp: %ValidationStamp{timestamp: DateTime.utc_now()}
       }
 
-      assert %SmartContractCallValidation{valid?: true} = %ValidateSmartContractCall{
-        contract_address: "@SC1",
-        transaction: incoming_tx,
-        inputs_before: DateTime.utc_now()
-      }
-      |> ValidateSmartContractCall.process()
+      assert %SmartContractCallValidation{valid?: true} =
+               %ValidateSmartContractCall{
+                 contract_address: "@SC1",
+                 transaction: incoming_tx,
+                 inputs_before: DateTime.utc_now()
+               }
+               |> ValidateSmartContractCall.process()
     end
 
     test "should validate smart contract call and return invalid message" do
       MockDB
       |> expect(:get_transaction, fn "@SC1", _, _ ->
-        {:ok, %Transaction{
-          data: %TransactionData{
-            code: ~s"""
-            @version 1
+        {:ok,
+         %Transaction{
+           data: %TransactionData{
+             code: ~s"""
+             @version 1
 
-            condition transaction: [
-              content: "hola"
-            ]
+             condition transaction: [
+               content: "hola"
+             ]
 
-            actions triggered_by: transaction do
-              Contract.set_content "hello"
-            end
-            """
-          }
-        }}
+             actions triggered_by: transaction do
+               Contract.set_content "hello"
+             end
+             """
+           }
+         }}
       end)
 
       MockClient
       |> expect(:send_message, fn _, %GetContractCalls{}, _ ->
         {:ok, %TransactionList{transactions: []}}
       end)
-
 
       incoming_tx = %Transaction{
         data: %TransactionData{
@@ -108,12 +107,13 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
         validation_stamp: %ValidationStamp{timestamp: DateTime.utc_now()}
       }
 
-      assert %SmartContractCallValidation{valid?: false} = %ValidateSmartContractCall{
-        contract_address: "@SC1",
-        transaction: incoming_tx,
-        inputs_before: DateTime.utc_now()
-      }
-      |> ValidateSmartContractCall.process()
+      assert %SmartContractCallValidation{valid?: false} =
+               %ValidateSmartContractCall{
+                 contract_address: "@SC1",
+                 transaction: incoming_tx,
+                 inputs_before: DateTime.utc_now()
+               }
+               |> ValidateSmartContractCall.process()
     end
 
     test "should validate smart contract call based on the inputs" do
@@ -124,57 +124,62 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
           validation_stamp: %ValidationStamp{timestamp: ~U[2023-05-23 15:30:27Z]}
         }
 
-        transactions = Enum.map(1..9, fn i ->
-          vote = if rem(i, 3) == 0 do
-            "X"
-          else
-            "Y"
-          end
+        transactions =
+          Enum.map(1..9, fn i ->
+            vote =
+              if rem(i, 3) == 0 do
+                "X"
+              else
+                "Y"
+              end
 
-          %Transaction{
-            data: %TransactionData{
-              content: "#{vote}"
-            },
-            validation_stamp: %ValidationStamp{timestamp: ~U[2023-05-23 15:30:27Z] |> DateTime.add(i) }
-          }
-        end)
+            %Transaction{
+              data: %TransactionData{
+                content: "#{vote}"
+              },
+              validation_stamp: %ValidationStamp{
+                timestamp: ~U[2023-05-23 15:30:27Z] |> DateTime.add(i)
+              }
+            }
+          end)
 
         {:ok, %TransactionList{transactions: transactions}}
       end)
 
       MockDB
       |> expect(:get_transaction, fn "@SC1", _, _ ->
-        {:ok, %Transaction{
-          data: %TransactionData{
-            code: ~S"""
-            @version 1
+        {:ok,
+         %Transaction{
+           data: %TransactionData{
+             code: ~S"""
+             @version 1
 
-            condition transaction: [
-              # Limit date: 2023-05-23 15:47:35
-              timestamp: Time.now() < 1684856867,
-              content: Regex.match?("^[X|Y]$")
-            ]
+             condition transaction: [
+               # Limit date: 2023-05-23 15:47:35
+               timestamp: Time.now() < 1684856867,
+               content: Regex.match?("^[X|Y]$")
+             ]
 
-            actions triggered_by: transaction do
+             actions triggered_by: transaction do
 
-              inputs = Contract.get_calls()
-              if List.size(inputs) > 9 do
-                vote_for_x = 0
-                vote_for_y = 0
+               inputs = Contract.get_calls()
+               if List.size(inputs) > 9 do
+                 vote_for_x = 0
+                 vote_for_y = 0
 
-                for input in inputs do
-                    if Regex.match?(input.content, "X") do
-                      vote_for_x = vote_for_x + 1
-                    else
-                      vote_for_y = vote_for_y + 1
-                    end
-                end
-                Contract.set_content "Votes results: X: #{String.from_number(vote_for_x)}; Y: #{String.from_number(vote_for_y)}"
-              end
-            end
-            """
-          }
-        }}
+                 for input in inputs do
+                     if Regex.match?(input.content, "X") do
+                       vote_for_x = vote_for_x + 1
+                     else
+                       vote_for_y = vote_for_y + 1
+                     end
+                 end
+                 Contract.set_content "Votes results: X: #{String.from_number(vote_for_x)}; Y: #{String.from_number(vote_for_y)}"
+               end
+             end
+             """
+           }
+         }}
       end)
 
       incoming_tx = %Transaction{
@@ -184,12 +189,13 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
         validation_stamp: %ValidationStamp{timestamp: ~U[2023-05-23 15:30:36Z]}
       }
 
-      assert %SmartContractCallValidation{valid?: true} = %ValidateSmartContractCall{
-        contract_address: "@SC1",
-        transaction: incoming_tx,
-        inputs_before: DateTime.utc_now()
-      }
-      |> ValidateSmartContractCall.process()
+      assert %SmartContractCallValidation{valid?: true} =
+               %ValidateSmartContractCall{
+                 contract_address: "@SC1",
+                 transaction: incoming_tx,
+                 inputs_before: DateTime.utc_now()
+               }
+               |> ValidateSmartContractCall.process()
     end
   end
 end
