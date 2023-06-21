ea3685b79242897800d3b37006c8a1f8fcd43813
Bastien CHAMAGNE <bastien@chamagne.fr>
2023-06-21 14:05:26 +0200

    Fix serialization that was adding milliseconds were it should not

--- test/archethic/contracts/contract/context_test.exs
+++ test/archethic/contracts/contract/context_test.exs
@@ -7,48 +7,48 @@ defmodule Archethic.Contracts.Contract.ContextTest do
 
   describe "serialization" do
     test "trigger=datetime" do
-      now = DateTime.utc_now() |> DateTime.truncate(:millisecond)
+      now = DateTime.utc_now()
 
       ctx = %Context{
         status: :no_output,
-        trigger: {:datetime, now},
-        timestamp: now
+        trigger: {:datetime, now |> DateTime.truncate(:second)},
+        timestamp: now |> DateTime.truncate(:millisecond)
       }
 
       assert {^ctx, <<>>} = ctx |> Context.serialize() |> Context.deserialize()
     end
 
     test "trigger=interval" do
-      now = DateTime.utc_now() |> DateTime.truncate(:millisecond)
+      now = DateTime.utc_now()
 
       ctx = %Context{
         status: :tx_output,
-        trigger: {:interval, "* */5 * * *", now},
-        timestamp: now
+        trigger: {:interval, "* */5 * * *", now |> DateTime.truncate(:second)},
+        timestamp: now |> DateTime.truncate(:millisecond)
       }
 
       assert {^ctx, <<>>} = ctx |> Context.serialize() |> Context.deserialize()
     end
 
     test "trigger=oracle" do
-      now = DateTime.utc_now() |> DateTime.truncate(:millisecond)
+      now = DateTime.utc_now()
 
       ctx = %Context{
         status: :failure,
         trigger: {:oracle, random_address()},
-        timestamp: now
+        timestamp: now |> DateTime.truncate(:millisecond)
       }
 
       assert {^ctx, <<>>} = ctx |> Context.serialize() |> Context.deserialize()
     end
 
     test "trigger=transaction" do
-      now = DateTime.utc_now() |> DateTime.truncate(:millisecond)
+      now = DateTime.utc_now()
 
       ctx = %Context{
         status: :tx_output,
         trigger: {:transaction, random_address()},
-        timestamp: now
+        timestamp: now |> DateTime.truncate(:millisecond)
       }
 
       assert {^ctx, <<>>} = ctx |> Context.serialize() |> Context.deserialize()
