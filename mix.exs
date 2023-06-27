dae55e77de65c1f1a1ba794d213b83a1da8d79a1
Neylix <julien.leclerc05@protonmail.com>
2023-06-27 15:39:00 +0200

    Up version 1.2.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.2.0",
+      version: "1.2.1",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
