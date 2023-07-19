5810196bc07cb16bef45023c2d80a445097eefbd
Bastien Chamagne <bastien@archethic.net>
2023-07-19 16:55:52 +0200

    Fix await confirmation + add protocol to ws handler

--- lib/archethic/utils/regression/playbook.ex
+++ lib/archethic/utils/regression/playbook.ex
@@ -318,7 +318,7 @@ defmodule Archethic.Utils.Regression.Playbook do
     )
 
     receive do
-      %{"transactionConfirmed" => %{"nbConfirmations" => 1}} ->
+      %{"transactionConfirmed" => %{"nbConfirmations" => n}} when n > 0 ->
         :ok
 
       %{"transactionError" => %{"reason" => reason}} ->
@@ -326,6 +326,9 @@ defmodule Archethic.Utils.Regression.Playbook do
 
       {:error, reason} ->
         {:error, reason}
+
+      unknown_msg ->
+        Logger.warn("await_replication received an unknown message: #{inspect(unknown_msg)}")
     end
   end
 
