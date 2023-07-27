57070b29a0aeb4a3ae987cfd3cfb463bf8b4c493
Risson <aime.risson.1@gmail.com>
2023-07-27 17:14:30 +0200

    scope implement context

--- lib/archethic/contracts/interpreter/scope.ex
+++ lib/archethic/contracts/interpreter/scope.ex
@@ -8,6 +8,8 @@ defmodule Archethic.Contracts.Interpreter.Scope do
   """
   @spec init(map()) :: :ok
   def init(global_variables \\ %{}) do
+    global_variables = Map.put(global_variables, "context_list", [])
+
     Process.put(
       :scope,
       global_variables
@@ -19,27 +21,109 @@ defmodule Archethic.Contracts.Interpreter.Scope do
   @doc """
   Create a new nested scope
   """
-  @spec create(list(String.t())) :: :ok
-  def create(scope_hierarchy) do
+  @spec create() :: :ok
+  def create() do
+    current_context = get_current_context()
+    current_scope_hierarchy = get_context_scope_hierarchy(current_context)
+    ref = new_ref()
+
     Process.put(
       :scope,
-      put_in(Process.get(:scope), scope_hierarchy, %{})
+      put_in(Process.get(:scope), [current_context] ++ current_scope_hierarchy ++ [ref], %{})
+    )
+
+    Process.put(
+      :scope,
+      update_in(Process.get(:scope), [current_context, "scope_hierarchy"], &(&1 ++ [ref]))
     )
 
     :ok
   end
 
   @doc """
+  Create new context
+  """
+  @spec create_context() :: :ok
+  def create_context() do
+    context_ref = new_ref()
+
+    new_context = %{
+      "scope_hierarchy" => []
+    }
+
+    # add context to scope and update context list
+    new_scope =
+      Process.get(:scope)
+      |> Map.put(context_ref, new_context)
+      |> Map.update!("context_list", &[context_ref | &1])
+
+    Process.put(:scope, new_scope)
+
+    :ok
+  end
+
+  @doc """
+  Leave a scope by removing it from current context's scope hierarchy and deleting its content
+  """
+  @spec leave_scope() :: :ok
+  def leave_scope() do
+    current_context = get_current_context()
+
+    Process.put(
+      :scope,
+      update_in(
+        Process.get(:scope),
+        [current_context, "scope_hierarchy"],
+        &List.delete_at(&1, -1)
+      )
+    )
+
+    :ok
+  end
+
+  @doc """
+  Leave a context by removing it from scope and context_list
+  """
+  @spec leave_context() :: :ok
+  def leave_context() do
+    current_context = get_current_context()
+
+    new_scope =
+      Process.get(:scope)
+      |> Map.delete(current_context)
+      |> Map.update!("context_list", &List.delete_at(&1, 0))
+
+    Process.put(:scope, new_scope)
+
+    :ok
+  end
+
+  @doc """
+  Execute ast after creating specific context for it and return execution's result
+  """
+  @spec execute(any(), map()) :: :ok
+  def execute(ast, constants) do
+    init(constants)
+    create_context()
+    {result, _} = Code.eval_quoted(ast)
+    leave_context()
+    result
+  end
+
+  @doc """
   Write the variable in the most relevant scope (cascade to all parent)
   Fallback to current scope if variable doesn't exist anywhere
   """
-  @spec write_cascade(list(String.t()), String.t(), any()) :: :ok
-  def write_cascade(scope_hierarchy, var_name, value) do
+  @spec write_cascade(String.t(), any()) :: :ok
+  def write_cascade(var_name, value) do
+    current_context = get_current_context()
+    context_scope_hierarchy = get_context_scope_hierarchy(current_context)
+
     Process.put(
       :scope,
       put_in(
         Process.get(:scope),
-        where_is(scope_hierarchy, var_name) ++ [var_name],
+        where_is(current_context, context_scope_hierarchy, var_name) ++ [var_name],
         value
       )
     )
@@ -48,15 +132,18 @@ defmodule Archethic.Contracts.Interpreter.Scope do
   end
 
   @doc """
-  Write the variable at given scope
+  Write the variable at given context's scope
   """
-  @spec write_at(list(String.t()), String.t(), any()) :: :ok
-  def write_at(scope_hierarchy, var_name, value) do
+  @spec write_at(String.t(), any()) :: :ok
+  def write_at(var_name, value) do
+    current_context = get_current_context()
+    current_scope_hierarchy = get_context_scope_hierarchy(current_context)
+
     Process.put(
       :scope,
       put_in(
         Process.get(:scope),
-        scope_hierarchy ++ [var_name],
+        [current_context] ++ current_scope_hierarchy ++ [var_name],
         value
       )
     )
@@ -93,25 +180,45 @@ defmodule Archethic.Contracts.Interpreter.Scope do
   end
 
   @doc """
-  Read the variable starting at given scope and cascading until the root
+  Read the variable from current context's scope
+  and cascading until the global scope (avoiding context's root)
   """
-  @spec read(list(String.t()), String.t()) :: any()
-  def read(scopes_hierarchy, var_name) do
+  @spec read(String.t()) :: any()
+  def read(var_name) do
+    current_context = get_current_context()
+    scope_hierarchy = get_context_scope_hierarchy(current_context)
+
     get_in(
       Process.get(:scope),
-      where_is(scopes_hierarchy, var_name) ++ [var_name]
+      where_is(current_context, scope_hierarchy, var_name) ++ [var_name]
     )
   end
 
   @doc """
-  Read the map's property starting at given scope and cascading until the root
+  Read the map's property starting at given context's scope and cascading until the root
   """
-  @spec read(list(String.t()), String.t(), String.t()) :: any()
-  def read(scopes_hierarchy, map_name, key_name) do
+  @spec read(String.t(), String.t()) :: any()
+  def read(map_name, key_name) do
+    current_context = get_current_context()
+    current_scope_hierarchy = get_context_scope_hierarchy(current_context)
+
     get_in(
       Process.get(:scope),
-      where_is(scopes_hierarchy, map_name) ++ [map_name, key_name]
+      where_is(current_context, current_scope_hierarchy, map_name) ++ [map_name, key_name]
     )
+  end
+
+  defp get_current_context() do
+    get_in(Process.get(:scope), ["context_list"])
+    |> List.first()
+  end
+
+  defp get_context_scope_hierarchy(context) do
+    get_in(Process.get(:scope), [context, "scope_hierarchy"])
+  end
+
+  defp new_ref() do
+    :erlang.list_to_binary(:erlang.ref_to_list(make_ref()))
   end
 
   defp get_function_ast(function_name, args) do
@@ -123,36 +230,39 @@ defmodule Archethic.Contracts.Interpreter.Scope do
   """
   @spec execute_function_ast(String.t(), list(any())) :: any()
   def execute_function_ast(function_name, args) do
-    get_function_ast(function_name, args)
-    |> Code.eval_quoted()
-    |> elem(0)
+    create_context()
+
+    result =
+      get_function_ast(function_name, args)
+      |> Code.eval_quoted()
+      |> elem(0)
+
+    leave_context()
+    result
   end
 
   # Return the path where to assign/read a variable.
   # It will recurse from the deepest path to the root path until it finds a match.
   # If no match it will return the current path.
-  defp where_is(current_path, variable_name) do
-    do_where_is(current_path, variable_name, current_path)
-  end
-
-  defp do_where_is(current_path, variable_name, []) do
-    # there are magic variables at the root of scope (contract/transaction/next/previous)
-    case get_in(Process.get(:scope), [variable_name]) do
-      nil ->
-        current_path
-
-      _ ->
-        []
+  defp where_is(context, current_path, variable_name) do
+    case do_where_is(context, variable_name, current_path) do
+      nil -> [context] ++ current_path
+      path -> path
     end
   end
 
-  defp do_where_is(current_path, variable_name, acc) do
-    case get_in(Process.get(:scope), acc ++ [variable_name]) do
-      nil ->
-        do_where_is(current_path, variable_name, List.delete_at(acc, -1))
+  defp do_where_is(_context, variable_name, []) do
+    # there are magic variables at the root of scope (contract/transaction/next/previous)
+    case get_in(Process.get(:scope), [variable_name]) do
+      nil -> nil
+      _ -> []
+    end
+  end
 
-      _ ->
-        acc
+  defp do_where_is(context, variable_name, acc) do
+    case get_in(Process.get(:scope), [context] ++ acc ++ [variable_name]) do
+      nil -> do_where_is(context, variable_name, List.delete_at(acc, -1))
+      _ -> [context] ++ acc
     end
   end
 end
