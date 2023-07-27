2c55838a798933e32f7583ed26ce4d54266b564a
Neylix <julien.leclerc05@protonmail.com>
2023-07-27 16:21:33 +0200

    Add new Code module in library

---
+++ lib/archethic/contracts/interpreter/library/common/code.ex
@@ -0,0 +1,35 @@
+defmodule Archethic.Contracts.Interpreter.Library.Common.Code do
+  @moduledoc false
+  @behaviour Archethic.Contracts.Interpreter.Library
+
+  alias Archethic.Contracts.Interpreter
+  alias Archethic.Contracts.Interpreter.ASTHelper, as: AST
+
+  @spec is_same?(binary(), binary()) :: boolean()
+  def is_same?(first_code, second_code) do
+    first_ast = first_code |> Interpreter.sanitize_code(ignore_meta?: true)
+    second_ast = second_code |> Interpreter.sanitize_code(ignore_meta?: true)
+
+    first_ast == second_ast
+  end
+
+  @spec is_valid?(binary()) :: boolean()
+  def is_valid?(code) do
+    case Interpreter.parse(code) do
+      {:ok, _} -> true
+      {:error, _} -> false
+    end
+  end
+
+  @spec check_types(atom(), list()) :: boolean()
+  def check_types(:is_same?, [first, second]) do
+    (AST.is_binary?(first) || AST.is_variable_or_function_call?(first)) &&
+      (AST.is_binary?(second) || AST.is_variable_or_function_call?(second))
+  end
+
+  def check_types(:is_valid?, [first]) do
+    AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
+  end
+
+  def check_types(_, _), do: false
+end
