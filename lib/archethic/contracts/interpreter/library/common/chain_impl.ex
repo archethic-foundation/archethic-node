f0c7660933f195ccf5615e48ddb54b925eeaf487
bchamagne <74045243+bchamagne@users.noreply.github.com>
2023-07-28 08:43:14 +0200

    Add Chain.get_transaction/1 (#1189)
    * Add Chain.get_transaction/1
    
    * Chain functions return nil instead of crashing
    
    * Add mocks to the unit tests
    
    * use Mock.expect instead of Mock.stub

--- lib/archethic/contracts/interpreter/library/common/chain_impl.ex
+++ lib/archethic/contracts/interpreter/library/common/chain_impl.ex
@@ -3,6 +3,8 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
   @behaviour Archethic.Contracts.Interpreter.Library.Common.Chain
 
   alias Archethic.Contracts.Interpreter.Legacy
+  alias Archethic.Contracts.Interpreter.Legacy.UtilsInterpreter
+  alias Archethic.Contracts.ContractConstants, as: Constants
 
   @impl Archethic.Contracts.Interpreter.Library.Common.Chain
   defdelegate get_genesis_address(address),
@@ -10,12 +12,34 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
     as: :get_genesis_address
 
   @impl Archethic.Contracts.Interpreter.Library.Common.Chain
-  defdelegate get_first_transaction_address(address),
-    to: Legacy.Library,
-    as: :get_first_transaction_address
+  def get_first_transaction_address(address) do
+    try do
+      Legacy.Library.get_first_transaction_address(address)
+    rescue
+      _ -> nil
+    end
+  end
 
   @impl Archethic.Contracts.Interpreter.Library.Common.Chain
-  defdelegate get_genesis_public_key(public_key),
-    to: Legacy.Library,
-    as: :get_genesis_public_key
+  def get_genesis_public_key(public_key) do
+    try do
+      Legacy.Library.get_genesis_public_key(public_key)
+    rescue
+      _ -> nil
+    end
+  end
+
+  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  def get_transaction(address) do
+    address
+    |> UtilsInterpreter.get_address("Chain.get_transaction/1")
+    |> Archethic.search_transaction()
+    |> then(fn
+      {:ok, tx} ->
+        Constants.from_transaction(tx)
+
+      {:error, _} ->
+        nil
+    end)
+  end
 end
