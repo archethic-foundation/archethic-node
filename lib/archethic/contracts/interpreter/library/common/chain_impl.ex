4f976d5f76cc9203cb395dfb89fa816eda41c9f1
Neylix <julien.leclerc05@protonmail.com>
2023-07-28 18:09:31 +0200

    Add new library function Chain.get_burn_address

--- lib/archethic/contracts/interpreter/library/common/chain_impl.ex
+++ lib/archethic/contracts/interpreter/library/common/chain_impl.ex
@@ -1,17 +1,21 @@
 defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
   @moduledoc false
-  @behaviour Archethic.Contracts.Interpreter.Library.Common.Chain
 
   alias Archethic.Contracts.Interpreter.Legacy
+  alias Archethic.Contracts.Interpreter.Library.Common.Chain
   alias Archethic.Contracts.Interpreter.Legacy.UtilsInterpreter
   alias Archethic.Contracts.ContractConstants, as: Constants
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  alias Archethic.TransactionChain.Transaction.ValidationStamp.LedgerOperations
+
+  @behaviour Chain
+
+  @impl Chain
   defdelegate get_genesis_address(address),
     to: Legacy.Library,
     as: :get_genesis_address
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Chain
   def get_first_transaction_address(address) do
     try do
       Legacy.Library.get_first_transaction_address(address)
@@ -20,7 +24,7 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
     end
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Chain
   def get_genesis_public_key(public_key) do
     try do
       Legacy.Library.get_genesis_public_key(public_key)
@@ -29,7 +33,7 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
     end
   end
 
-  @impl Archethic.Contracts.Interpreter.Library.Common.Chain
+  @impl Chain
   def get_transaction(address) do
     address
     |> UtilsInterpreter.get_address("Chain.get_transaction/1")
@@ -42,4 +46,7 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.ChainImpl do
         nil
     end)
   end
+
+  @impl Chain
+  def get_burn_address(), do: LedgerOperations.burning_address() |> Base.encode16()
 end
