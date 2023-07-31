91292512258e379b30d3ababadfa1996d6b67dfb
Neylix <julien.leclerc05@protonmail.com>
2023-07-31 09:39:19 +0200

    Add keccak256 algo in SC Crypto.hash library

--- test/archethic/contracts/interpreter/library/common/crypto_test.exs
+++ test/archethic/contracts/interpreter/library/common/crypto_test.exs
@@ -5,12 +5,8 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.CryptoTest do
   """
 
   use ArchethicCase
-  import ArchethicCase
 
   alias Archethic.Contracts.Interpreter.Library.Common.Crypto
-
-  alias Archethic.TransactionChain.Transaction
-  alias Archethic.TransactionChain.TransactionData
 
   doctest Crypto
 
@@ -18,30 +14,24 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.CryptoTest do
   describe "hash/1" do
     test "should work without algo" do
       text = "wu-tang"
-
-      code = ~s"""
-      actions triggered_by: transaction do
-        Contract.set_content Crypto.hash("#{text}")
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: content}} = sanitize_parse_execute(code)
-      assert content == Base.encode16(:crypto.hash(:sha256, text))
+      assert Crypto.hash(text) == Base.encode16(:crypto.hash(:sha256, text))
     end
   end
 
   describe "hash/2" do
     test "should work with algo" do
       text = "wu-tang"
+      assert Crypto.hash(text, "sha512") == Base.encode16(:crypto.hash(:sha512, text))
+    end
 
-      code = ~s"""
-      actions triggered_by: transaction do
-        Contract.set_content Crypto.hash("#{text}", "sha512")
-      end
-      """
+    test "should work with keccak256" do
+      text = "wu-tang"
 
-      assert %Transaction{data: %TransactionData{content: content}} = sanitize_parse_execute(code)
-      assert content == Base.encode16(:crypto.hash(:sha512, text))
+      # Here directly use string to ensure ExKeccak dependency still works
+      expected_hash = "CBB3D06AFFDADC9F6B5949E13AA7B06303731811452F61C6F3851F325CBE9D3E"
+
+      assert ExKeccak.hash_256(text) |> Base.encode16() == expected_hash
+      assert Crypto.hash(text, "keccak256") == expected_hash
     end
   end
 end
