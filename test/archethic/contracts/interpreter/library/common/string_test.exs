ea27178c1848ee27c0ff48058e4df95cecc4c695
Neylix <julien.leclerc05@protonmail.com>
2023-08-03 15:11:21 +0200

    refactor String tests

--- test/archethic/contracts/interpreter/library/common/string_test.exs
+++ test/archethic/contracts/interpreter/library/common/string_test.exs
@@ -5,133 +5,56 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.StringTest do
   """
 
   use ArchethicCase
-  import ArchethicCase
 
   alias Archethic.Contracts.Interpreter.Library.Common.String
-
-  alias Archethic.TransactionChain.Transaction
-  alias Archethic.TransactionChain.TransactionData
 
   doctest String
 
   # ----------------------------------------
   describe "size/1" do
-    test "should work" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        Contract.set_content String.size("hello")
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "5"}} = sanitize_parse_execute(code)
+    test "should return the size of a string" do
+      assert String.size("hello") == 5
     end
   end
 
   # ----------------------------------------
   describe "in?/1" do
-    test "should work" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.in?("bob,alice", "bob") do
-          Contract.set_content "ok"
-        else
-          Contract.set_content "ko"
-        end
-      end
-      """
+    test "should return true if a string contains another string" do
+      assert String.in?("bob,alice", "bob")
+    end
 
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
-
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.in?("bob,alice", "robert") do
-          Contract.set_content "ko"
-        else
-          Contract.set_content "ok"
-
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+    test "shoudl return false if a string does not contain another string" do
+      refute String.in?("bob,alice", "robert")
     end
   end
 
   # ----------------------------------------
   describe "to_number/1" do
     test "should parse integer" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.to_number("14") == 14 do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert 14 == String.to_number("14")
     end
 
     test "should parse float" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.to_number("14.1") == 14.1 do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert String.to_number("14.1") == 14.1
     end
 
     test "should return nil if not a number" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.to_number("bob") == nil do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert String.to_number("bob") == nil
     end
   end
 
   # ----------------------------------------
   describe "from_number/1" do
     test "should convert int" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.from_number(14) == "14" do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert String.from_number(14) == "14"
     end
 
     test "should convert float" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.from_number(14.1) == "14.1" do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert String.from_number(14.1) == "14.1"
     end
 
     test "should display float as int if possible" do
-      code = ~s"""
-      actions triggered_by: transaction do
-        if String.from_number(14.0) == "14" do
-          Contract.set_content "ok"
-        end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+      assert String.from_number(14.0) == "14"
     end
   end
 
@@ -149,99 +72,15 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.StringTest do
 
   # ----------------------------------------
   describe "to_uppercase/1" do
-    test "should work on a string" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_uppercase("IiIiIiIiIiII")
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "IIIIIIIIIIII"}} =
-               sanitize_parse_execute(code)
-    end
-
-    test "should work on a variable" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          var = "IiIiIiIiIiII"
-          Contract.set_content String.to_uppercase(var)
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "IIIIIIIIIIII"}} =
-               sanitize_parse_execute(code)
-    end
-
-    test "should not parse if compiler knows there is a type error" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_uppercase(123)
-      end
-      """
-
-      {:error, _, "invalid function arguments"} = sanitize_parse_execute(code)
-    end
-
-    test "should raise when not a string" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          var = 123
-          Contract.set_content String.to_uppercase(var)
-      end
-      """
-
-      assert_raise(FunctionClauseError, fn ->
-        sanitize_parse_execute(code)
-      end)
+    test "should convert string to uppercase" do
+      assert String.to_uppercase("IiIiIiIiIiII") == "IIIIIIIIIIII"
     end
   end
 
   # ----------------------------------------
   describe "to_lowercase/1" do
-    test "should work on a string" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_lowercase("IiIiIiIiIiII")
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "iiiiiiiiiiii"}} =
-               sanitize_parse_execute(code)
-    end
-
-    test "should work on a variable" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          var = "IiIiIiIiIiII"
-          Contract.set_content String.to_lowercase(var)
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "iiiiiiiiiiii"}} =
-               sanitize_parse_execute(code)
-    end
-
-    test "should not parse if compiler knows there is a type error" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_lowercase(123)
-      end
-      """
-
-      {:error, _, "invalid function arguments"} = sanitize_parse_execute(code)
-    end
-
-    test "should raise when not a string" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          var = 123
-          Contract.set_content String.to_lowercase(var)
-      end
-      """
-
-      assert_raise(FunctionClauseError, fn ->
-        sanitize_parse_execute(code)
-      end)
+    test "should convert string to lowercase" do
+      assert String.to_lowercase("IiIiIiIiIiII") == "iiiiiiiiiiii"
     end
   end
 end
