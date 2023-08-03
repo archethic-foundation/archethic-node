d6947a0fe65714c705b7156367ae586c1ed98c73
Neylix <julien.leclerc05@protonmail.com>
2023-08-03 15:03:22 +0200

    String.to_hex convert any string to hex

--- test/archethic/contracts/interpreter/library/common/string_test.exs
+++ test/archethic/contracts/interpreter/library/common/string_test.exs
@@ -8,7 +8,6 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.StringTest do
   import ArchethicCase
 
   alias Archethic.Contracts.Interpreter.Library.Common.String
-  alias Archethic.Contracts.ContractConstants, as: Constants
 
   alias Archethic.TransactionChain.Transaction
   alias Archethic.TransactionChain.TransactionData
@@ -138,119 +137,13 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.StringTest do
 
   # ----------------------------------------
   describe "to_hex/1" do
-    test "should work on a string" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_hex("abc123def456")
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ABC123DEF456"}} =
-               sanitize_parse_execute(code)
+    test "should convert string to hex" do
+      assert "77696C6C206265636F6D6520686578" == String.to_hex("will become hex")
     end
 
-    test "should work on a variable" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          var = "abc123def456"
-          Contract.set_content String.to_hex(var)
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ABC123DEF456"}} =
-               sanitize_parse_execute(code)
-    end
-
-    test "should work on a constant" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_hex(transaction.address)
-      end
-      """
-
-      address = random_address()
-
-      trigger_tx = %Transaction{
-        type: :data,
-        address: address,
-        data: %TransactionData{}
-      }
-
-      assert %Transaction{data: %TransactionData{content: hex}} =
-               sanitize_parse_execute(code, %{
-                 "transaction" => Constants.from_transaction(trigger_tx)
-               })
-
-      assert address == Base.decode16!(hex)
-    end
-
-    test "should be able to compare with hash" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          if Crypto.hash("foo") == String.to_hex("2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae") do
-            Contract.set_content "ok"
-          end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
-    end
-
-    test "should be able to compare" do
-      address = random_address()
-
-      code = ~s"""
-      actions triggered_by: transaction do
-          if transaction.address == String.to_hex("#{Base.encode16(address, case: :lower)}") do
-            Contract.set_content "ok"
-          end
-      end
-      """
-
-      trigger_tx = %Transaction{
-        type: :data,
-        address: address,
-        data: %TransactionData{}
-      }
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} =
-               sanitize_parse_execute(code, %{
-                 "transaction" => Constants.from_transaction(trigger_tx)
-               })
-    end
-
-    test "should not parse if compiler knows there is a type error" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          Contract.set_content String.to_hex(123)
-      end
-      """
-
-      {:error, _, "invalid function arguments"} = sanitize_parse_execute(code)
-    end
-
-    test "should return nil if size of string is incorrect" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          if nil == String.to_hex("abc") do
-            Contract.set_content "ok"
-          end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
-    end
-
-    test "should return nil if size of string contains non hexadecimal chars" do
-      code = ~s"""
-      actions triggered_by: transaction do
-          if nil == String.to_hex("fghijk") do
-            Contract.set_content "ok"
-          end
-      end
-      """
-
-      assert %Transaction{data: %TransactionData{content: "ok"}} = sanitize_parse_execute(code)
+    test "should keep hex if string is already hex" do
+      assert "ABCD" = String.to_hex("ABCD")
+      assert "ABCD" = String.to_hex("abcd")
     end
   end
 
