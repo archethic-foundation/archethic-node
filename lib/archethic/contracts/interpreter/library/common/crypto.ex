7405c8c6812297cd79d3136abfe79d00b794a287
Neylix <julien.leclerc05@protonmail.com>
2023-08-03 16:02:48 +0200

    Add Crypto.sign function in SC library

--- lib/archethic/contracts/interpreter/library/common/crypto.ex
+++ lib/archethic/contracts/interpreter/library/common/crypto.ex
@@ -2,10 +2,14 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.Crypto do
   @moduledoc false
   @behaviour Archethic.Contracts.Interpreter.Library
 
-  alias Archethic.Tag
   alias Archethic.Contracts.Interpreter.ASTHelper, as: AST
   alias Archethic.Contracts.Interpreter.Legacy
   alias Archethic.Contracts.Interpreter.Legacy.UtilsInterpreter
+  alias Archethic.Contracts.Interpreter.Scope
+
+  alias Archethic.Crypto
+
+  alias Archethic.Tag
 
   use Tag
 
@@ -17,6 +21,29 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.Crypto do
 
   def hash(content, algo), do: Legacy.Library.hash(content, algo)
 
+  @spec sign(hash :: binary()) :: map()
+  def sign(hash) do
+    hash_payload = Base.decode16!(hash)
+
+    case Scope.read_global([:contract_seed]) do
+      nil ->
+        raise "Contract seed has not been set for Crypto.sign"
+
+      seed ->
+        {_pub, <<_::16, priv::binary>>} = Crypto.derive_keypair(seed, 0, :secp256k1)
+
+        {:ok, {r, s, v}} = ExSecp256k1.sign(hash_payload, priv)
+
+        %{
+          "signature" => %{
+            "r" => Base.encode16(r),
+            "s" => Base.encode16(s)
+          },
+          "recid" => v
+        }
+    end
+  end
+
   @spec check_types(atom(), list()) :: boolean()
   def check_types(:hash, [first, second]) do
     (AST.is_binary?(first) || AST.is_variable_or_function_call?(first)) &&
@@ -24,6 +51,10 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.Crypto do
   end
 
   def check_types(:hash, [first]) do
+    AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
+  end
+
+  def check_types(:sign, [first]) do
     AST.is_binary?(first) || AST.is_variable_or_function_call?(first)
   end
 
--- test/archethic/contracts/interpreter/library/common/crypto_test.exs
+++ test/archethic/contracts/interpreter/library/common/crypto_test.exs
@@ -6,7 +6,15 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.CryptoTest do
 
   use ArchethicCase
 
+  alias Archethic.Contracts.Contract
+  alias Archethic.Contracts.Interpreter
   alias Archethic.Contracts.Interpreter.Library.Common.Crypto
+
+  alias Archethic.TransactionChain.Transaction
+  alias Archethic.TransactionChain.TransactionData
+
+  alias Archethic.ContractFactory
+  alias Archethic.TransactionFactory
 
   doctest Crypto
 
@@ -42,6 +50,44 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.CryptoTest do
       hex_hash = Base.encode16(hash)
 
       assert Crypto.hash(hex_hash, "keccak256") == expected_hash
+    end
+  end
+
+  describe "sign/1" do
+    test "should retrieve seed from scope and sign hash" do
+      code = ~S"""
+      @version 1
+
+      condition transaction: []
+
+      actions triggered_by: transaction do
+        hash = Crypto.hash(transaction.content, "sha256")
+        sig = Crypto.sign(hash)
+        Contract.set_content Json.to_string(sig)
+      end
+      """
+
+      contract = ContractFactory.create_valid_contract_tx(code) |> Contract.from_transaction!()
+
+      trigger_tx = TransactionFactory.create_valid_transaction([], content: "I'll be signed !")
+
+      assert {:ok, %Transaction{data: %TransactionData{content: content}}} =
+               Interpreter.execute_trigger({:transaction, nil, nil}, contract, trigger_tx, nil)
+
+      assert {:ok, sig} = Jason.decode(content)
+
+      assert %{
+               "signature" => %{
+                 "r" => "BCAA43A2972A94FE42F4989EBB826B3F9BDC841623E22C7E7F8602D06C15B99E",
+                 "s" => "1017847D5B93517FA802CC3CA07D66DBABC72D3BAEEFC416D1351345AD007290"
+               },
+               "recid" => 0
+             } = sig
+    end
+
+    test "should raise an error if contract seed is not set" do
+      hash = :crypto.strong_rand_bytes(32) |> Base.encode16()
+      assert_raise RuntimeError, fn -> Crypto.sign(hash) end
     end
   end
 end
