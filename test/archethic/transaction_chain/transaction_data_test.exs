98765ee40d3c92327cb86cf754c1e3444e2a0d97
Bastien Chamagne <bastien@archethic.net>
2023-08-09 15:58:46 +0200

    merge 2 TransactionDataTest modules

--- test/archethic/transaction_chain/transaction_data_test.exs
+++ test/archethic/transaction_chain/transaction_data_test.exs
@@ -1,6 +1,7 @@
 defmodule Archethic.TransactionChain.TransactionDataTest do
   @moduledoc false
 
+  alias Archethic.Crypto
   alias Archethic.TransactionChain.TransactionData
   alias Archethic.TransactionChain.TransactionData.Ownership
   alias Archethic.TransactionChain.TransactionData.Ledger
@@ -8,8 +9,11 @@ defmodule Archethic.TransactionChain.TransactionDataTest do
   alias Archethic.TransactionChain.TransactionData.TokenLedger
   alias Archethic.TransactionChain.TransactionData.Recipient
 
+  use ExUnitProperties
   use ArchethicCase
   import ArchethicCase
+
+  doctest TransactionData
 
   describe "serialize/deserialize" do
     test "should work" do
@@ -66,6 +70,65 @@ defmodule Archethic.TransactionChain.TransactionDataTest do
                data
                |> TransactionData.serialize(1)
                |> TransactionData.deserialize(1)
+    end
+
+    property "symmetric serialization/deserialization of transaction data" do
+      check all(
+              code <- StreamData.binary(),
+              content <- StreamData.binary(),
+              secret <- StreamData.binary(min_length: 1),
+              authorized_key_seeds <-
+                StreamData.list_of(StreamData.binary(length: 32), min_length: 1),
+              transfers <-
+                StreamData.map_of(StreamData.binary(length: 32), StreamData.positive_integer()),
+              recipients <- list_of(StreamData.binary(length: 32))
+            ) do
+        authorized_public_keys =
+          Enum.map(authorized_key_seeds, fn seed ->
+            {pub, _} = Crypto.generate_deterministic_keypair(seed)
+            pub
+          end)
+
+        recipients_addresses = Enum.map(recipients, &<<0::8, 0::8, &1::binary>>)
+
+        transfers =
+          Enum.map(transfers, fn {to, amount} ->
+            %UCOLedger.Transfer{to: <<0::8, 0::8, to::binary>>, amount: amount}
+          end)
+
+        {tx_data, _} =
+          %TransactionData{
+            code: code,
+            content: content,
+            ownerships: [
+              Ownership.new(
+                secret,
+                :crypto.strong_rand_bytes(32),
+                authorized_public_keys
+              )
+            ],
+            ledger: %Ledger{
+              uco: %UCOLedger{
+                transfers: transfers
+              }
+            },
+            recipients: recipients_addresses
+          }
+          |> TransactionData.serialize(current_transaction_version())
+          |> TransactionData.deserialize(current_transaction_version())
+
+        assert tx_data.code == code
+        assert tx_data.content == content
+        assert List.first(tx_data.ownerships).secret == secret
+
+        assert Enum.all?(
+                 Ownership.list_authorized_public_keys(List.first(tx_data.ownerships)),
+                 &(&1 in authorized_public_keys)
+               )
+
+        assert tx_data.recipients == recipients_addresses
+        assert tx_data.ledger.uco.transfers == transfers
+      end
     end
   end
 end
