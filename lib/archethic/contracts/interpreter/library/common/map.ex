3e6542aed32c3f31d14743dbdd4e2a16488161ee
Neylix <julien.leclerc05@protonmail.com>
2023-08-24 13:10:08 +0200

    Add Map.delete function in SC

--- lib/archethic/contracts/interpreter/library/common/map.ex
+++ lib/archethic/contracts/interpreter/library/common/map.ex
@@ -8,32 +8,25 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.Map do
   use Tag
 
   @spec new() :: map()
-  defdelegate new(),
-    to: Map
+  defdelegate new(), to: Map
 
   @spec keys(map()) :: list(String.t())
-  defdelegate keys(map),
-    to: Map
+  defdelegate keys(map), to: Map
 
   @spec values(map()) :: list(any())
-  defdelegate values(map),
-    to: Map
+  defdelegate values(map), to: Map
 
   @spec size(map()) :: integer()
-  def size(map) do
-    length(Map.keys(map))
-  end
+  def size(map), do: map_size(map)
 
   @spec get(map(), binary(), any()) :: any()
-  defdelegate get(map, key, default \\ nil),
-    to: Map
+  defdelegate get(map, key, default \\ nil), to: Map
 
   @spec set(map(), binary(), any()) :: map()
-  def set(map, key, value) do
-    Map.update(map, key, value, fn _ ->
-      value
-    end)
-  end
+  defdelegate set(map, key, value), to: Map, as: :put
+
+  @spec delete(map(), binary()) :: map()
+  defdelegate delete(map, key), to: Map
 
   @spec check_types(atom(), list()) :: boolean()
   def check_types(:new, []) do
@@ -65,6 +58,10 @@ defmodule Archethic.Contracts.Interpreter.Library.Common.Map do
   def check_types(:set, [first, second, _third]) do
     (AST.is_map?(first) || AST.is_variable_or_function_call?(first)) &&
       (AST.is_binary?(second) || AST.is_variable_or_function_call?(second))
+  end
+
+  def check_types(:delete, [first, _second]) do
+    AST.is_map?(first) || AST.is_variable_or_function_call?(first)
   end
 
   def check_types(_, _), do: false
