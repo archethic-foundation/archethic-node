fe1118d981aaa39aa234e56868356fecfe5a26e8
Neylix <julien.leclerc05@protonmail.com>
2023-10-02 12:51:48 +0200

    Add contract_context in validation messages

--- lib/archethic/p2p/message/validate_transaction.ex
+++ lib/archethic/p2p/message/validate_transaction.ex
@@ -2,8 +2,9 @@ defmodule Archethic.P2P.Message.ValidateTransaction do
   @moduledoc false
 
   @enforce_keys [:transaction]
-  defstruct [:transaction]
+  defstruct [:transaction, :contract_context]
 
+  alias Archethic.Contracts.Contract
   alias Archethic.TransactionChain.Transaction
   alias Archethic.P2P.Message.ReplicationError
   alias Archethic.P2P.Message.Ok
@@ -11,7 +12,8 @@ defmodule Archethic.P2P.Message.ValidateTransaction do
   alias Archethic.Crypto
 
   @type t :: %__MODULE__{
-          transaction: Transaction.t()
+          transaction: Transaction.t(),
+          contract_context: nil | Contract.Context.t()
         }
 
   @spec process(__MODULE__.t(), Crypto.key()) :: Ok.t() | ReplicationError.t()
@@ -27,16 +29,27 @@ defmodule Archethic.P2P.Message.ValidateTransaction do
   end
 
   @spec serialize(t()) :: bitstring()
-  def serialize(%__MODULE__{transaction: tx}) do
-    Transaction.serialize(tx)
+  def serialize(%__MODULE__{transaction: tx, contract_context: nil}) do
+    <<Transaction.serialize(tx)::bitstring, 0::8>>
+  end
+
+  def serialize(%__MODULE__{transaction: tx, contract_context: contract_context}) do
+    <<Transaction.serialize(tx)::bitstring, 1::8,
+      Contract.Context.serialize(contract_context)::bitstring>>
   end
 
   @spec deserialize(bitstring()) :: {t(), bitstring()}
   def deserialize(bin) when is_bitstring(bin) do
     {tx, rest} = Transaction.deserialize(bin)
 
+    {contract_context, rest} =
+      case rest do
+        <<0::8, rest::bitstring>> -> {nil, rest}
+        <<1::8, rest::bitstring>> -> Contract.Context.deserialize(rest)
+      end
+
     {
-      %__MODULE__{transaction: tx},
+      %__MODULE__{transaction: tx, contract_context: contract_context},
       rest
     }
   end
