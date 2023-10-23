9c6f83ccec3c2535badb35ed08de64dde8317b4c
Neylix <julien.leclerc05@protonmail.com>
2023-10-23 16:28:08 +0200

    Up version 1.3.1-rc4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.3.1-rc3",
+      version: "1.3.1-rc4",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
