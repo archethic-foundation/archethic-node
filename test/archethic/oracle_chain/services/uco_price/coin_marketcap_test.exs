91745e3453f97d05f15807b58eb0becdd542e0f3
bchamagne <bastien@archethic.net>
2023-11-17 15:02:39 +0100

    duplicate coin market cap oracle provider because they will soon change the url

--- test/archethic/oracle_chain/services/uco_price/coin_marketcap_test.exs
+++ test/archethic/oracle_chain/services/uco_price/coin_marketcap_test.exs
@@ -1,11 +1,18 @@
 defmodule Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCapTest do
   use ExUnit.Case
 
-  alias Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCap
+  alias Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCapUniris
+  alias Archethic.OracleChain.Services.UCOPrice.Providers.CoinMarketCapArchethic
 
   @tag oracle_provider: true
-  test "fetch/1 should get the current UCO price from CoinMarketCap" do
-    assert {:ok, %{"eur" => prices}} = CoinMarketCap.fetch(["eur"])
+  test "fetch/1 should get the current UCO price from CoinMarketCap (uniris)" do
+    assert {:ok, %{"eur" => prices}} = CoinMarketCapUniris.fetch(["eur"])
+    assert is_list(prices)
+  end
+
+  @tag oracle_provider: true
+  test "fetch/1 should get the current UCO price from CoinMarketCap (archethic)" do
+    assert {:ok, %{"eur" => prices}} = CoinMarketCapArchethic.fetch(["eur"])
     assert is_list(prices)
   end
 end
