9716ce419c1ff5ff4e47dac4d86158300218cb7e
bchamagne <bastien@archethic.net>
2023-12-01 09:33:13 +0100

    Bump version v1.4.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.4.0",
+      version: "1.4.1",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
