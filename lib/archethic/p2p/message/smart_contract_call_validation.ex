881cf17fe9f57a4c7163fd73b528caddc587a6cd
bchamagne <bastien@archethic.net>
2023-12-06 08:13:05 +0100

    Add reason to SmartContractCallValidation msg

--- lib/archethic/p2p/message/smart_contract_call_validation.ex
+++ lib/archethic/p2p/message/smart_contract_call_validation.ex
@@ -3,9 +3,16 @@ defmodule Archethic.P2P.Message.SmartContractCallValidation do
   Represents a module to attest the validation of a transaction towards a contract
   """
 
+  alias Archethic.Contracts.Contract.Failure
+  alias Archethic.Utils.VarInt
+
   @type t :: %__MODULE__{
           status:
-            :ok | {:error, :transaction_not_exists | :invalid_execution | :insufficient_funds},
+            :ok
+            | {:error, :transaction_not_exists}
+            | {:error, :insufficient_funds}
+            | {:error, :invalid_execution, Failure.t()}
+            | {:error, :invalid_condition, String.t()},
           fee: non_neg_integer()
         }
 
@@ -13,60 +20,45 @@ defmodule Archethic.P2P.Message.SmartContractCallValidation do
 
   @doc """
   Serialize message into binary
-
-  ## Examples
-
-      iex> %SmartContractCallValidation{status: :ok, fee: 186_435_476}
-      ...> |> SmartContractCallValidation.serialize()
-      <<0, 0, 0, 0, 0, 11, 28, 199, 148>>
-
-      iex> %SmartContractCallValidation{status: {:error, :transaction_not_exists}, fee: 0}
-      ...> |> SmartContractCallValidation.serialize()
-      <<1, 0, 0, 0, 0, 0, 0, 0, 0>>
-
-      iex> %SmartContractCallValidation{status: {:error, :invalid_execution}, fee: 0}
-      ...> |> SmartContractCallValidation.serialize()
-      <<2, 0, 0, 0, 0, 0, 0, 0, 0>>
   """
   def serialize(%__MODULE__{status: status, fee: fee}) do
-    <<serialize_status(status)::8, fee::64>>
+    <<serialize_status(status)::bitstring, fee::64>>
   end
 
-  defp serialize_status(:ok), do: 0
-  defp serialize_status({:error, :transaction_not_exists}), do: 1
-  defp serialize_status({:error, :invalid_execution}), do: 2
-  defp serialize_status({:error, :insufficient_funds}), do: 3
+  defp serialize_status(:ok), do: <<0::8>>
+  defp serialize_status({:error, :transaction_not_exists}), do: <<1::8>>
+
+  defp serialize_status({:error, :invalid_execution, failure}),
+    do: <<2::8, Failure.serialize(failure)::bitstring>>
+
+  defp serialize_status({:error, :invalid_condition, subject}),
+    do: <<3::8, VarInt.from_value(byte_size(subject))::binary, subject::binary>>
+
+  defp serialize_status({:error, :insufficient_funds}), do: <<4::8>>
 
   @doc """
   Deserialize the encoded message
-
-  ## Examples
-
-      iex> SmartContractCallValidation.deserialize(<<0, 0, 0, 0, 0, 11, 28, 199, 148>>)
-      {
-        %SmartContractCallValidation{status: :ok, fee: 186_435_476},
-        ""
-      }
-
-      iex> SmartContractCallValidation.deserialize(<<1, 0, 0, 0, 0, 0, 0, 0, 0>>)
-      {
-        %SmartContractCallValidation{status: {:error, :transaction_not_exists}, fee: 0},
-        ""
-      }
-
-      iex> SmartContractCallValidation.deserialize(<<2, 0, 0, 0, 0, 0, 0, 0, 0>>)
-      {
-        %SmartContractCallValidation{status: {:error, :invalid_execution}, fee: 0},
-        ""
-      }
   """
-  def deserialize(<<status_byte::8, fee::64, rest::bitstring>>) do
-    status = deserialize_status(status_byte)
+  def deserialize(<<bin::bitstring>>) do
+    {status, <<fee::64, rest::bitstring>>} = deserialize_status(bin)
     {%__MODULE__{status: status, fee: fee}, rest}
   end
 
-  defp deserialize_status(0), do: :ok
-  defp deserialize_status(1), do: {:error, :transaction_not_exists}
-  defp deserialize_status(2), do: {:error, :invalid_execution}
-  defp deserialize_status(3), do: {:error, :insufficient_funds}
+  defp deserialize_status(<<0::8, rest::bitstring>>), do: {:ok, rest}
+
+  defp deserialize_status(<<1::8, rest::bitstring>>),
+    do: {{:error, :transaction_not_exists}, rest}
+
+  defp deserialize_status(<<2::8, rest::bitstring>>) do
+    {failure, rest} = Failure.deserialize(rest)
+    {{:error, :invalid_execution, failure}, rest}
+  end
+
+  defp deserialize_status(<<3::8, rest::bitstring>>) do
+    {subject_size, rest} = VarInt.get_value(rest)
+    <<subject::binary-size(subject_size), rest::bitstring>> = rest
+    {{:error, :invalid_condition, subject}, rest}
+  end
+
+  defp deserialize_status(<<4::8, rest::bitstring>>), do: {{:error, :inets_sup}, rest}
 end
