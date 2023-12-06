15c0893e0b55eefe692b7d98a9f2442a3683e00f
bchamagne <bastien@archethic.net>
2023-12-06 08:13:05 +0100

    Add reason to SmartContractCallValidation msg

--- test/archethic/p2p/message/smart_contract_call_validation_test.exs
+++ test/archethic/p2p/message/smart_contract_call_validation_test.exs
@@ -4,4 +4,31 @@ defmodule Archethic.P2P.Message.SmartContractCallValidationTest do
   alias Archethic.P2P.Message.SmartContractCallValidation
 
   doctest SmartContractCallValidation
+
+  describe "serialization deserialization" do
+    test "should encode decode properly without reason" do
+      msg = %SmartContractCallValidation{valid?: true, fee: 186_435_476}
+
+      assert {^msg, <<>>} =
+               msg
+               |> SmartContractCallValidation.serialize()
+               |> SmartContractCallValidation.deserialize()
+
+      msg = %SmartContractCallValidation{valid?: false, fee: 104}
+
+      assert {^msg, <<>>} =
+               msg
+               |> SmartContractCallValidation.serialize()
+               |> SmartContractCallValidation.deserialize()
+    end
+
+    test "should encode decode properly with a reason" do
+      msg = %SmartContractCallValidation{valid?: false, fee: 104, reason: "inacceptable value"}
+
+      assert {^msg, <<>>} =
+               msg
+               |> SmartContractCallValidation.serialize()
+               |> SmartContractCallValidation.deserialize()
+    end
+  end
 end
