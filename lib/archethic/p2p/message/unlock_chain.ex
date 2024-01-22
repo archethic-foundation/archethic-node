4ebc12a7d5b13dcd124f8cc794ef2009f80b2f0f
Neylix <julien.leclerc05@protonmail.com>
2024-01-22 17:12:04 +0100

    Add UnlockChain message

---
+++ lib/archethic/p2p/message/unlock_chain.ex
@@ -0,0 +1,29 @@
+defmodule Archethic.P2P.Message.UnlockChain do
+  @moduledoc """
+  Request a storage node to unlock the chain for an address
+  """
+  alias Archethic.Crypto
+  alias Archethic.Mining.ChainLock
+  alias Archethic.P2P.Message.Ok
+  alias Archethic.Utils
+
+  @enforce_keys [:address]
+  defstruct [:address]
+
+  @type t :: %__MODULE__{address: Crypto.prepended_hash()}
+
+  @spec process(__MODULE__.t(), Crypto.key()) :: Ok.t()
+  def process(%__MODULE__{address: address}, _) do
+    ChainLock.unlock(address)
+    %Ok{}
+  end
+
+  @spec serialize(t()) :: bitstring()
+  def serialize(%__MODULE__{address: address}), do: <<address::binary>>
+
+  @spec deserialize(bitstring()) :: {t(), bitstring}
+  def deserialize(bin) do
+    {address, rest} = Utils.deserialize_address(bin)
+    {%__MODULE__{address: address}, rest}
+  end
+end
