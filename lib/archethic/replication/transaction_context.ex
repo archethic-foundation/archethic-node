d47e87f9a8289d0599f91b6fe7f2168b82964ec6
Neylix <julien.leclerc05@protonmail.com>
2024-02-12 23:49:06 +0100

    Optimize stream previous chain in replication

--- lib/archethic/replication/transaction_context.ex
+++ lib/archethic/replication/transaction_context.ex
@@ -33,18 +33,22 @@ defmodule Archethic.Replication.TransactionContext do
   @doc """
   Stream transaction chain
   """
-  @spec stream_transaction_chain(address :: Crypto.versioned_hash(), list(Node.t())) ::
-          Enumerable.t() | list(Transaction.t())
-  def stream_transaction_chain(address, node_list) when is_binary(address) do
-    with storage_nodes <- Election.chain_storage_nodes(address, node_list),
-         {:ok, genesis_address} when genesis_address != address <-
-           TransactionChain.fetch_genesis_address(address, storage_nodes),
-         paging_address when paging_address != address <-
-           TransactionChain.get_last_stored_address(genesis_address) do
-      TransactionChain.fetch(address, storage_nodes, paging_state: paging_address)
-      |> Stream.take_while(&(Transaction.previous_address(&1) != address))
-    else
-      _ -> []
+  @spec stream_transaction_chain(
+          genesis_address :: Crypto.prepended_hash(),
+          limit_address :: Crypto.prepended_hash(),
+          nodes :: list(Node.t())
+        ) :: Enumerable.t() | list(Transaction.t())
+  def stream_transaction_chain(genesis_address, limit_address, node_list) do
+    case TransactionChain.get_last_stored_address(genesis_address) do
+      ^limit_address ->
+        []
+
+      paging_address ->
+        storage_nodes = Election.chain_storage_nodes(limit_address, node_list)
+
+        genesis_address
+        |> TransactionChain.fetch(storage_nodes, paging_state: paging_address)
+        |> Stream.take_while(&(Transaction.previous_address(&1) != limit_address))
     end
   end
 
