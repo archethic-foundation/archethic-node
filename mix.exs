dd32a51f5d17bf2e504812e76713ba903579d5de
Neylix <julien.leclerc05@protonmail.com>
2024-03-15 17:02:40 +0100

    Up version 1.5.0-rc5

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.5.0-rc4",
+      version: "1.5.0-rc5",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
