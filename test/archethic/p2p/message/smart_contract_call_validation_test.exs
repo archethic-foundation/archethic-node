78c9f507fd16241bac1766061fefcd7e980f2f1c
bchamagne <bastien@archethic.net>
2024-04-10 16:50:20 +0200

    add latest_validation_time to SmartContractCallValidation message

--- test/archethic/p2p/message/smart_contract_call_validation_test.exs
+++ test/archethic/p2p/message/smart_contract_call_validation_test.exs
@@ -7,21 +7,35 @@ defmodule Archethic.P2P.Message.SmartContractCallValidationTest do
   doctest SmartContractCallValidation
 
   test "serialization deserialization" do
-    msg = %SmartContractCallValidation{status: :ok, fee: 186_435_476}
+    last_chain_sync_date = DateTime.utc_now() |> DateTime.truncate(:millisecond)
+
+    msg = %SmartContractCallValidation{
+      status: :ok,
+      fee: 186_435_476,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
              |> SmartContractCallValidation.serialize()
              |> SmartContractCallValidation.deserialize()
 
-    msg = %SmartContractCallValidation{status: {:error, :transaction_not_exists}, fee: 0}
+    msg = %SmartContractCallValidation{
+      status: {:error, :transaction_not_exists},
+      fee: 0,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
              |> SmartContractCallValidation.serialize()
              |> SmartContractCallValidation.deserialize()
 
-    msg = %SmartContractCallValidation{status: {:error, :invalid_condition, "content"}, fee: 0}
+    msg = %SmartContractCallValidation{
+      status: {:error, :invalid_condition, "content"},
+      fee: 0,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
@@ -29,7 +43,12 @@ defmodule Archethic.P2P.Message.SmartContractCallValidationTest do
              |> SmartContractCallValidation.deserialize()
 
     failure = %Failure{user_friendly_error: "Friendly error", error: :execution_timeout}
-    msg = %SmartContractCallValidation{status: {:error, :invalid_execution, failure}, fee: 0}
+
+    msg = %SmartContractCallValidation{
+      status: {:error, :invalid_execution, failure},
+      fee: 0,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
@@ -42,7 +61,11 @@ defmodule Archethic.P2P.Message.SmartContractCallValidationTest do
       data: %{"code" => 123, "message" => "Throw error message", "data" => nil}
     }
 
-    msg = %SmartContractCallValidation{status: {:error, :invalid_execution, failure}, fee: 0}
+    msg = %SmartContractCallValidation{
+      status: {:error, :invalid_execution, failure},
+      fee: 0,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
@@ -55,7 +78,11 @@ defmodule Archethic.P2P.Message.SmartContractCallValidationTest do
       data: %{"code" => 123, "message" => "Throw error message", "data" => ["list", "value"]}
     }
 
-    msg = %SmartContractCallValidation{status: {:error, :invalid_execution, failure}, fee: 0}
+    msg = %SmartContractCallValidation{
+      status: {:error, :invalid_execution, failure},
+      fee: 0,
+      last_chain_sync_date: last_chain_sync_date
+    }
 
     assert {^msg, <<>>} =
              msg
