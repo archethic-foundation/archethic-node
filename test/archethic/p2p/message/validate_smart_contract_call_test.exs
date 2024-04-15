f8f1a9ad803ba8f8d56feaf89058cb8b9be5ae57
Neylix <julien.leclerc05@protonmail.com>
2024-04-15 09:42:49 +0200

    Add fund check tests

--- test/archethic/p2p/message/validate_smart_contract_call_test.exs
+++ test/archethic/p2p/message/validate_smart_contract_call_test.exs
@@ -2,16 +2,28 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
   use ArchethicCase
   import ArchethicCase
 
+  alias Archethic.P2P.Message.UnspentOutputList
+  alias Archethic.P2P.Message.GetUnspentOutputs
   alias Archethic.Mining.Fee
   alias Archethic.P2P
   alias Archethic.P2P.Node
   alias Archethic.P2P.Message.SmartContractCallValidation
   alias Archethic.P2P.Message.ValidateSmartContractCall
 
+  alias Archethic.TransactionChain.Transaction
+  alias Archethic.TransactionChain.Transaction.ValidationStamp.LedgerOperations.UnspentOutput
+
+  alias Archethic.TransactionChain.Transaction.ValidationStamp.LedgerOperations.VersionedUnspentOutput
+
   alias Archethic.TransactionChain.TransactionData.Recipient
+  alias Archethic.TransactionChain.TransactionData.Ledger
+  alias Archethic.TransactionChain.TransactionData.UCOLedger
+  alias Archethic.TransactionChain.TransactionData.UCOLedger.Transfer
 
   alias Archethic.ContractFactory
   alias Archethic.TransactionFactory
+
+  alias Archethic.Utils
 
   doctest ValidateSmartContractCall
 
@@ -223,6 +235,136 @@ defmodule Archethic.P2P.Message.ValidateSmartContractCallTest do
       assert %SmartContractCallValidation{status: {:error, :invalid_execution}, fee: 0} =
                %ValidateSmartContractCall{
                  recipient: %Recipient{address: "@SC1_for_contract_with_invalid_message"},
+                 transaction: incoming_tx,
+                 timestamp: DateTime.utc_now()
+               }
+               |> ValidateSmartContractCall.process(:crypto.strong_rand_bytes(32))
+    end
+
+    test "should return insufficient_funds if contract has not enough funds " do
+      tx =
+        %Transaction{address: contract_address} =
+        ~s"""
+        @version 1
+
+        condition triggered_by: transaction, as: []
+
+        actions triggered_by: transaction do
+          amount = Map.get(transaction.uco_transfers, contract.address)
+          Contract.add_uco_transfer to: transaction.address, amount: amount + 5
+        end
+        """
+        |> ContractFactory.create_valid_contract_tx(seed: random_seed())
+
+      contract_genesis_address = Transaction.previous_address(tx)
+
+      v_utxo =
+        %UnspentOutput{
+          from: random_address(),
+          amount: Utils.to_bigint(3),
+          type: :UCO,
+          timestamp: DateTime.utc_now()
+        }
+        |> VersionedUnspentOutput.wrap_unspent_output(current_protocol_version())
+
+      utxo = %UnspentOutput{
+        from: random_address(),
+        amount: Utils.to_bigint(5),
+        type: :UCO,
+        timestamp: DateTime.utc_now()
+      }
+
+      recipient = %Recipient{address: contract_genesis_address}
+
+      incoming_tx =
+        TransactionFactory.create_valid_transaction([utxo],
+          ledger: %Ledger{
+            uco: %UCOLedger{transfers: [%Transfer{to: contract_address, amount: 4}]}
+          },
+          recipients: [recipient]
+        )
+
+      MockClient
+      |> expect(:send_message, fn _, %GetUnspentOutputs{address: ^contract_genesis_address}, _ ->
+        {:ok, %UnspentOutputList{unspent_outputs: [v_utxo]}}
+      end)
+
+      MockDB
+      |> expect(:get_last_chain_address, fn ^contract_genesis_address ->
+        {contract_address, DateTime.utc_now()}
+      end)
+      |> expect(:get_transaction, fn ^contract_address, _, _ ->
+        {:ok, tx}
+      end)
+
+      assert %SmartContractCallValidation{status: {:error, :insufficient_funds}, fee: 0} =
+               %ValidateSmartContractCall{
+                 recipient: recipient,
+                 transaction: incoming_tx,
+                 timestamp: DateTime.utc_now()
+               }
+               |> ValidateSmartContractCall.process(:crypto.strong_rand_bytes(32))
+    end
+
+    test "should return :ok if contract has enough funds " do
+      tx =
+        %Transaction{address: contract_address} =
+        ~s"""
+        @version 1
+
+        condition triggered_by: transaction, as: []
+
+        actions triggered_by: transaction do
+          amount = Map.get(transaction.uco_transfers, contract.address)
+          Contract.add_uco_transfer to: transaction.address, amount: amount + 1
+        end
+        """
+        |> ContractFactory.create_valid_contract_tx(seed: random_seed())
+
+      contract_genesis_address = Transaction.previous_address(tx)
+
+      v_utxo =
+        %UnspentOutput{
+          from: random_address(),
+          amount: Utils.to_bigint(3),
+          type: :UCO,
+          timestamp: DateTime.utc_now()
+        }
+        |> VersionedUnspentOutput.wrap_unspent_output(current_protocol_version())
+
+      utxo = %UnspentOutput{
+        from: random_address(),
+        amount: Utils.to_bigint(5),
+        type: :UCO,
+        timestamp: DateTime.utc_now()
+      }
+
+      recipient = %Recipient{address: contract_genesis_address}
+
+      incoming_tx =
+        TransactionFactory.create_valid_transaction([utxo],
+          ledger: %Ledger{
+            uco: %UCOLedger{transfers: [%Transfer{to: contract_address, amount: 4}]}
+          },
+          recipients: [recipient]
+        )
+
+      MockClient
+      |> expect(:send_message, fn _, %GetUnspentOutputs{address: ^contract_genesis_address}, _ ->
+        {:ok, %UnspentOutputList{unspent_outputs: [v_utxo]}}
+      end)
+
+      MockDB
+      |> expect(:get_last_chain_address, fn ^contract_genesis_address ->
+        {contract_address, DateTime.utc_now()}
+      end)
+      |> expect(:get_transaction, fn ^contract_address, _, _ ->
+        {:ok, tx}
+      end)
+
+      assert %SmartContractCallValidation{status: :ok, fee: _} =
+               %ValidateSmartContractCall{
+                 recipient: recipient,
                  transaction: incoming_tx,
                  timestamp: DateTime.utc_now()
                }
