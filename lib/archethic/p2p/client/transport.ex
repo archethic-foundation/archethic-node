e6f49d016b8de5d6262f2367ba2bcd5a89e46459
bchamagne <74045243+bchamagne@users.noreply.github.com>
2024-10-04 15:58:00 +0200

    Reconnection backoff & heartbeat mechanism (#1346)
    * connection backoff and wake up
    
    heartbeat mechanism
    
    unit test the heartbeat mechanism
    
    remove max_connection_attempts
    
    add code_change/4 to add the missing fields in the state
    
    * close the socket when disconnecting, cap at 6 hours for now
    
    * add an assertion on transport closed
    
    * stop incrementing the attempts to avoid overflow

--- lib/archethic/p2p/client/transport.ex
+++ lib/archethic/p2p/client/transport.ex
@@ -4,4 +4,5 @@ defmodule Archethic.P2P.Client.Transport do
   @callback handle_connect(:inet.ip_address(), :inet.port_number()) :: {:ok, :inet.socket()}
   @callback handle_message(tuple()) :: {:ok, binary()} | {:error, :closed} | {:error, any()}
   @callback handle_send(:inet.socket(), binary()) :: :ok
+  @callback handle_close(:inet.socket()) :: :ok
 end
