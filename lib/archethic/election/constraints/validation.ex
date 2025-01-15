95d78eae4db49744bbd0c5b77e3f562949367d9b
Neylix <julien.leclerc05@protonmail.com>
2025-01-15 17:44:55 +0100

    validation election number of node is HD

--- lib/archethic/election/constraints/validation.ex
+++ lib/archethic/election/constraints/validation.ex
@@ -4,41 +4,33 @@ defmodule Archethic.Election.ValidationConstraints do
   """
 
   @default_min_validation_geo_patch 3
-  @default_min_validations 3
 
   defstruct [
     :min_geo_patch,
-    :min_validation_nodes,
     :validation_number
   ]
 
+  alias Archethic.Election.HypergeometricDistribution
   alias Archethic.TransactionChain.Transaction
-  alias Archethic.TransactionChain.TransactionData
-  alias Archethic.TransactionChain.TransactionData.Ledger
-  alias Archethic.TransactionChain.TransactionData.UCOLedger
 
   @typedoc """
   Each validation constraints represent a function which will be executed during the election algorithms:
   - min_geo_patch: Require number of distinct geographic patch for the elected validation nodes.
   This property ensure the geographical security of the transaction validation by spliting
   the computation in many place on the world.
-  - min_validation_nodes: Require number of minimum validation nodes.
   - validation_number: Require number of validation nodes for a given transaction.
   """
   @type t :: %__MODULE__{
           min_geo_patch: (() -> non_neg_integer()),
-          min_validation_nodes: (non_neg_integer() -> non_neg_integer()),
           validation_number: (Transaction.t(), non_neg_integer() -> non_neg_integer())
         }
 
   def new(
         min_geo_patch_fun \\ &min_geo_patch/0,
-        min_validation_nodes_fun \\ &min_validation_nodes/1,
-        validation_number_fun \\ &validation_number/2
+        validation_number_fun \\ &hypergeometric_distribution/1
       ) do
     %__MODULE__{
       min_geo_patch: min_geo_patch_fun,
-      min_validation_nodes: min_validation_nodes_fun,
       validation_number: validation_number_fun
     }
   end
@@ -50,46 +42,12 @@ defmodule Archethic.Election.ValidationConstraints do
   def min_geo_patch, do: @default_min_validation_geo_patch
 
   @doc """
-  Define the minimum of validations
+  Run a simulation of the hypergeometric distribution based on a number of nodes
   """
-  @spec min_validation_nodes(non_neg_integer()) :: non_neg_integer()
-  def min_validation_nodes(nb_authorized_nodes)
-      when nb_authorized_nodes < @default_min_validations,
-      do: nb_authorized_nodes
-
-  def min_validation_nodes(_), do: @default_min_validations
-
-  @doc """
-  Get the number of validations for a given transaction.
-  """
-  @spec validation_number(Transaction.t(), nb_authorized_nodes :: non_neg_integer()) ::
-          non_neg_integer()
-  def validation_number(
-        %Transaction{
-          data: %TransactionData{ledger: %Ledger{uco: %UCOLedger{transfers: transfers}}}
-        },
-        nb_authorized_nodes
-      )
-      when is_integer(nb_authorized_nodes) do
-    total_transfers = Enum.reduce(transfers, 0.0, &(&2 + &1.amount))
-
-    if total_transfers > 0 do
-      validation_number =
-        @default_min_validations +
-          (:math.log10(total_transfers / 100_000_000) |> trunc())
-
-      cond do
-        validation_number > nb_authorized_nodes ->
-          nb_authorized_nodes
-
-        validation_number < @default_min_validations ->
-          min_validation_nodes(nb_authorized_nodes)
-
-        true ->
-          validation_number
-      end
-    else
-      min_validation_nodes(nb_authorized_nodes)
-    end
+  @spec hypergeometric_distribution(nodes :: list(Node.t())) :: pos_integer()
+  def hypergeometric_distribution(nodes) when is_list(nodes) and length(nodes) >= 0 do
+    nodes
+    |> length()
+    |> HypergeometricDistribution.run_simulation()
   end
 end
