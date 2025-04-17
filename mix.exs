71f9f4a939c8b0da648544fed4777da7250b39ab
Samuel Manzanera <samuelmanzanera@protonmail.com>
2025-04-17 14:06:04 +0200

    Up version 1.6.13

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.6.12",
+      version: "1.6.13",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
