905bed45227750fdbda0bb429b45104551daaec3
pvalette <35062602+pvalette@users.noreply.github.com>
2025-05-13 23:35:23 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Archethic.MixProject do
   def project do
     [
       app: :archethic,
-      version: "1.6.20",
+      version: "1.6.21",
       build_path: "_build",
       config_path: "config/config.exs",
       deps_path: "deps",
